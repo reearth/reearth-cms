24c552cae156b080a53b384feb9d54a17774c924
maherhamoui6 <hammimo022@gmail.com>
2022-03-25 01:54:46 +0300

    Merge branch 'main' of https://github.com/reearth/reearth-cms into init/db-conn
    # Conflicts:
    #	server/go.mod
    #	server/go.sum

---
+++ server/internal/app/app.go
@@ -0,0 +1,107 @@
+package app
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/reearth/reearth-cms/server/pkg/rerror"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+)
+
+func initEcho(ctx context.Context, cfg *ServerConfig) *echo.Echo {
+	if cfg.Config == nil {
+		log.Fatalln("ServerConfig.Config is nil")
+	}
+
+	e := echo.New()
+	e.Debug = cfg.Debug
+	e.HideBanner = true
+	e.HidePort = true
+	e.HTTPErrorHandler = errorHandler(e.DefaultHTTPErrorHandler)
+
+	// basic middleware
+	logger := GetEchoLogger()
+	e.Logger = logger
+	e.Use(logger.Hook(), middleware.Recover(), otelecho.Middleware("reearth-cms"))
+	origins := allowedOrigins(cfg)
+	if len(origins) > 0 {
+		e.Use(
+			middleware.CORSWithConfig(middleware.CORSConfig{
+				AllowOrigins: origins,
+			}),
+		)
+	}
+
+	// GraphQL Playground without auth
+	if cfg.Debug || cfg.Config.Dev {
+		e.GET("/graphql", echo.WrapHandler(
+			playground.Handler("reearth-cms", "/api/graphql"),
+		))
+	}
+
+	// apis
+	api := e.Group("/api")
+	api.GET("/ping", Ping())
+
+	return e
+}
+func allowedOrigins(cfg *ServerConfig) []string {
+	if cfg == nil {
+		return nil
+	}
+	origins := append([]string{}, cfg.Config.Origins...)
+	if cfg.Debug {
+		origins = append(origins, "http://localhost:3000", "http://localhost:8080")
+	}
+	return origins
+}
+
+func errorMessage(err error, log func(string, ...interface{})) (int, string) {
+	code := http.StatusBadRequest
+	msg := err.Error()
+
+	if err2, ok := err.(*echo.HTTPError); ok {
+		code = err2.Code
+		if msg2, ok := err2.Message.(string); ok {
+			msg = msg2
+		} else if msg2, ok := err2.Message.(error); ok {
+			msg = msg2.Error()
+		} else {
+			msg = "error"
+		}
+		if err2.Internal != nil {
+			log("echo internal err: %+v", err2)
+		}
+	} else if errors.Is(err, rerror.ErrNotFound) {
+		code = http.StatusNotFound
+		msg = "not found"
+	} else {
+		if ierr := rerror.UnwrapErrInternal(err); ierr != nil {
+			code = http.StatusInternalServerError
+			msg = "internal server error"
+		}
+	}
+
+	return code, msg
+}
+func errorHandler(next func(error, echo.Context)) func(error, echo.Context) {
+	return func(err error, c echo.Context) {
+		if c.Response().Committed {
+			return
+		}
+
+		code, msg := errorMessage(err, func(f string, args ...interface{}) {
+			c.Echo().Logger.Errorf(f, args...)
+		})
+		if err := c.JSON(code, map[string]string{
+			"error": msg,
+		}); err != nil {
+			next(err, c)
+		}
+	}
+}
---
+++ server/internal/app/main.go
@@ -0,0 +1,77 @@
+package app
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/reearth/reearth-cms/server/pkg/log"
+
+	"github.com/labstack/echo/v4"
+)
+
+func Start(debug bool, version string) {
+	log.Infof("reearth-cms %s", version)
+
+	ctx := context.Background()
+
+	// Load config
+	conf, cerr := ReadConfig(debug)
+	if cerr != nil {
+		log.Fatal(cerr)
+	}
+
+	// Start web server
+	NewServer(ctx, &ServerConfig{
+		Config: conf,
+		Debug:  debug,
+	}).Run()
+}
+
+type WebServer struct {
+	address   string
+	appServer *echo.Echo
+}
+
+type ServerConfig struct {
+	Config *Config
+	Debug  bool
+}
+
+func NewServer(ctx context.Context, cfg *ServerConfig) *WebServer {
+	port := cfg.Config.Port
+	if port == "" {
+		port = "8080"
+	}
+
+	address := "0.0.0.0:" + port
+	if cfg.Debug {
+		address = "localhost:" + port
+	}
+
+	w := &WebServer{
+		address: address,
+	}
+
+	w.appServer = initEcho(ctx, cfg)
+	return w
+}
+
+func (w *WebServer) Run() {
+	defer log.Infoln("Server shutdown")
+
+	debugLog := ""
+	if w.appServer.Debug {
+		debugLog += " with debug mode"
+	}
+	log.Infof("server started%s at http://%s\n", debugLog, w.address)
+
+	go func() {
+		err := w.appServer.Start(w.address)
+		log.Fatalln(err.Error())
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+}
