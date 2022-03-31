617a593b66164940a6fa938cfca47911673468db
mimoham24 <69579255+mimoham24@users.noreply.github.com>
2022-03-31 23:39:22 +0400

    Update server/internal/usecase/pagination.go
    Co-authored-by: rot1024 <aayhrot@gmail.com>

--- server/internal/usecase/pagination.go
+++ server/internal/usecase/pagination.go
@@ -2,23 +2,14 @@ package usecase
 
 type Cursor string
 
+// Pagination is a struct for Relay-Style Cursor Pagination
+// ref: https://www.apollographql.com/docs/react/features/pagination/#relay-style-cursor-pagination
 type Pagination struct {
 	Before *Cursor
 	After  *Cursor
 	First  *int
 	Last   *int
 }
-
-//func NewPagination(first *int, last *int, before *Cursor, after *Cursor) *Pagination {
-//	// Relay-Style Cursor Pagination
-//	// ref: https://www.apollographql.com/docs/react/features/pagination/#relay-style-cursor-pagination
-//	return &Pagination{
-//		Before: before,
-//		After:  after,
-//		First:  first,
-//		Last:   last,
-//	}
-//}
 
 type PageInfo struct {
 	TotalCount      int
