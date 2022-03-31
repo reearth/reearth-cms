31b0a6744e5a13f2702aa1df5efd9d584c476872
maherhamoui6 <hammimo022@gmail.com>
2022-03-31 08:42:16 +0300

    resolve notes

--- server/internal/usecase/pagination.go
+++ server/internal/usecase/pagination.go
@@ -1,5 +1,7 @@
 package usecase
 
+type Cursor string
+
 type Pagination struct {
 	Before *Cursor
 	After  *Cursor
@@ -7,13 +9,68 @@ type Pagination struct {
 	Last   *int
 }
 
-func NewPagination(first *int, last *int, before *Cursor, after *Cursor) *Pagination {
-	// Relay-Style Cursor Pagination
-	// ref: https://www.apollographql.com/docs/react/features/pagination/#relay-style-cursor-pagination
-	return &Pagination{
-		Before: before,
-		After:  after,
-		First:  first,
-		Last:   last,
+//func NewPagination(first *int, last *int, before *Cursor, after *Cursor) *Pagination {
+//	// Relay-Style Cursor Pagination
+//	// ref: https://www.apollographql.com/docs/react/features/pagination/#relay-style-cursor-pagination
+//	return &Pagination{
+//		Before: before,
+//		After:  after,
+//		First:  first,
+//		Last:   last,
+//	}
+//}
+
+type PageInfo struct {
+	TotalCount      int
+	StartCursor     *Cursor
+	EndCursor       *Cursor
+	HasNextPage     bool
+	HasPreviousPage bool
+}
+
+func NewPageInfo(totalCount int, startCursor *Cursor, endCursor *Cursor, hasNextPage bool, hasPreviousPage bool) *PageInfo {
+	var sc Cursor
+	var ec Cursor
+	if startCursor != nil {
+		sc = *startCursor
 	}
+	if endCursor != nil {
+		ec = *endCursor
+	}
+
+	return &PageInfo{
+		TotalCount:      totalCount,
+		StartCursor:     sc.CopyRef(),
+		EndCursor:       ec.CopyRef(),
+		HasNextPage:     hasNextPage,
+		HasPreviousPage: hasPreviousPage,
+	}
+}
+
+func (c Cursor) Ref() *Cursor {
+	return &c
+}
+
+func CursorFromRef(c *string) *Cursor {
+	if c == nil {
+		return nil
+	}
+	d := Cursor(*c)
+	return &d
+}
+
+func (c *Cursor) CopyRef() *Cursor {
+	if c == nil {
+		return nil
+	}
+	d := *c
+	return &d
+}
+
+func (c *Cursor) StringRef() *string {
+	if c == nil {
+		return nil
+	}
+	s := string(*c)
+	return &s
 }
