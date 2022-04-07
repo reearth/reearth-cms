08f07627abded8749c7b75f5314cca6509169ca6
basel.issmail <basel.issmail@gmail.com>
2022-04-07 08:30:13 +0300

    Add routing

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,25 @@
+import styled from "@emotion/styled";
+import { Router } from "@reach/router";
 import React from "react";
+import { IntlProvider } from "react-intl";
+
+import NotFound from "@reearth/components/pages/NotFound";
+
+import RootPage from "./components/pages/Authentication/RootPage";
 
 const App: React.FC = () => {
-  return <h1>Re:earth CMS</h1>;
+  return (
+    <IntlProvider locale="en" defaultLocale="en">
+      <StyledRouter>
+        <RootPage path="/" />
+        <NotFound default />
+      </StyledRouter>
+    </IntlProvider>
+  );
 };
+
+const StyledRouter = styled(Router)`
+  height: 100%;
+`;
 
 export default App;
