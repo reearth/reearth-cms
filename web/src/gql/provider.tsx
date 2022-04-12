0017fcfc1e2ebd8829d97055c1f33e0b9e557743
basel.issmail <basel.issmail@gmail.com>
2022-04-12 07:58:35 +0300

    Add initial GQL provider

---
+++ web/src/gql/provider.tsx
@@ -0,0 +1,39 @@
+import {
+  ApolloProvider,
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import React from "react";
+
+import { useError } from "../state";
+
+const Provider: React.FC = ({ children }) => {
+  const endpoint = window.REEARTH_CONFIG?.api
+    ? `${window.REEARTH_CONFIG.api}/graphql`
+    : "/api/graphql";
+  const [, setError] = useError();
+
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (!networkError && !graphQLErrors) return;
+    const error =
+      networkError?.message ?? graphQLErrors?.map((e) => e.message).join(", ");
+    if (error) {
+      setError(error);
+    }
+  });
+
+  const cache = new InMemoryCache({});
+
+  const client = new ApolloClient({
+    uri: endpoint,
+    link: ApolloLink.from([errorLink]),
+    cache,
+    connectToDevTools: process.env.NODE_ENV === "development",
+  });
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+
+export default Provider;
