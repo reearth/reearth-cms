1705d33ce5cb5c66e339e6d4798b820c2479b5f2
basel.issmail <basel.issmail@gmail.com>
2022-04-14 08:32:05 +0300

    Add routing

---
+++ web/src/components/pages/Authentication/RootPage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export type Props = {
+  path?: string;
+};
+
+const RootPage: React.FC<Props> = () => {
+  return <h1>CMS root page</h1>;
+};
+
+export default RootPage;
