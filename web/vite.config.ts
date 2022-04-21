f570dbf529dd73fec79fb201208dfb8829f139e6
rot1024 <aayhrot@gmail.com>
2022-04-21 20:06:50 +0900

    fix ci

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -16,5 +16,8 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
   },
 });
