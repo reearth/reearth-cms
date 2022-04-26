b9b0e64088e52733beb0332fd16c129f2a131036
basel.issmail <basel.issmail@gmail.com>
2022-04-26 11:32:34 +0300

    Add root page

--- web/src/components/pages/RootPage/index.tsx
+++ web/src/components/pages/RootPage/index.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../auth";
 
 const RootPage: React.FC<{ default?: boolean }> = () => {
-  const { isAuthenticated, error, isLoading, login } = useAuth();
+  const { isAuthenticated, isLoading, login } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,7 +17,6 @@ const RootPage: React.FC<{ default?: boolean }> = () => {
     <h1>Loading</h1>
   ) : !isAuthenticated ? (
     <>
-      {error && <h1>Error</h1>}
       <div style={{ margin: "40px" }}>
         <button onClick={() => login()}>Log In</button>
       </div>
