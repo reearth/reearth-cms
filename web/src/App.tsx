f7f928273065f9b62c5737a0e4de895f3668b0b5
basel.issmail <basel.issmail@gmail.com>
2022-04-27 12:22:52 +0300

    Fix imports

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,8 @@
 import NotFound from "@reearth-cms/components/atoms/NotFound";
 import RootPage from "@reearth-cms/components/pages/Authentication/RootPage";
+import { Provider as GqlProvider } from "@reearth-cms/gql";
+import { Provider as I18nProvider } from "@reearth-cms/i18n";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
-
-import { Provider as GqlProvider } from "./gql";
-import { Provider as I18nProvider } from "./i18n";
 
 import "./App.css";
 import "antd/dist/antd.css";
