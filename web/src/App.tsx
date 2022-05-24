cd000a54c34e84197dc4288143aed8b53e29404e
basel.issmail <basel.issmail@gmail.com>
2022-05-24 23:30:29 +0300

    fix ordering

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,21 +4,21 @@ import Account from "@reearth-cms/components/organisms/Settings/Account";
 import DashboardPage from "@reearth-cms/components/pages/Dashboard";
 import RootPage from "@reearth-cms/components/pages/RootPage";
 import WorkspacePage from "@reearth-cms/components/pages/Workspace";
+import WorkspaceListPage from "@reearth-cms/components/pages/WorkspaceList";
 import { Provider as GqlProvider } from "@reearth-cms/gql";
 import { Provider as I18nProvider } from "@reearth-cms/i18n";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 
 import "./App.css";
 import "antd/dist/antd.css";
-import WorkspaceListPage from "./components/pages/WorkspaceList";
 
 function AppRoutes() {
   const routes = useRoutes([
     { path: "/", element: <RootPage /> },
     { path: "/dashboard", element: <DashboardPage /> },
     { path: "/dashboard/:workspaceId", element: <DashboardPage /> },
-    { path: "/workspace/:workspaceId", element: <WorkspacePage /> },
-    { path: "/workspace", element: <WorkspaceListPage /> },
+    { path: "/workspaces/:workspaceId", element: <WorkspacePage /> },
+    { path: "/workspaces", element: <WorkspaceListPage /> },
     { path: "/account", element: <Account /> },
     { path: "*", element: <NotFound /> },
   ]);
