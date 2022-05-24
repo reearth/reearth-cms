34feffa3e24f695a9b81fd150c404064ddb3c26e
basel.issmail <basel.issmail@gmail.com>
2022-05-24 22:56:03 +0300

    remove unecessary prop

--- web/src/components/pages/Workspace/index.tsx
+++ web/src/components/pages/Workspace/index.tsx
@@ -1,11 +1,7 @@
 import { AuthenticationRequiredPage } from "@reearth-cms/auth";
 import Workspace from "@reearth-cms/components/organisms/Settings/Workspace";
 
-export type Props = {
-  path?: string;
-};
-
-const WorkspacePage: React.FC<Props> = () => {
+const WorkspacePage: React.FC = () => {
   return (
     <AuthenticationRequiredPage>
       <Workspace />
