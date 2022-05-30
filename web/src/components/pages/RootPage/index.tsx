ef697840143ae254f1876f88869527e2b3fcf808
basel.issmail <basel.issmail@gmail.com>
2022-05-30 09:20:33 +0300

    Save changes

--- web/src/components/pages/RootPage/index.tsx
+++ web/src/components/pages/RootPage/index.tsx
@@ -1,29 +1,21 @@
 import { useAuth } from "@reearth-cms/auth";
-import Typography from "@reearth-cms/components/atoms/Typography";
 import { useEffect } from "react";
-import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
 const RootPage: React.FC = () => {
   const { isAuthenticated, isLoading, login } = useAuth();
   const navigate = useNavigate();
-  const { t } = useTranslation();
-  const { Title } = Typography;
 
   useEffect(() => {
     if (isAuthenticated) {
       navigate("/dashboard");
     }
-  }, [isAuthenticated, navigate]);
+    if (!isAuthenticated && !isLoading) {
+      login();
+    }
+  }, [isAuthenticated, navigate, login, isLoading]);
 
-  return isLoading ? (
-    <h1>Loading</h1>
-  ) : !isAuthenticated ? (
-    <div style={{ margin: "40px" }}>
-      <Title>{t("CMS root page")}</Title>
-      <button onClick={() => login()}>Log In</button>
-    </div>
-  ) : null;
+  return isLoading ? <h1>Loading</h1> : null;
 };
 
 export default RootPage;
