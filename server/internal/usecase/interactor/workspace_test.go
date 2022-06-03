e2c495f830ba19bbc1a855b53823aabf30237715
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-06-03 08:50:38 +0300

    feat(server): project queries and mutations to GraphQL schema (#76)
    * add gql schema, queries, mutation, resolvers, loaders
    
    * isolate project.graphql schema
    
    * fix go linting errors
    
    * fix PR comments

--- server/internal/usecase/interactor/workspace_test.go
+++ server/internal/usecase/interactor/workspace_test.go
@@ -14,7 +14,7 @@ import (
 func TestCreateWorkspace(t *testing.T) {
 	ctx := context.Background()
 
-	db := memory.InitRepos()
+	db := memory.New()
 
 	u := user.New().NewID().Email("aaa@bbb.com").Workspace(id.NewWorkspaceID()).MustBuild()
 	workspaceUC := NewWorkspace(db)
---
+++ server/internal/usecase/interfaces/project.go
@@ -0,0 +1,37 @@
+package interfaces
+
+import (
+	"context"
+	"errors"
+
+	"github.com/reearth/reearth-cms/server/internal/usecase"
+	"github.com/reearth/reearth-cms/server/pkg/id"
+	"github.com/reearth/reearth-cms/server/pkg/project"
+)
+
+type CreateProjectParam struct {
+	WorkspaceID id.WorkspaceID
+	Name        *string
+	Description *string
+	Alias       *string
+}
+
+type UpdateProjectParam struct {
+	ID          id.ProjectId
+	Name        *string
+	Description *string
+}
+
+var (
+	ErrProjectAliasIsNotSet    error = errors.New("project alias is not set")
+	ErrProjectAliasAlreadyUsed error = errors.New("project alias is already used by another project")
+)
+
+type Project interface {
+	Fetch(context.Context, []id.ProjectId, *usecase.Operator) ([]*project.Project, error)
+	FindByWorkspace(context.Context, id.WorkspaceID, *usecase.Pagination, *usecase.Operator) ([]*project.Project, *usecase.PageInfo, error)
+	Create(context.Context, CreateProjectParam, *usecase.Operator) (*project.Project, error)
+	Update(context.Context, UpdateProjectParam, *usecase.Operator) (*project.Project, error)
+	CheckAlias(context.Context, string) (bool, error)
+	Delete(context.Context, id.ProjectId, *usecase.Operator) error
+}
