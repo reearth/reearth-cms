f7c266104f2037bbb9f599cad609865a52a699e3
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-06-07 09:35:57 +0300

    fix test name

--- server/internal/usecase/interactor/workspace_test.go
+++ server/internal/usecase/interactor/workspace_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateWorkspace(t *testing.T) {
+func TestWorkspace_Create(t *testing.T) {
 	ctx := context.Background()
 
 	db := memory.New()
