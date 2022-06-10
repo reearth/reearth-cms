48df1d54d2336f54274535ccf782cf54bc399448
yuuri111 <s.harada0501@gmail.com>
2022-06-10 11:07:36 +0900

    create asset go and so on

--- server/internal/usecase/repo/container.go
+++ server/internal/usecase/repo/container.go
@@ -1,8 +1,62 @@
 package repo
 
+import (
+	"github.com/reearth/reearth-cms/server/internal/usecase"
+	"github.com/reearth/reearth-cms/server/pkg/id"
+)
+
 type Container struct {
 	Lock        Lock
 	User        User
 	Transaction Transaction
 	Workspace   Workspace
+}
+
+type WorkspaceFilter struct {
+	Readable id.WorkspaceIDList
+	Writable id.WorkspaceIDList
+}
+
+func WorkspaceFilterFromOperator(o *usecase.Operator) WorkspaceFilter {
+	return WorkspaceFilter{
+		Readable: o.AllReadableWorkspaces(),
+		Writable: o.AllWritableWorkspaces(),
+	}
+}
+
+func (f WorkspaceFilter) Clone() WorkspaceFilter {
+	return WorkspaceFilter{
+		Readable: f.Readable.Clone(),
+		Writable: f.Writable.Clone(),
+	}
+}
+
+func (f WorkspaceFilter) Merge(g WorkspaceFilter) WorkspaceFilter {
+	var r, w id.WorkspaceIDList
+	if f.Readable != nil || g.Readable != nil {
+		if f.Readable == nil {
+			r = append(g.Readable[:0:0], g.Readable...)
+		} else {
+			r = append(f.Readable, g.Readable...)
+		}
+	}
+	if f.Writable != nil || g.Writable != nil {
+		if f.Writable == nil {
+			w = append(g.Writable[:0:0], g.Writable...)
+		} else {
+			w = append(f.Writable, g.Writable...)
+		}
+	}
+	return WorkspaceFilter{
+		Readable: r,
+		Writable: w,
+	}
+}
+
+func (f WorkspaceFilter) CanRead(id id.WorkspaceID) bool {
+	return f.Readable == nil || f.Readable.Has(id)
+}
+
+func (f WorkspaceFilter) CanWrite(id id.WorkspaceID) bool {
+	return f.Writable == nil || f.Writable.Has(id)
 }
--- server/pkg/id/id.go
+++ server/pkg/id/id.go
@@ -3,34 +3,56 @@ package id
 import "github.com/reearth/reearth-cms/server/pkg/id/idx"
 
 type Workspace struct{}
+type Project struct{}
 type User struct{}
+type Asset struct{}
 
 func (Workspace) Type() string { return "workspace" }
+func (Project) Type() string   { return "project" }
 func (User) Type() string      { return "user" }
+func (Asset) Type() string     { return "asset" }
 
 type WorkspaceID = idx.ID[Workspace]
+type ProjectID = idx.ID[Project]
 type UserID = idx.ID[User]
+type AssetID = idx.ID[Asset]
 
 var NewWorkspaceID = idx.New[Workspace]
+var NewProjectID = idx.New[Project]
 var NewUserID = idx.New[User]
+var NewAssetID = idx.New[Asset]
 
 var MustWorkspaceID = idx.Must[Workspace]
+var MustProjectID = idx.Must[Project]
 var MustUserID = idx.Must[User]
+var MustAssetID = idx.Must[Asset]
 
 var WorkspaceIDFrom = idx.From[Workspace]
+var ProjectIDFrom = idx.From[Project]
 var UserIDFrom = idx.From[User]
+var AssetIDFrom = idx.From[Asset]
 
 var WorkspaceIDFromRef = idx.FromRef[Workspace]
+var ProjectIDFromRef = idx.FromRef[Project]
 var UserIDFromRef = idx.FromRef[User]
+var AssetIDFromRef = idx.FromRef[Asset]
 
 type WorkspaceIDList = idx.List[Workspace]
+type ProjectIDList = idx.List[Project]
 type UserIDList = idx.List[User]
+type AssetIDList = idx.List[Asset]
 
 var WorkspaceIDListFrom = idx.ListFrom[Workspace]
+var ProjectIDListFrom = idx.ListFrom[Project]
 var UserIDListFrom = idx.ListFrom[User]
+var AssetIDListFrom = idx.ListFrom[Asset]
 
 type WorkspaceIDSet = idx.Set[Workspace]
+type ProjectIDSet = idx.Set[Project]
 type UserIDSet = idx.Set[User]
+type AssetIDSet = idx.Set[Asset]
 
 var NewWorkspaceIDSet = idx.NewSet[Workspace]
+var NewProjectIDSet = idx.NewSet[Project]
 var NewUserIDSet = idx.NewSet[User]
+var NewAssetIDSet = idx.NewSet[Asset]
