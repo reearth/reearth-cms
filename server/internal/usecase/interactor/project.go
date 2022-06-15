6a22456cc3510a7d733683a4ee094775ff7cbdd4
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-06-15 20:14:36 +0300

    imp usecases in the new way

--- server/internal/usecase/interactor/project.go
+++ server/internal/usecase/interactor/project.go
@@ -10,146 +10,116 @@ import (
 	"github.com/reearth/reearth-cms/server/pkg/id"
 	"github.com/reearth/reearth-cms/server/pkg/project"
 	"github.com/reearth/reearth-cms/server/pkg/rerror"
+	"github.com/reearth/reearth-cms/server/pkg/util"
 )
 
 type Project struct {
-	common
-	projectRepo   repo.Project
-	userRepo      repo.User
-	workspaceRepo repo.Workspace
-	transaction   repo.Transaction
+	repos *repo.Container
 }
 
 func NewProject(r *repo.Container) interfaces.Project {
 	return &Project{
-		projectRepo:   r.Project,
-		userRepo:      r.User,
-		workspaceRepo: r.Workspace,
-		transaction:   r.Transaction,
+		repos: r,
 	}
 }
 
 func (i *Project) Fetch(ctx context.Context, ids []id.ProjectID, operator *usecase.Operator) ([]*project.Project, error) {
-	return i.projectRepo.FindByIDs(ctx, ids)
+	projects, err := i.repos.Project.FindByIDs(ctx, ids)
+	if err != nil {
+		return nil, err
+	}
+	pIDs := util.Map(projects, func(p *project.Project) id.WorkspaceID { return p.Workspace() })
+	return Run1(ctx, operator, i.repos, Usecase().WithReadableWorkspaces(pIDs...).Transaction(),
+		func() ([]*project.Project, error) {
+			return i.repos.Project.FindByIDs(ctx, ids)
+		})
 }
 
 func (i *Project) FindByWorkspace(ctx context.Context, id id.WorkspaceID, p *usecase.Pagination, operator *usecase.Operator) ([]*project.Project, *usecase.PageInfo, error) {
-	return i.projectRepo.FindByWorkspace(ctx, id, p)
+	return Run2(ctx, operator, i.repos, Usecase().WithReadableWorkspaces(id).Transaction(),
+		func() ([]*project.Project, *usecase.PageInfo, error) {
+			return i.repos.Project.FindByWorkspace(ctx, id, p)
+		})
 }
 
 func (i *Project) Create(ctx context.Context, p interfaces.CreateProjectParam, operator *usecase.Operator) (_ *project.Project, err error) {
-	if err := i.CanWriteWorkspace(p.WorkspaceID, operator); err != nil {
-		return nil, err
-	}
+	return Run1(ctx, operator, i.repos, Usecase().WithWritableWorkspaces(p.WorkspaceID).Transaction(),
+		func() (_ *project.Project, err error) {
+			pb := project.New().
+				NewID().
+				Workspace(p.WorkspaceID)
+			if p.Name != nil {
+				pb = pb.Name(*p.Name)
+			}
+			if p.Description != nil {
+				pb = pb.Description(*p.Description)
+			}
+			if p.Alias != nil {
+				pb = pb.Alias(*p.Alias)
+			}
 
-	tx, err := i.transaction.Begin()
-	if err != nil {
-		return
-	}
-	defer func() {
-		if err2 := tx.End(ctx); err == nil && err2 != nil {
-			err = err2
-		}
-	}()
+			proj, err := pb.Build()
+			if err != nil {
+				return nil, err
+			}
 
-	pb := project.New().
-		NewID().
-		Workspace(p.WorkspaceID)
-	if p.Name != nil {
-		pb = pb.Name(*p.Name)
-	}
-	if p.Description != nil {
-		pb = pb.Description(*p.Description)
-	}
-	if p.Alias != nil {
-		pb = pb.Alias(*p.Alias)
-	}
-
-	project, err := pb.Build()
-	if err != nil {
-		return nil, err
-	}
-
-	err = i.projectRepo.Save(ctx, project)
-	if err != nil {
-		return nil, err
-	}
-
-	tx.Commit()
-	return project, nil
+			err = i.repos.Project.Save(ctx, proj)
+			if err != nil {
+				return nil, err
+			}
+			return proj, nil
+		})
 }
 
 func (i *Project) Update(ctx context.Context, p interfaces.UpdateProjectParam, operator *usecase.Operator) (_ *project.Project, err error) {
-	tx, err := i.transaction.Begin()
-	if err != nil {
-		return
-	}
-	defer func() {
-		if err2 := tx.End(ctx); err == nil && err2 != nil {
-			err = err2
-		}
-	}()
-
-	prj, err := i.projectRepo.FindByID(ctx, p.ID)
+	proj, err := i.repos.Project.FindByID(ctx, p.ID)
 	if err != nil {
 		return nil, err
 	}
-	if err := i.CanWriteWorkspace(prj.Workspace(), operator); err != nil {
-		return nil, err
-	}
+	return Run1(ctx, operator, i.repos, Usecase().WithWritableWorkspaces(proj.Workspace()).Transaction(),
+		func() (_ *project.Project, err error) {
+			if p.Name != nil {
+				proj.UpdateName(*p.Name)
+			}
 
-	if p.Name != nil {
-		prj.UpdateName(*p.Name)
-	}
+			if p.Description != nil {
+				proj.UpdateDescription(*p.Description)
+			}
 
-	if p.Description != nil {
-		prj.UpdateDescription(*p.Description)
-	}
+			if err := i.repos.Project.Save(ctx, proj); err != nil {
+				return nil, err
+			}
 
-	if err := i.projectRepo.Save(ctx, prj); err != nil {
-		return nil, err
-	}
-
-	tx.Commit()
-	return prj, nil
+			return proj, nil
+		})
 }
 
 func (i *Project) CheckAlias(ctx context.Context, alias string) (bool, error) {
-	if !project.CheckAliasPattern(alias) {
-		return false, project.ErrInvalidAlias
-	}
+	return Run1(ctx, nil, i.repos, Usecase().Transaction(),
+		func() (bool, error) {
+			if !project.CheckAliasPattern(alias) {
+				return false, project.ErrInvalidAlias
+			}
 
-	prj, err := i.projectRepo.FindByPublicName(ctx, alias)
-	if prj == nil && err == nil || err != nil && errors.Is(err, rerror.ErrNotFound) {
-		return true, nil
-	}
+			prj, err := i.repos.Project.FindByPublicName(ctx, alias)
+			if prj == nil && err == nil || err != nil && errors.Is(err, rerror.ErrNotFound) {
+				return true, nil
+			}
 
-	return false, err
+			return false, err
+		})
 }
 
 func (i *Project) Delete(ctx context.Context, projectID id.ProjectID, operator *usecase.Operator) (err error) {
-	tx, err := i.transaction.Begin()
-	if err != nil {
-		return
-	}
-	defer func() {
-		if err2 := tx.End(ctx); err == nil && err2 != nil {
-			err = err2
-		}
-	}()
-
-	prj, err := i.projectRepo.FindByID(ctx, projectID)
+	proj, err := i.repos.Project.FindByID(ctx, projectID)
 	if err != nil {
 		return err
 	}
-	if err := i.CanWriteWorkspace(prj.Workspace(), operator); err != nil {
-		return err
-	}
-
-	if err := i.projectRepo.Remove(ctx, prj.ID()); err != nil {
-		return err
-	}
-
-	tx.Commit()
-	return nil
+	return Run0(ctx, operator, i.repos, Usecase().WithWritableWorkspaces(proj.Workspace()).Transaction(),
+		func() error {
+			if err := i.repos.Project.Remove(ctx, projectID); err != nil {
+				return err
+			}
+			return nil
+		})
 }
