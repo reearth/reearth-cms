517b5927a679a7fc2dc5e926802a402fdaba4bc6
yuuri111 <s.harada0501@gmail.com>
2022-06-15 12:16:27 +0900

    create asset and id

---
+++ server/pkg/asset/id.go
@@ -0,0 +1,22 @@
+package asset
+
+import (
+	"github.com/reearth/reearth-cms/server/pkg/id"
+)
+
+type ID = id.AssetID
+type WorkspaceID = id.WorkspaceID
+
+var NewID = id.NewAssetID
+var NewWorkspaceID = id.NewWorkspaceID
+
+var MustID = id.MustAssetID
+var MustWorkspaceID = id.MustWorkspaceID
+
+var IDFrom = id.AssetIDFrom
+var WorkspaceIDFrom = id.WorkspaceIDFrom
+
+var IDFromRef = id.AssetIDFromRef
+var WorkspaceIDFromRef = id.WorkspaceIDFromRef
+
+var ErrInvalidID = id.ErrInvalidID
