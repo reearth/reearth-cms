0d827124229b8610fd4a5e652d578bd4441b9fd4
rot1024 <aayhrot@gmail.com>
2022-06-24 12:25:44 +0900

    add meta struct

--- server/internal/infrastructure/mongo/mongogit/consumer.go
+++ server/internal/infrastructure/mongo/mongogit/consumer.go
@@ -6,23 +6,38 @@ import (
 )
 
 type Consumer interface {
-	ConsumeWithVersion(raw bson.Raw, version Version) error
+	ConsumeWithVersion(raw bson.Raw, meta Meta) error
 }
 
-type FuncConsumer func(data bson.Raw, version Version) error
+type FuncConsumer func(data bson.Raw, meta Meta) error
 
-func (s FuncConsumer) ConsumeWithVersion(raw bson.Raw, version Version) error {
-	return s(raw, versionFromRaw(raw))
+func (s FuncConsumer) ConsumeWithVersion(raw bson.Raw, meta Meta) error {
+	return s(raw, metaFromRaw(raw))
 }
 
-type SimpleConsumer[T any] func(data T, version Version) error
+type SimpleConsumer[T any] func(data T, meta Meta) error
 
-func (s SimpleConsumer[T]) ConsumeWithVersion(raw bson.Raw, version Version) error {
+func (s SimpleConsumer[T]) ConsumeWithVersion(raw bson.Raw, meta Meta) error {
 	var t T
 	if err := bson.Unmarshal(raw, &t); err != nil {
 		return err
 	}
-	return s(t, versionFromRaw(raw))
+	return s(t, metaFromRaw(raw))
+}
+
+type SliceConsumer[T any] struct {
+	Result []DataAndMeta[T]
+	c      SimpleConsumer[T]
+}
+
+func (s *SliceConsumer[T]) ConsumeWithVersion(raw bson.Raw, meta Meta) error {
+	if s.c == nil {
+		s.c = SimpleConsumer[T](func(data T, meta Meta) error {
+			s.Result = append(s.Result, DataAndMeta[T]{Data: data, Meta: meta})
+			return nil
+		})
+	}
+	return s.c.ConsumeWithVersion(raw, meta)
 }
 
 type consumerAdapter struct {
@@ -30,21 +45,32 @@ type consumerAdapter struct {
 }
 
 func (c *consumerAdapter) Consume(raw bson.Raw) error {
-	return c.c.ConsumeWithVersion(raw, versionFromRaw(raw))
+	return c.c.ConsumeWithVersion(raw, metaFromRaw(raw))
 }
 
 func consumer(c Consumer) mongodoc.Consumer {
 	return &consumerAdapter{c: c}
 }
 
-const versionkey = "__v"
+const (
+	versionKey         = "__v"
+	previousVersionKey = "__w"
+	refKey             = "__refs"
+)
 
-type versioned struct {
-	Version Version `json:"__v" bson:"__v"`
+type Meta struct {
+	Version         Version `json:"__v" bson:"__v"`
+	PreviousVersion Version `json:"__w" bson:"__w"`
+	Ref             []Ref   `json:"__refs" bson:"__refs"`
 }
 
-func versionFromRaw(r bson.Raw) Version {
-	v := versioned{}
-	_ = bson.Unmarshal(r, &v)
-	return v.Version
+func metaFromRaw(r bson.Raw) Meta {
+	m := Meta{}
+	_ = bson.Unmarshal(r, &m)
+	return m
+}
+
+type DataAndMeta[T any] struct {
+	Data T
+	Meta Meta
 }
--- server/internal/infrastructure/mongo/mongogit/consumer_test.go
+++ server/internal/infrastructure/mongo/mongogit/consumer_test.go
@@ -10,12 +10,21 @@ import (
 
 func TestFuncConsumer(t *testing.T) {
 	err := errors.New("aaa")
-	raw, _ := bson.Marshal(map[string]any{versionkey: Version("x"), "test": "hoge"})
+	raw, _ := bson.Marshal(map[string]any{
+		versionKey:         Version("x"),
+		previousVersionKey: Version("y"),
+		refKey:             []Ref{Ref("main")},
+		"test":             "hoge",
+	})
 
 	called := 0
-	c := FuncConsumer(func(r bson.Raw, v Version) error {
+	c := FuncConsumer(func(r bson.Raw, m Meta) error {
 		assert.Equal(t, bson.Raw(raw), r)
-		assert.Equal(t, Version("x"), v)
+		assert.Equal(t, Meta{
+			Version:         Version("x"),
+			PreviousVersion: Version("y"),
+			Ref:             []Ref{Ref("main")},
+		}, m)
 		called++
 		return err
 	})
@@ -26,12 +35,21 @@ func TestFuncConsumer(t *testing.T) {
 func TestSimpleConsumer(t *testing.T) {
 	type d struct{ Test string }
 	err := errors.New("aaa")
-	raw, _ := bson.Marshal(map[string]any{versionkey: Version("x"), "test": "hoge"})
+	raw, _ := bson.Marshal(map[string]any{
+		versionKey:         Version("x"),
+		previousVersionKey: Version("y"),
+		refKey:             []Ref{Ref("main")},
+		"test":             "hoge",
+	})
 
 	called := 0
-	c := SimpleConsumer[d](func(e d, v Version) error {
+	c := SimpleConsumer[d](func(e d, m Meta) error {
 		assert.Equal(t, d{Test: "hoge"}, e)
-		assert.Equal(t, Version("x"), v)
+		assert.Equal(t, Meta{
+			Version:         Version("x"),
+			PreviousVersion: Version("y"),
+			Ref:             []Ref{Ref("main")},
+		}, m)
 		called++
 		return err
 	})
@@ -39,7 +57,51 @@ func TestSimpleConsumer(t *testing.T) {
 	assert.Equal(t, 1, called)
 }
 
+func TestSliceConsumer(t *testing.T) {
+	type d struct{ Test string }
+	raw, _ := bson.Marshal(map[string]any{
+		versionKey:         Version("x"),
+		previousVersionKey: Version("y"),
+		refKey:             []Ref{Ref("main")},
+		"test":             "hoge",
+	})
+
+	c := &SliceConsumer[d]{}
+	assert.NoError(t, consumer(c).Consume(raw))
+	assert.Equal(t, []DataAndMeta[d]{
+		{
+			Data: d{
+				Test: "hoge",
+			},
+			Meta: Meta{
+				Version:         Version("x"),
+				PreviousVersion: Version("y"),
+				Ref:             []Ref{Ref("main")},
+			},
+		},
+	}, c.Result)
+}
+
 func TestVersionFromRaw(t *testing.T) {
-	raw, _ := bson.Marshal(map[string]any{versionkey: Version("x"), "test": "hoge"})
-	assert.Equal(t, Version("x"), versionFromRaw(raw))
+	raw, _ := bson.Marshal(map[string]any{
+		versionKey:         Version("x"),
+		previousVersionKey: Version("y"),
+		refKey:             []Ref{Ref("main")},
+		"test":             "hoge",
+	})
+	assert.Equal(t, Meta{
+		Version:         Version("x"),
+		PreviousVersion: Version("y"),
+		Ref:             []Ref{Ref("main")},
+	}, metaFromRaw(raw))
+
+	raw, _ = bson.Marshal(map[string]any{
+		versionKey: Version("x"),
+		"test":     "hoge",
+	})
+	assert.Equal(t, Meta{
+		Version:         Version("x"),
+		PreviousVersion: VersionZero,
+		Ref:             nil,
+	}, metaFromRaw(raw))
 }
