ce44853f535f038937680ad814f7e10b2f0b13dc
basel.issmail <basel.issmail@gmail.com>
2022-07-05 07:34:17 +0300

    Remove test pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,8 @@
 import { Provider as Auth0Provider } from "@reearth-cms/auth";
 import NotFound from "@reearth-cms/components/atoms/NotFound";
-import Account from "@reearth-cms/components/organisms/Settings/Account";
 import DashboardPage from "@reearth-cms/components/pages/Dashboard";
 import MembersPage from "@reearth-cms/components/pages/Members";
 import RootPage from "@reearth-cms/components/pages/RootPage";
-import WorkspacePage from "@reearth-cms/components/pages/Workspace";
-import WorkspaceListPage from "@reearth-cms/components/pages/WorkspaceList";
 import { Provider as GqlProvider } from "@reearth-cms/gql";
 import { Provider as I18nProvider } from "@reearth-cms/i18n";
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
@@ -18,9 +15,6 @@ function AppRoutes() {
     { path: "/dashboard", element: <DashboardPage /> },
     { path: "/dashboard/:workspaceId", element: <DashboardPage /> },
     { path: "/workspaces/:workspaceId/members", element: <MembersPage /> },
-    { path: "/workspaces/:workspaceId", element: <WorkspacePage /> },
-    { path: "/workspaces", element: <WorkspaceListPage /> },
-    { path: "/account", element: <Account /> },
     { path: "*", element: <NotFound /> },
   ]);
   return routes;
