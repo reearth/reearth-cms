7fe5385bc27c828723412884ca43a79f16421d7b
nourbalaha <nourbalaha0@gmail.com>
2022-07-20 17:29:02 +0900

    test: added TestAsset_CreatedAt

--- server/pkg/asset/asset_test.go
+++ server/pkg/asset/asset_test.go
@@ -43,3 +43,25 @@ func TestAsset_AssetType(t *testing.T) {
 	assert.Equal(t, wantPreviewType, got.PreviewType())
 	assert.Equal(t, "yyy", got.Hash())
 }
+
+func TestAsset_CreatedAt(t *testing.T) {
+    aid := NewID()
+    pid := NewProjectID()
+    uid := NewUserID()
+	u := user.New().ID(uid).Email("test@test.com").MustBuild()
+	p := project.New().ID(pid).MustBuild()
+    var size uint64 = 15
+	
+    got := Asset{
+		id:          aid,
+		project:     p,
+		projectId:   pid,
+		createdBy:   u,
+		createdById: uid,
+		fileName:    "hoge",
+		previewType: PreviewTypeFromRef(getStrRef(PreviewTypeIMAGE.String())),
+		size:        size,
+		hash:        "yyy",
+    }
+    assert.Equal(t, time.Time{}, got.CreatedAt())
+}
