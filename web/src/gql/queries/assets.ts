bc1cdd697f28e731dd07f1930792e64a5aec3804
nourbalaha <nourbalaha0@gmail.com>
2022-07-25 13:38:01 +0900

    feat: added asset gql schema

---
+++ web/src/gql/queries/assets.ts
@@ -0,0 +1,72 @@
+import { gql } from "@apollo/client";
+
+export const GET_ASSETS = gql`
+  query GetAssets(
+    $projectId: ID!
+    $keyword: String
+    $sort: AssetSortType
+    $pagination: Pagination
+  ) {
+    assets(
+      projectId: $projectId
+      keyword: $keyword
+      sort: $sort
+      pagination: $pagination
+    ) {
+      nodes {
+        id
+        projectId
+        createdAt
+        createdById
+        fileName
+        size
+        previewType
+        file {
+          name
+          size
+          contentType
+          path
+        }
+        hash
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      totalCount
+    }
+  }
+`;
+
+export const CREATE_ASSET = gql`
+  mutation CreateAsset($projectId: ID!, $file: Upload!) {
+    createAsset(input: { projectId: $projectId, file: $file }) {
+      asset {
+        id
+        projectId
+        createdAt
+        createdById
+        fileName
+        size
+        previewType
+        file {
+          name
+          size
+          contentType
+          path
+        }
+        hash
+      }
+    }
+  }
+`;
+
+export const DELETE_ASSET = gql`
+  mutation DeleteAsset($assetId: ID!) {
+    deleteAsset(input: { assetId: $assetId }) {
+      assetId
+    }
+  }
+`;
