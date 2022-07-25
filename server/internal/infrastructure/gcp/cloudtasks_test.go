ae086214b25ece85eaf1f30267f337d26ea0454d
HideBa <baba.papa1120.ba@gmail.com>
2022-07-25 14:44:13 +0900

    impl sample test

--- server/internal/infrastructure/gcp/cloudtasks_test.go
+++ server/internal/infrastructure/gcp/cloudtasks_test.go
@@ -5,27 +5,20 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/reearth/reearth-cms/server/pkg/id"
-	"github.com/reearth/reearth-cms/server/pkg/id/idx"
 	"github.com/reearth/reearth-cms/server/pkg/task"
-	"gopkg.in/h2non/gock.v1"
 )
 
 func TestTaskRunner_Run(t *testing.T) {
-	t.Cleanup(func() {
-		gock.EnableNetworking()
-		gock.OffAll()
-	})
-	gock.DisableNetworking()
-
 	queuePath := fmt.Sprintf("projects/%s/locations/%s/queues/%s", "reearth-oss", "asia-northeast1", "test")
-	credFilePath := "./gcp-credential.json"
+	// credFilePath := "./gcp-credential.json"
 	subscriberURL := "https://example.com/task_handler"
 	payload := task.Payload{
 		DecompressAsset: task.NewDecompressAssetPayloadForTest("test"),
 	}
 	type fields struct {
-		queuePath string
+		queuePath     string
+		subscriberURL string
+		credFilePath  string
 	}
 	type args struct {
 		ctx context.Context
@@ -36,7 +29,6 @@ func TestTaskRunner_Run(t *testing.T) {
 		setup   func()
 		fields  fields
 		args    args
-		want    id.TaskID
 		wantErr bool
 	}{
 		{
@@ -45,27 +37,27 @@ func TestTaskRunner_Run(t *testing.T) {
 			// 	gock.New()
 			// },
 			fields: fields{
-				queuePath: queuePath,
+				queuePath:     queuePath,
+				subscriberURL: subscriberURL,
 			},
 			args: args{
 				ctx: context.Background(),
 				p:   payload,
 			},
-			want:    idx.ID[id.Task]{},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tr := &TaskRunner{
-				queuePath: tt.fields.queuePath,
+				queuePath:     tt.fields.queuePath,
+				subscriberURL: tt.fields.subscriberURL,
 			}
-			got, err := tr.Run(tt.args.ctx, tt.args.p)
+			_, err := tr.Run(tt.args.ctx, tt.args.p)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("TaskRunner.Run() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			fmt.Printf("got-----%v", got)
 			// if !reflect.DeepEqual(got, tt.want) {
 			// 	t.Errorf("TaskRunner.Run() = %v, want %v", got, tt.want)
 			// }
