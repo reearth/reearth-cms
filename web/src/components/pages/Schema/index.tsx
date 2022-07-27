21f44524dccc549ba3a130b39e42c0f669ea423f
basel.issmail <basel.issmail@gmail.com>
2022-07-27 07:57:47 +0300

    Add schema page

---
+++ web/src/components/pages/Schema/index.tsx
@@ -0,0 +1,7 @@
+import { AuthenticationRequiredPage } from "@reearth-cms/auth";
+
+const ProjectPage: React.FC = () => {
+  return <AuthenticationRequiredPage></AuthenticationRequiredPage>;
+};
+
+export default ProjectPage;
