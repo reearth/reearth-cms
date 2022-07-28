3a810cf021cf950336f59d8896a187bf2bf54020
nourbalaha <nourbalaha0@gmail.com>
2022-07-28 13:35:44 +0900

    wip: fix TestFindByID errors

--- server/internal/infrastructure/mongo/asset_test.go
+++ server/internal/infrastructure/mongo/asset_test.go
@@ -3,26 +3,19 @@ package mongo
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/reearth/reearth-cms/server/pkg/asset"
 	"github.com/reearth/reearth-cms/server/pkg/id"
-	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFindByID(t *testing.T) {
-	f := &asset.File{}
-
 	expected := asset.New().
 		NewID().
 		Project(id.NewProjectID()).
 		CreatedBy(id.NewUserID()).
-		CreatedAt(time.Now()).
 		FileName("name").
 		Size(10).
-		Type(lo.ToPtr(asset.PreviewTypeGEO)).
-		File(f).
 		Hash("https://reearth.io/").
 		MustBuild()
 
