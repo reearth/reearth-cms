f8258ca162dd1a6e2bc0c2ab8f28c070aff56caf
nourbalaha <nourbalaha0@gmail.com>
2022-08-01 22:03:50 +0900

    feat: added utils

--- web/src/utils/format.ts
+++ web/src/utils/format.ts
@@ -3,3 +3,15 @@ import moment from "moment";
 export const dateTimeFormat = (date: Date, format = "YYYY-MM-DD hh:mm") => {
   return `${moment(date).format(format)}`;
 };
+
+export const bytesFormat = (bytes: number, decimals = 2) => {
+  if (bytes === 0) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+};
