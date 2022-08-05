30414b40daed304338edfa488d0ab617709686a2
maherhamoui6 <hammimo022@gmail.com>
2022-08-05 03:13:49 +0300

    version init

--- server/pkg/item/version.go
+++ server/pkg/item/version.go
@@ -9,6 +9,36 @@ type Version struct {
 	fields  []FieldID
 }
 
+func NewVersion(version *string, parent, ref []string, fields []FieldID) *Version {
+	if version == nil {
+		return nil
+	}
+	return &Version{
+		version: *version,
+		parent:  parent,
+		ref:     ref,
+		fields:  fields,
+	}
+}
+
+func (v *Version) AddRef(ref *string) {
+	if ref == nil {
+		return
+	}
+	v.ref = append(v.ref, *ref)
+}
+
+func (v *Version) AddParent(parent *string) {
+	if parent == nil {
+		return
+	}
+	v.parent = append(v.parent, *parent)
+}
+
+func (v *Version) AddField(field FieldID) {
+	v.fields = append(v.fields, field)
+}
+
 func (v *Version) Version() string {
 	return v.version
 }
