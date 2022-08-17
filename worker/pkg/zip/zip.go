8fffd4221dd5769491d053b681396483bfddf7cd
HideBa <baba.papa1120.ba@gmail.com>
2022-08-17 11:24:56 +0900

    delete

--- worker/pkg/zip/zip.go
+++ worker/pkg/zip/zip.go
@@ -3,43 +3,7 @@ package zip
 import (
 	"archive/zip"
 	"io"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 )
-
-func Unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	for _, f := range r.File {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-
-		if f.FileInfo().IsDir() {
-			path := filepath.Join(dest, f.Name)
-			_ = os.MkdirAll(path, f.Mode())
-		} else {
-			buf := make([]byte, f.UncompressedSize)
-			_, err = io.ReadFull(rc, buf)
-			if err != nil {
-				return err
-			}
-
-			path := filepath.Join(dest, f.Name)
-			if err = ioutil.WriteFile(path, buf, f.Mode()); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
 
 type Unzipper struct {
 	r   *zip.Reader
