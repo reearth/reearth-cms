7d89dd7e441ea44024439bc38551c9d30d936c25
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-08-24 11:33:47 +0300

    Revert "test(internal): add test to memory"
    This reverts commit 403f99b8b2255a8ad454e5d1f166f1d54a52bab7.

--- server/internal/infrastructure/memory/model_test.go
+++ server/internal/infrastructure/memory/model_test.go
@@ -1,17 +1,12 @@
 package memory
 
 import (
-	"context"
 	"testing"
 
-	"github.com/reearth/reearth-cms/server/internal/usecase"
-	"github.com/reearth/reearth-cms/server/internal/usecase/repo"
 	"github.com/reearth/reearth-cms/server/pkg/id"
 	"github.com/reearth/reearth-cms/server/pkg/model"
-	"github.com/reearth/reearthx/util"
-
-	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
+	"github.com/reearth/reearthx/util"
 )
 
 func TestNewModel(t *testing.T) {
@@ -22,43 +17,4 @@ func TestNewModel(t *testing.T) {
 
 	got := NewModel()
 	assert.Equal(t, expected, got)
-}
-
-func TestMemory_Filtered(t *testing.T) {
-	r := &Model{
-		data: &util.SyncMap[id.ModelID, *model.Model]{},
-		f:    repo.WorkspaceFilter{},
-	}
-	expected := &Model{
-		data: &util.SyncMap[id.ModelID, *model.Model]{},
-		f:    r.f.Merge(r.f),
-		now:  &util.TimeNow{},
-	}
-
-	got := r.Filtered(r.f)
-	assert.Equal(t, expected, got)
-}
-
-func TestMemory_FindByProject(t *testing.T) {
-	ctx := context.Background()
-
-	r := &Model{
-		data: &util.SyncMap[id.ModelID, *model.Model]{},
-		f:    repo.WorkspaceFilter{},
-	}
-	modelList := model.List{}
-
-	pageInfo := usecase.NewPageInfo(
-		0,
-		lo.ToPtr(usecase.Cursor("")),
-		lo.ToPtr(usecase.Cursor("")),
-		true,
-		true,
-	)
-	var arg *usecase.Cursor
-	resModelList, respageInfo, err := r.FindByProject(ctx, id.NewProjectID(), usecase.NewPagination(lo.ToPtr(1), lo.ToPtr(2), arg, arg))
-
-	assert.NoError(t, err)
-	assert.Equal(t, modelList, resModelList)
-	assert.Equal(t, pageInfo, respageInfo)
 }
