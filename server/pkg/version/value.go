cdf1ef3be8d4c1a425ba853ce55feba4374318ad
rot1024 <aayhrot@gmail.com>
2022-08-25 19:55:18 +0900

    feat(server): version controlled MongoDB docs (#94)
    * impl version
    
    * impl consumer
    
    * refactor
    
    * add default
    
    * add meta struct
    
    * fix version query
    
    * add util.Try
    
    * add mongotest package
    
    * fix
    
    * impl memorygit wip
    
    * fix errors
    
    * impl load
    
    * add util.Try
    
    * implement memory and inner (updateRef left)
    
    * load & loadAll testing
    
    * impl UpdateRef
    
    * impl Store
    
    * refactor
    
    * impl version pkg and memory
    
    * use uuid
    
    * impl archived
    
    * allow multiple parents
    
    * Add accepts VersionOrRef
    
    * NewValue
    
    * refactor
    
    * add types from reearthx
    
    * use go.testEnvFile
    
    * impl collection
    
    * refactor
    
    * imp FindOne test
    
    * test Find and Paginate
    
    * impl all query
    
    * fix
    
    * add count
    
    * fix test
    
    Co-authored-by: maherhamoui6 <hammimo022@gmail.com>

---
+++ server/pkg/version/value.go
@@ -0,0 +1,89 @@
+package version
+
+type Value[T any] struct {
+	version Version
+	parents Versions
+	refs    Refs
+	value   T
+}
+
+func NewValue[T any](version Version, parents Versions, refs Refs, value T) *Value[T] {
+	res := &Value[T]{
+		version: version,
+		parents: parents,
+		refs:    refs,
+		value:   value,
+	}
+	if !res.validate() {
+		return nil
+	}
+	if parents != nil && parents.Len() > 0 {
+		res.parents = parents.Clone()
+	}
+	if refs != nil && refs.Len() > 0 {
+		res.refs = refs.Clone()
+	}
+	return res
+}
+
+func MustBeValue[T any](version Version, parents Versions, refs Refs, value T) *Value[T] {
+	v := NewValue(version, parents, refs, value)
+	if v == nil {
+		panic("invalid version or parents")
+	}
+	return v
+}
+
+func (v Value[T]) Version() Version {
+	return v.version
+}
+
+func (v Value[T]) Parents() Versions {
+	if v.parents == nil {
+		return Versions{}
+	}
+	return v.parents.Clone()
+}
+
+func (v Value[T]) Refs() Refs {
+	if v.refs == nil {
+		return Refs{}
+	}
+	return v.refs.Clone()
+}
+
+func (v Value[T]) Value() T {
+	return v.value
+}
+
+func (v Value[T]) Ref() *Value[T] {
+	return &v
+}
+
+func (v *Value[T]) Clone() *Value[T] {
+	if v == nil {
+		return nil
+	}
+	return NewValue(v.version, v.parents, v.refs, v.value)
+}
+
+func (v *Value[T]) AddRefs(refs ...Ref) {
+	if v.refs == nil {
+		v.refs = Refs{}
+	}
+	v.refs.Add(refs...)
+}
+
+func (v *Value[T]) DeleteRefs(refs ...Ref) {
+	if v.refs == nil {
+		return
+	}
+	v.refs.Delete(refs...)
+	if v.refs.Len() == 0 {
+		v.refs = nil
+	}
+}
+
+func (v *Value[T]) validate() bool {
+	return !v.version.IsZero() && !v.Parents().Has(v.version)
+}
