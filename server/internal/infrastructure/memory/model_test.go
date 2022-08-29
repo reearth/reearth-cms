661cc4054c5ace1b8e8de6a26a7963f566ed5810
Go nakagawa <nakagawago0614@gmail.com>
2022-08-29 17:08:52 +0900

    test(internal): add test to memory

--- server/internal/infrastructure/memory/model_test.go
+++ server/internal/infrastructure/memory/model_test.go
@@ -7,8 +7,9 @@ import (
 	"github.com/reearth/reearth-cms/server/internal/usecase"
 	"github.com/reearth/reearth-cms/server/internal/usecase/repo"
 	"github.com/reearth/reearth-cms/server/pkg/id"
-	"github.com/reearth/reearth-cms/server/pkg/key"
+	// "github.com/reearth/reearth-cms/server/pkg/key"
 	"github.com/reearth/reearth-cms/server/pkg/model"
+	"github.com/reearth/reearthx/rerror"
 	"github.com/reearth/reearthx/util"
 
 	"github.com/samber/lo"
@@ -49,9 +50,10 @@ func TestMemory_FindByProject(t *testing.T) {
 		f:    repo.WorkspaceFilter{},
 	}
 
-	modelList := model.List{
-		model.New().NewID().Key(key.Random()).Project(pId).Schema(id.NewSchemaID()).MustBuild(),
-	}
+	modelList := model.List(r.data.FindAll(func(_ id.ModelID, m *model.Model) bool {
+		return m.Project().Equal(pId)
+	})).SortByID()
+
 	pageInfo := usecase.NewPageInfo(
 		0,
 		lo.ToPtr(usecase.Cursor("")),
@@ -65,4 +67,37 @@ func TestMemory_FindByProject(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, modelList, resModelList)
 	assert.Equal(t, pageInfo, respageInfo)
+}
+
+func TestMemory_CountByProject(t *testing.T) {
+	ctx := context.Background()
+	pId := id.NewProjectID()
+
+	r := &Model{
+		data: &util.SyncMap[id.ModelID, *model.Model]{},
+		f:    repo.WorkspaceFilter{},
+	}
+
+	res, err := r.CountByProject(ctx, pId)
+
+	assert.NoError(t, err)
+	assert.Equal(t, res, 0)
+}
+
+func TestMemory_FindByKey(t *testing.T) {
+	ctx := context.Background()
+	pId := id.NewProjectID()
+	key := "id"
+	r := &Model{
+		data: &util.SyncMap[id.ModelID, *model.Model]{},
+		f:    repo.WorkspaceFilter{},
+	}
+	m := r.data.Find(func(_ id.ModelID, m *model.Model) bool {
+		return m.Key().String() == key && m.Project().Equal(pId)
+	})
+
+	res, err := r.FindByKey(ctx, pId, key)
+
+	assert.ErrorIs(t, err, rerror.ErrNotFound)
+	assert.Equal(t, m, res)
 }
