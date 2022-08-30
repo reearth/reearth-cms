ae0c33d836654048c896c917d3ed105b57bb3a3b
basel.issmail <basel.issmail@gmail.com>
2022-08-30 07:48:12 +0300

    save

---
+++ web/src/components/organisms/Project/Schema/hooks.ts
@@ -0,0 +1,207 @@
+import { useCallback, useMemo, useState } from "react";
+
+import {
+  useGetModelsQuery,
+  useCreateModelMutation,
+  useCreateFieldMutation,
+  SchemaFiledType,
+  SchemaFieldTypePropertyInput,
+} from "@reearth-cms/gql/graphql-client-api";
+
+type Model = {
+  id: string;
+  name: string;
+  description?: string;
+  key: string;
+  schema: Schema;
+};
+
+type Schema = {
+  id: string;
+  fields: Field[];
+};
+
+type Field = {
+  id: string;
+  type: FieldType;
+  title: string;
+  description: string | null | undefined;
+  required: boolean;
+  unique: boolean;
+};
+
+export type FieldType =
+  | "Text"
+  | "TextArea"
+  | "RichText"
+  | "MarkdownText"
+  | "Asset"
+  | "Date"
+  | "Bool"
+  | "Select"
+  | "Tag"
+  | "Integer"
+  | "Reference"
+  | "URL";
+
+type Params = {
+  projectId?: string;
+  modelId?: string;
+};
+
+export default ({ projectId, modelId }: Params) => {
+  const [modelModalShown, setModelModalShown] = useState(false);
+  const [fieldModalShown, setFieldModalShown] = useState(false);
+
+  const { data, refetch } = useGetModelsQuery({
+    variables: { projectId: projectId ?? "", first: 100 },
+    skip: !projectId,
+  });
+
+  const models = useMemo(() => {
+    return (data?.models.nodes ?? [])
+      .map<Model | undefined>(model =>
+        model
+          ? {
+              id: model.id,
+              description: model.description,
+              name: model.name,
+              key: model.key,
+              schema: {
+                id: model.schema.id,
+                fields: model.schema.fields.map(field => ({
+                  id: field.id,
+                  description: field.description,
+                  title: field.title,
+                  type: field.type,
+                  unique: field.unique,
+                  required: field.required,
+                })),
+              },
+            }
+          : undefined,
+      )
+      .filter((model): model is Model => !!model);
+  }, [data?.models.nodes]);
+
+  const rawModel = useMemo(
+    () => data?.models.nodes.find((p: any) => p?.id === modelId),
+    [data, modelId],
+  );
+  const model = useMemo(
+    () =>
+      rawModel?.id
+        ? {
+            id: rawModel.id,
+            description: rawModel.description,
+            name: rawModel.name,
+            key: rawModel.key,
+            schema: {
+              id: rawModel.schema.id,
+              fields: rawModel.schema.fields.map(field => ({
+                id: field.id,
+                description: field.description,
+                title: field.title,
+                type: field.type,
+                unique: field.unique,
+                required: field.required,
+              })),
+            },
+          }
+        : undefined,
+    [rawModel],
+  );
+
+  const [createNewModel] = useCreateModelMutation({
+    refetchQueries: ["GetModels"],
+  });
+
+  const handleModelCreate = useCallback(
+    async (data: { name: string; description: string; key: string }) => {
+      if (!projectId) return;
+      const model = await createNewModel({
+        variables: {
+          projectId,
+          name: data.name,
+          description: data.description,
+          key: data.key,
+        },
+      });
+      if (model.errors || !model.data?.createModel) {
+        setModelModalShown(false);
+        return;
+      }
+
+      setModelModalShown(false);
+      refetch();
+    },
+    [createNewModel, projectId, refetch],
+  );
+
+  const [createNewField] = useCreateFieldMutation({
+    refetchQueries: ["GetModels"],
+  });
+
+  const handleFieldCreate = useCallback(
+    async (data: {
+      title: string;
+      description: string;
+      key: string;
+      multiValue: boolean;
+      unique: boolean;
+      required: boolean;
+      type: SchemaFiledType;
+      typeProperty: SchemaFieldTypePropertyInput;
+    }) => {
+      if (!modelId) return;
+      const field = await createNewField({
+        variables: {
+          modelId,
+          title: data.title,
+          description: data.description,
+          key: data.key,
+          multiValue: data.multiValue,
+          unique: data.unique,
+          required: data.required,
+          type: data.type,
+          typeProperty: data.typeProperty,
+        },
+      });
+      if (field.errors || !field.data?.createField) {
+        setModelModalShown(false);
+        return;
+      }
+
+      setModelModalShown(false);
+      refetch();
+    },
+    [createNewModel, projectId, refetch, modelId],
+  );
+
+  const handleModelModalClose = useCallback(() => {
+    setModelModalShown(false);
+  }, []);
+
+  const handleModelModalOpen = useCallback(() => setModelModalShown(true), []);
+
+  const handleFieldModalClose = useCallback(() => {
+    setFieldModalShown(false);
+  }, []);
+
+  const handleFieldModalOpen = useCallback(() => {
+    if (modelId) setFieldModalShown(true);
+  }, [modelId]);
+
+  return {
+    model,
+    models,
+    modelModalShown,
+    handleModelModalOpen,
+    handleModelModalClose,
+    handleModelCreate,
+    fieldModalShown,
+    handleFieldModalOpen,
+    handleFieldModalClose,
+    handleFieldCreate,
+  };
+};
