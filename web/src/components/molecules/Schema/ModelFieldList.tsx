8cd8f10e51c6caf84e095ef86e1fc706f90196ed
basel.issmail <basel.issmail@gmail.com>
2022-08-30 08:59:16 +0300

    Add field types

---
+++ web/src/components/molecules/Schema/ModelFieldList.tsx
@@ -0,0 +1,92 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+import Icon from "@reearth-cms/components/atoms/Icon";
+import List from "@reearth-cms/components/atoms/List";
+
+import { fieldTypes } from "./fieldTypes";
+import { Field } from "./types";
+
+export interface Props {
+  className?: string;
+  fields?: Field[];
+}
+
+const ModelFieldList: React.FC<Props> = ({ fields }) => {
+  return (
+    <>
+      <FieldStyledList
+        itemLayout="horizontal"
+        dataSource={fields}
+        renderItem={item => (
+          <>
+            <List.Item extra={<Icon icon="more" style={{ fontSize: "22px" }} />}>
+              <List.Item.Meta
+                avatar={
+                  <FieldThumbnail>
+                    <StyledIcon
+                      icon={fieldTypes[item?.type].icon}
+                      color={fieldTypes[item?.type].color}
+                    />
+                    <h3>{item?.type}</h3>
+                  </FieldThumbnail>
+                }
+                title={`${item?.title}${item.required ? " *" : ""}`}
+              />
+            </List.Item>
+          </>
+        )}
+      />
+    </>
+  );
+};
+
+const StyledIcon = styled(Icon)`
+  border: 1px solid #f0f0f0;
+  width: 28px;
+  height: 28px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FieldThumbnail = styled.div`
+  display: flex;
+  align-items: center;
+  h3 {
+    margin: 0;
+    margin-left: 12px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    color: rgba(0, 0, 0, 0.45);
+  }
+`;
+
+const FieldStyledList = styled(List)`
+  padding-top: 24px;
+  .ant-list-item {
+    background-color: #fff;
+    cursor: pointer;
+    + .ant-list-item {
+      margin-top: 12px;
+    }
+    padding: 24px;
+    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.15);
+    .ant-list-item-meta {
+      .ant-list-item-meta-content {
+        text-align: center;
+        margin: auto;
+      }
+      .ant-list-item-meta-title {
+        margin: 0;
+      }
+      align-items: center;
+      .ant-list-item-meta-avatar {
+        min-width: 130px;
+      }
+    }
+  }
+`;
+
+export default ModelFieldList;
---
+++ web/src/components/molecules/Schema/fieldTypes.ts
@@ -0,0 +1,44 @@
+export const fieldTypes = {
+  Text: {
+    icon: "textT",
+    title: "Text",
+    description: "Heading and titles, one-line field",
+    color: "#FF7875",
+  },
+  TextArea: {
+    icon: "textAlignLeft",
+    title: "TextArea",
+    description: "Multi line text",
+    color: "#FF7875",
+  },
+  MarkdownText: {
+    icon: "markDown",
+    title: "Markdown text",
+    description: "Rich text which supports md style",
+    color: "#FF7875",
+  },
+  Asset: {
+    icon: "asset",
+    title: "Asset",
+    description: "Description",
+    color: "#FF9C6E",
+  },
+  Select: {
+    icon: "listBullets",
+    title: "Option",
+    description: "Description",
+    color: "#7CB305",
+  },
+  Integer: {
+    icon: "numberNine",
+    title: "Int",
+    description: "Description",
+    color: "#36CFC9",
+  },
+  URL: {
+    icon: "link",
+    title: "URL",
+    description: "Description",
+    color: "#9254DE",
+  },
+};
