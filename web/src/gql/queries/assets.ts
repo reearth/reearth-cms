9da1975a9a8bbb01cc34f861ae17bed0f1fd9c6f
Nour Balaha <nourbalaha0@gmail.com>
2022-09-08 10:51:35 +0900

    feat(web): asset ui organisms and pages (#152)
    * feat: added utils
    
    * added user to auth hook
    
    * fix: atoms and molecules
    
    * wip: added organisms and pagegs for asset
    
    * fix: fixed ci warnings
    
    * fixed the linting issues
    
    * feat: added createdById
    
    * feat: added useNotification to state
    
    * feat: added edges to assets query
    
    * wip: upload asset modal
    
    * temporary changes for acceptance test
    
    * wip: asset url
    
    * [FE]: getAsset and updateAsset
    
    * [FE]: wip hooks
    
    * fix: removed use-file-input
    
    * feat: added uploadLink to gql provider
    
    * [FE]: implemented getAsset and updaetAasset hooks
    
    * [FE]: downgraded cesium
    
    * [FE]: enabled TilesetPreview
    
    * [FE]: added currentUser to hooks
    
    [FE]: added currentUser to hooks
    
    * [FE]: fixed styles for preview asset
    
    * [FE]: fixed upload file
    
    * [FE]: fixed download asset
    
    * [FE]: wip table pagination
    
    * feat: access asset from project page
    
    * [FE]: hash to uuid
    
    * uuidToURL
    
    * refactor: upload modal component
    
    * refactor: asset page loading flow
    
    * fix: DownloadButton using js-file-download
    
    * fix: proTable imports and exports
    
    * fix: merged with main
    
    * fix: merge fixes
    
    * merge fixes 2
    
    * fix: page imports
    
    * [FE]: updated uuidToURL method
    
    * wip: uuidToURL
    
    * wip: gql client api
    
    * fix: update asset notification messages
    
    * wip: fixing uuidToURL
    
    * refactor + cleanup
    
    * wip: pr change requests fix
    
    * removed alerts
    
    * wip: pr change request fixes 2
    
    * fix: sort callbacks proper naming

--- web/src/gql/queries/assets.ts
+++ web/src/gql/queries/assets.ts
@@ -8,6 +8,25 @@ export const GET_ASSETS = gql`
     $pagination: Pagination
   ) {
     assets(projectId: $projectId, keyword: $keyword, sort: $sort, pagination: $pagination) {
+      edges {
+        cursor
+        node {
+          id
+          projectId
+          createdAt
+          createdById
+          fileName
+          size
+          previewType
+          file {
+            name
+            size
+            contentType
+            path
+          }
+          uuid
+        }
+      }
       nodes {
         id
         projectId
@@ -22,22 +41,43 @@ export const GET_ASSETS = gql`
           contentType
           path
         }
-        hash
+        uuid
       }
       pageInfo {
+        startCursor
         endCursor
         hasNextPage
         hasPreviousPage
-        startCursor
       }
       totalCount
     }
   }
 `;
 
+export const GET_ASSET = gql`
+  query GetAsset($assetId: ID!) {
+    asset(assetId: $assetId) {
+      id
+      projectId
+      createdAt
+      createdById
+      fileName
+      size
+      previewType
+      file {
+        name
+        size
+        contentType
+        path
+      }
+      uuid
+    }
+  }
+`;
+
 export const CREATE_ASSET = gql`
-  mutation CreateAsset($projectId: ID!, $file: Upload!) {
-    createAsset(input: { projectId: $projectId, file: $file }) {
+  mutation CreateAsset($projectId: ID!, $createdById: ID!, $file: Upload!) {
+    createAsset(input: { projectId: $projectId, createdById: $createdById, file: $file }) {
       asset {
         id
         projectId
@@ -52,7 +92,30 @@ export const CREATE_ASSET = gql`
           contentType
           path
         }
-        hash
+        uuid
+      }
+    }
+  }
+`;
+
+export const UPDATE_ASSET = gql`
+  mutation UpdateAsset($id: ID!, $previewType: PreviewType) {
+    updateAsset(input: { id: $id, previewType: $previewType }) {
+      asset {
+        id
+        projectId
+        createdAt
+        createdById
+        fileName
+        size
+        previewType
+        file {
+          name
+          size
+          contentType
+          path
+        }
+        uuid
       }
     }
   }
