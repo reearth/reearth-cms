1102db4f5d41d6ad63ca34a687685b30c251d001
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-09-08 21:25:52 +0300

    feat(server): integration domain models (#212)
    * start with base draft
    
    * refactor code
    
    * refactor code
    
    * add integration builder
    
    * add webhook builder
    
    * adding tests
    
    * add test

---
+++ server/pkg/integration/id.go
@@ -0,0 +1,16 @@
+package integration
+
+import (
+	"github.com/reearth/reearth-cms/server/pkg/id"
+)
+
+type ID = id.IntegrationID
+type WebhookID = id.WebhookID
+type UserID = id.UserID
+type ModelID = id.ModelID
+
+var NewID = id.NewIntegrationID
+var MustID = id.MustIntegrationID
+var IDFrom = id.IntegrationIDFrom
+var IDFromRef = id.IntegrationIDFromRef
+var ErrInvalidID = id.ErrInvalidID
