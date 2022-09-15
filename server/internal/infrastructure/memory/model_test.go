6c6b433c560d258a08c8d625a1bdb0f746617b5f
Go nakagawa <nakagawago0614@gmail.com>
2022-09-15 14:34:16 +0900

    test(internal): improve coverage of test

--- server/internal/infrastructure/memory/model_test.go
+++ server/internal/infrastructure/memory/model_test.go
@@ -121,6 +121,8 @@ func TestMemory_FindByKey(t *testing.T) {
 	expected := r.data.Find(func(_ id.ModelID, m *model.Model) bool {
 		return m.Key().String() == key && m.Project() == pId
 	})
+	// pid, key is bad, so I'm getting nil back...
+	// Also, r.data.store is not working properly.
 	got, err := r.FindByKey(ctx, pId, key)
 	assert.ErrorIs(t, err, rerror.ErrNotFound)
 	assert.Equal(t, expected, got)
@@ -200,15 +202,40 @@ func TestMemory_Save(t *testing.T) {
 	r.data.Store(id.NewModelID(), m)
 	err := r.Save(ctx, m)
 	assert.NoError(t, err)
+
+	err = errors.New("test")
+	r = &Model{
+		data: &util.SyncMap[id.ModelID, *model.Model]{},
+		f:    repo.WorkspaceFilter{},
+		err:  err,
+	}
+	gotErr := r.Save(ctx, m)
+	assert.Equal(t, gotErr, err)
 }
 
 func TestMemory_Remove(t *testing.T) {
 	ctx := context.Background()
 	m := &model.Model{}
+	pId := id.NewProjectID()
+	mId := id.NewModelID()
 	r := &Model{
 		data: &util.SyncMap[id.ModelID, *model.Model]{},
 		f:    repo.WorkspaceFilter{},
 	}
-	err := r.Remove(ctx, m.ID())
-	assert.ErrorIs(t, err, rerror.ErrNotFound)
+	r.data.Store(mId, model.New().NewID().Schema(id.NewSchemaID()).RandomKey().Project(pId).MustBuild())
+
+	gotErr := r.Remove(ctx, m.ID())
+	assert.ErrorIs(t, gotErr, rerror.ErrNotFound)
+
+	got := r.Remove(ctx, mId)
+	assert.Nil(t, got)
+
+	err := errors.New("test")
+	r = &Model{
+		data: &util.SyncMap[id.ModelID, *model.Model]{},
+		f:    repo.WorkspaceFilter{},
+		err:  err,
+	}
+	gotErr = r.Remove(ctx, m.ID())
+	assert.Equal(t, gotErr, err)
 }
