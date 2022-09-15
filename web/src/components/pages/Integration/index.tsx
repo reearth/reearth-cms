3d0079a3daa15d6d143774463bcd1a0a7cb0fc3b
basel.issmail <basel.issmail@gmail.com>
2022-09-15 08:42:13 +0300

    Add page component

---
+++ web/src/components/pages/Integration/index.tsx
@@ -0,0 +1,7 @@
+import { AuthenticationRequiredPage } from "@reearth-cms/auth";
+
+const SchemaPage: React.FC = () => {
+  return <AuthenticationRequiredPage></AuthenticationRequiredPage>;
+};
+
+export default SchemaPage;
