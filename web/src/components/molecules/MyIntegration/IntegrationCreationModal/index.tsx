79018d346c3fcd4348197627f6d344c03496f29b
basel.issmail <basel.issmail@gmail.com>
2022-09-17 16:54:05 +0300

    Add integration creation modal

--- web/src/components/molecules/MyIntegration/IntegrationCreationModal/index.tsx
+++ web/src/components/molecules/MyIntegration/IntegrationCreationModal/index.tsx
@@ -1,67 +1,57 @@
-import React, { useCallback } from "react";
+import React from "react";
 
 import Button from "@reearth-cms/components/atoms/Button";
 import Form from "@reearth-cms/components/atoms/Form";
+import Icon from "@reearth-cms/components/atoms/Icon";
 import Input from "@reearth-cms/components/atoms/Input";
 import Modal from "@reearth-cms/components/atoms/Modal";
+import TextArea from "@reearth-cms/components/atoms/TextArea";
+import Upload from "@reearth-cms/components/atoms/Upload";
 import { useT } from "@reearth-cms/i18n";
-
-export interface FormValues {
-  name: string;
-}
 
 export interface Props {
   open?: boolean;
-  onClose?: (refetch?: boolean) => void;
-  onSubmit?: (values: FormValues) => Promise<void> | void;
+  onClose?: () => void;
+  onSubmit?: () => Promise<void> | void;
 }
-
-const initialValues: FormValues = {
-  name: "",
-};
 
 const IntegrationCreationModal: React.FC<Props> = ({ open, onClose, onSubmit }) => {
   const t = useT();
   const [form] = Form.useForm();
 
-  const handleSubmit = useCallback(() => {
-    form
-      .validateFields()
-      .then(async values => {
-        await onSubmit?.(values);
-        onClose?.(true);
-        form.resetFields();
-      })
-      .catch(info => {
-        console.log("Validate Failed:", info);
-      });
-  }, [form, onClose, onSubmit]);
-
   return (
     <Modal
       visible={open}
-      onCancel={() => onClose?.(true)}
-      onOk={handleSubmit}
+      onCancel={() => onClose?.()}
+      onOk={onSubmit}
       title={t("New Integration")}
       footer={[
         <Button key="back" onClick={() => onClose?.()}>
-          Cancel
+          {t("Cancel")}
         </Button>,
-        <Button key="submit" type="primary" onClick={handleSubmit}>
-          Save
+        <Button key="submit" type="primary" onClick={onSubmit}>
+          {t("Create")}
         </Button>,
       ]}>
-      <Form form={form} layout="vertical" initialValues={initialValues}>
+      <Form form={form} layout="vertical" initialValues={{}}>
+        <Form.Item label={t("Logo Image")}>
+          <Upload maxCount={1} listType="picture-card">
+            <Icon icon="plus" />
+          </Upload>
+        </Form.Item>
         <Form.Item
           name="name"
-          label={t("Workspace name")}
+          label={t("Integration Name")}
           rules={[
             {
               required: true,
-              message: t("Please input the title of the current workspace!"),
+              message: t("Please input the title of the integration!"),
             },
           ]}>
           <Input />
+        </Form.Item>
+        <Form.Item name="description" label={t("Description")}>
+          <TextArea rows={3} showCount maxLength={100} />
         </Form.Item>
       </Form>
     </Modal>
