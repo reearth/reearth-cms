e663d5723c083734651932b13b93a09f97bddd52
basel.issmail <basel.issmail@gmail.com>
2022-09-29 12:41:02 +0300

    Refactor page header

--- web/src/components/atoms/PageHeader/index.tsx
+++ web/src/components/atoms/PageHeader/index.tsx
@@ -1,3 +1,16 @@
-import { PageHeader } from "antd";
+import styled from "@emotion/styled";
+import { PageHeader, PageHeaderProps } from "antd";
 
-export default PageHeader;
+export type Props = PageHeaderProps & {
+  backgroundColor?: string;
+};
+
+const Header: React.FC<Props> = ({ ...props }) => {
+  return <StyledPageHeader {...props} backgroundColor={props.backgroundColor} />;
+};
+
+const StyledPageHeader = styled(PageHeader)<{ backgroundColor?: string }>`
+  background-color: ${({ backgroundColor }) => backgroundColor ?? "#FFF"};
+`;
+
+export default Header;
