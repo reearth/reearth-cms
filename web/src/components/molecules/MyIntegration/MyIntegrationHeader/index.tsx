4ade8b4f2e50f03df4e6967db6f3236424244991
basel.issmail <basel.issmail@gmail.com>
2022-09-29 10:52:37 +0300

    fix

--- web/src/components/molecules/MyIntegration/MyIntegrationHeader/index.tsx
+++ web/src/components/molecules/MyIntegration/MyIntegrationHeader/index.tsx
@@ -3,16 +3,17 @@ import styled from "@emotion/styled";
 import PageHeader from "@reearth-cms/components/atoms/PageHeader";
 
 export type Props = {
+  backgroundColor?: string;
   title: string;
   onBack?: () => void;
 };
 
-const MyIntegrationHeader: React.FC<Props> = ({ title, onBack }) => {
-  return <IntegrationPageHeader onBack={onBack} title={title} />;
+const MyIntegrationHeader: React.FC<Props> = ({ title, backgroundColor, onBack }) => {
+  return <IntegrationPageHeader onBack={onBack} title={title} backgroundColor={backgroundColor} />;
 };
 
-const IntegrationPageHeader = styled(PageHeader)`
-  background: #fff;
+const IntegrationPageHeader = styled(PageHeader)<{ backgroundColor?: string }>`
+  background: ${({ backgroundColor }) => backgroundColor || "unset"};
 `;
 
 export default MyIntegrationHeader;
