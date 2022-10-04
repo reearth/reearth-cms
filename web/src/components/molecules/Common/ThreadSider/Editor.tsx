cd380e99373dcd0f2753a0abcacf135b1be6ef25
nourbalaha <nourbalaha0@gmail.com>
2022-10-05 02:00:28 +0900

    chore: i18n

--- web/src/components/molecules/Common/ThreadSider/Editor.tsx
+++ web/src/components/molecules/Common/ThreadSider/Editor.tsx
@@ -1,6 +1,7 @@
 import Button from "@reearth-cms/components/atoms/Button";
 import Form from "@reearth-cms/components/atoms/Form";
 import Input from "@reearth-cms/components/atoms/Input";
+import { useT } from "@reearth-cms/i18n";
 
 const { TextArea } = Input;
 
@@ -11,15 +12,18 @@ type EditorProps = {
   value: any;
 };
 
-export const Editor: React.FC<EditorProps> = ({ onChange, onSubmit, submitting, value }) => (
-  <>
-    <Form.Item>
-      <TextArea rows={4} onChange={onChange} value={value} />
-    </Form.Item>
-    <Form.Item>
-      <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
-        Add Comment
-      </Button>
-    </Form.Item>
-  </>
-);
+export const Editor: React.FC<EditorProps> = ({ onChange, onSubmit, submitting, value }) => {
+  const t = useT();
+  return (
+    <>
+      <Form.Item>
+        <TextArea rows={4} onChange={onChange} value={value} />
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
+          {t("Add Comment")}
+        </Button>
+      </Form.Item>
+    </>
+  );
+};
