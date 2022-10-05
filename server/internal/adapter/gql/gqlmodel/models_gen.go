28543b657b171fe4e3dc290af05997e2726752a7
Go Nakagawa <nakagawago0614@gmail.com>
2022-10-05 12:11:18 +0900

    feat(server): add graphql schema to project public setting (#243)
    * feat(server): add graphql schema to project public setting
    
    * feat(server): fix struct
    
    * feat(server): exec gqlgen
    
    * feat(server): modify projectpublicsetting.graphql
    
    * feat(server): delete projectPublicSetting.graphql
    
    * feat(server): rerun generate cmd
    
    * fix(server): delete mutation
    
    * feat(server): add resolver
    
    * feat(server): remove new resolver file
    
    * feat(server): remove resolver

--- server/internal/adapter/gql/gqlmodel/models_gen.go
+++ server/internal/adapter/gql/gqlmodel/models_gen.go
@@ -348,6 +348,7 @@ type Model struct {
 	Key         string    `json:"key"`
 	Project     *Project  `json:"project"`
 	Schema      *Schema   `json:"schema"`
+	Public      bool      `json:"public"`
 	CreatedAt   time.Time `json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
 }
@@ -386,14 +387,15 @@ type Pagination struct {
 }
 
 type Project struct {
-	ID          ID         `json:"id"`
-	Name        string     `json:"name"`
-	Description string     `json:"description"`
-	Alias       string     `json:"alias"`
-	WorkspaceID ID         `json:"workspaceId"`
-	Workspace   *Workspace `json:"workspace"`
-	CreatedAt   time.Time  `json:"createdAt"`
-	UpdatedAt   time.Time  `json:"updatedAt"`
+	ID          ID                  `json:"id"`
+	Name        string              `json:"name"`
+	Description string              `json:"description"`
+	Alias       string              `json:"alias"`
+	WorkspaceID ID                  `json:"workspaceId"`
+	Workspace   *Workspace          `json:"workspace"`
+	CreatedAt   time.Time           `json:"createdAt"`
+	UpdatedAt   time.Time           `json:"updatedAt"`
+	Publication *ProjectPublication `json:"publication"`
 }
 
 func (Project) IsNode()        {}
@@ -418,6 +420,11 @@ type ProjectEdge struct {
 
 type ProjectPayload struct {
 	Project *Project `json:"project"`
+}
+
+type ProjectPublication struct {
+	Scope       ProjectPublicationScope `json:"scope"`
+	AssetPublic bool                    `json:"assetPublic"`
 }
 
 type PublishModelInput struct {
@@ -711,9 +718,15 @@ type UpdateModelInput struct {
 }
 
 type UpdateProjectInput struct {
-	ProjectID   ID      `json:"projectId"`
-	Name        *string `json:"name"`
-	Description *string `json:"description"`
+	ProjectID   ID                             `json:"projectId"`
+	Name        *string                        `json:"name"`
+	Description *string                        `json:"description"`
+	Publication *UpdateProjectPublicationInput `json:"publication"`
+}
+
+type UpdateProjectPublicationInput struct {
+	Scope       *ProjectPublicationScope `json:"scope"`
+	AssetPublic *bool                    `json:"assetPublic"`
 }
 
 type UpdateWebhookInput struct {
@@ -979,6 +992,49 @@ func (e *PreviewType) UnmarshalGQL(v interface{}) error {
 }
 
 func (e PreviewType) MarshalGQL(w io.Writer) {
+	fmt.Fprint(w, strconv.Quote(e.String()))
+}
+
+type ProjectPublicationScope string
+
+const (
+	ProjectPublicationScopePublic  ProjectPublicationScope = "PUBLIC"
+	ProjectPublicationScopeLimited ProjectPublicationScope = "LIMITED"
+	ProjectPublicationScopePrivate ProjectPublicationScope = "PRIVATE"
+)
+
+var AllProjectPublicationScope = []ProjectPublicationScope{
+	ProjectPublicationScopePublic,
+	ProjectPublicationScopeLimited,
+	ProjectPublicationScopePrivate,
+}
+
+func (e ProjectPublicationScope) IsValid() bool {
+	switch e {
+	case ProjectPublicationScopePublic, ProjectPublicationScopeLimited, ProjectPublicationScopePrivate:
+		return true
+	}
+	return false
+}
+
+func (e ProjectPublicationScope) String() string {
+	return string(e)
+}
+
+func (e *ProjectPublicationScope) UnmarshalGQL(v interface{}) error {
+	str, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("enums must be strings")
+	}
+
+	*e = ProjectPublicationScope(str)
+	if !e.IsValid() {
+		return fmt.Errorf("%s is not a valid ProjectPublicationScope", str)
+	}
+	return nil
+}
+
+func (e ProjectPublicationScope) MarshalGQL(w io.Writer) {
 	fmt.Fprint(w, strconv.Quote(e.String()))
 }
 
