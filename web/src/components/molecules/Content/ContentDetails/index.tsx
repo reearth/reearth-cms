01de2de2d1b4887664a3cef4be75fa88a204d377
basel.issmail <basel.issmail@gmail.com>
2022-10-08 16:14:19 +0300

    Refactor wrapper

--- web/src/components/molecules/Content/ContentDetails/index.tsx
+++ web/src/components/molecules/Content/ContentDetails/index.tsx
@@ -1,7 +1,5 @@
-import styled from "@emotion/styled";
-
-import Content from "@reearth-cms/components/atoms/Content";
 import ContentForm from "@reearth-cms/components/molecules/Content/ContentForm";
+import ContentWrapper from "@reearth-cms/components/molecules/Content/ContentWrapper";
 import { ItemField } from "@reearth-cms/components/molecules/Content/types";
 import { Model } from "@reearth-cms/components/molecules/Schema/types";
 
@@ -27,38 +25,18 @@ const ContentDetailsMolecule: React.FC<Props> = ({
   itemId,
 }) => {
   return (
-    <PaddedContent>
-      <LeftPaneWrapper>{ModelsMenu}</LeftPaneWrapper>
-      <ContentChild>
-        <ContentForm
-          onBack={onBack}
-          itemId={itemId}
-          onItemCreate={onItemCreate}
-          onItemUpdate={onItemUpdate}
-          model={model}
-          defaultFormValues={defaultFormValues}
-          initialFormValues={initialFormValues}
-        />
-      </ContentChild>
-    </PaddedContent>
+    <ContentWrapper modelsMenu={ModelsMenu}>
+      <ContentForm
+        onBack={onBack}
+        itemId={itemId}
+        onItemCreate={onItemCreate}
+        onItemUpdate={onItemUpdate}
+        model={model}
+        defaultFormValues={defaultFormValues}
+        initialFormValues={initialFormValues}
+      />
+    </ContentWrapper>
   );
 };
-
-const LeftPaneWrapper = styled.div`
-  width: 200px;
-`;
-
-const PaddedContent = styled(Content)`
-  margin: 16px;
-  background-color: #fff;
-  display: flex;
-  min-height: 100%;
-`;
-
-const ContentChild = styled.div`
-  flex: 1;
-  background-color: #fff;
-  padding: 24px;
-`;
 
 export default ContentDetailsMolecule;
--- web/src/components/molecules/Content/ContentListMolecule/index.tsx
+++ web/src/components/molecules/Content/ContentListMolecule/index.tsx
@@ -1,11 +1,11 @@
 import styled from "@emotion/styled";
 
 import Button from "@reearth-cms/components/atoms/Button";
-import Content from "@reearth-cms/components/atoms/Content";
 import Icon from "@reearth-cms/components/atoms/Icon";
 import PageHeader from "@reearth-cms/components/atoms/PageHeader";
 import { ProColumns } from "@reearth-cms/components/atoms/ProTable";
 import ContentTable from "@reearth-cms/components/molecules/Content/ContentTable";
+import ContentWrapper from "@reearth-cms/components/molecules/Content/ContentWrapper";
 import { ContentTableField } from "@reearth-cms/components/molecules/Content/types";
 import { Model } from "@reearth-cms/components/molecules/Schema/types";
 import { useT } from "@reearth-cms/i18n";
@@ -28,48 +28,24 @@ const ContentListMolecule: React.FC<Props> = ({
   const t = useT();
 
   return (
-    <PaddedContent>
-      <LeftPaneWrapper>{ModelsMenu}</LeftPaneWrapper>
-      <ContentChild>
-        <PageHeader
-          title={model?.name}
-          extra={
-            <Button
-              type="primary"
-              onClick={onItemAdd}
-              icon={<Icon icon="plus" />}
-              disabled={!model}>
-              {t("New Item")}
-            </Button>
-          }
-        />
-        <StyledContentTable
-          contentTableFields={contentTableFields}
-          contentTableColumns={contentTableColumns}
-        />
-      </ContentChild>
-    </PaddedContent>
+    <ContentWrapper modelsMenu={ModelsMenu}>
+      <PageHeader
+        title={model?.name}
+        extra={
+          <Button type="primary" onClick={onItemAdd} icon={<Icon icon="plus" />} disabled={!model}>
+            {t("New Item")}
+          </Button>
+        }
+      />
+      <StyledContentTable
+        contentTableFields={contentTableFields}
+        contentTableColumns={contentTableColumns}
+      />
+    </ContentWrapper>
   );
 };
 
-const LeftPaneWrapper = styled.div`
-  width: 200px;
-`;
-
-const PaddedContent = styled(Content)`
-  margin: 16px;
-  background-color: #fff;
-  display: flex;
-  min-height: 100%;
-`;
-
 const StyledContentTable = styled(ContentTable)`
-  flex: 1;
-  background-color: #fff;
-  padding: 24px;
-`;
-
-const ContentChild = styled.div`
   flex: 1;
   background-color: #fff;
   padding: 24px;
