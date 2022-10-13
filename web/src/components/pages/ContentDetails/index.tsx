da5b7c6e6b4bae7aeb02cbd9367bbc2a0eecdfb4
basel.issmail <basel.issmail@gmail.com>
2022-10-13 20:05:47 +0300

    Fix merging issue

--- web/src/components/pages/ContentDetails/index.tsx
+++ web/src/components/pages/ContentDetails/index.tsx
@@ -1,13 +1,12 @@
 import { AuthenticationRequiredPage } from "@reearth-cms/auth";
+import ProjectMenu from "@reearth-cms/components/molecules/Common/ProjectMenu";
 import Dashboard from "@reearth-cms/components/organisms/Dashboard";
 import ContentDetails from "@reearth-cms/components/organisms/Project/Content/ContentDetails";
 
 const ContentDetailsPage: React.FC = () => {
   return (
     <AuthenticationRequiredPage>
-      <Dashboard defaultSelectedKeys={["content"]} menuType="project">
-        <ContentDetails />
-      </Dashboard>
+      <Dashboard defaultSelectedKeys={["content"]} child={ContentDetails} sidebar={ProjectMenu} />
     </AuthenticationRequiredPage>
   );
 };
