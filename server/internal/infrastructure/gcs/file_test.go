24de5a07d0f54ade908973c69027bd45a397044a
nourbalaha <nourbalaha0@gmail.com>
2022-10-17 11:39:20 +0900

    test: TestFile_GetFSObjectPath

--- server/internal/infrastructure/gcs/file_test.go
+++ server/internal/infrastructure/gcs/file_test.go
@@ -10,10 +10,10 @@ import (
 func TestFile_GetFSObjectPath(t *testing.T) {
 	u := newUUID()
 	n := "xxx.yyy"
-	assert.Equal(t, path.Join(u[:2], u[2:], "xxx.yyy"), getGCSObjectPath(u, n))
+	assert.Equal(t, path.Join(gcsAssetBasePath, u[:2], u[2:], "xxx.yyy"), getGCSObjectPath(u, n))
 
 	n = "ああああ.yyy"
-	assert.Equal(t, path.Join(u[:2], u[2:], "----.yyy"), getGCSObjectPath(u, n))
+	assert.Equal(t, path.Join(gcsAssetBasePath, u[:2], u[2:], "----.yyy"), getGCSObjectPath(u, n))
 
 	assert.Equal(t, "", getGCSObjectPath("", ""))
 }
