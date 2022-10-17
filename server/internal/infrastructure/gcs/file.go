8ff4d88907db2b6dc834b8149223d165c4c2d8ca
Nour Balaha <nourbalaha0@gmail.com>
2022-10-17 13:33:04 +0900

    fix(server): asset path (#284)
    * fix: gcs asset path
    
    * test: TestFile_GetFSObjectPath
    
    * test: TestFile_GetURL

--- server/internal/infrastructure/gcs/file.go
+++ server/internal/infrastructure/gcs/file.go
@@ -66,7 +66,7 @@ func (f *fileRepo) ReadAsset(ctx context.Context, u string, fn string) (io.ReadC
 	if sn == "" {
 		return nil, rerror.ErrNotFound
 	}
-	return f.read(ctx, path.Join(gcsAssetBasePath, sn))
+	return f.read(ctx, sn)
 }
 
 func (f *fileRepo) UploadAsset(ctx context.Context, file *file.File) (string, error) {
@@ -108,7 +108,7 @@ func (f *fileRepo) DeleteAsset(ctx context.Context, u string, fn string) error {
 
 func (r *fileRepo) GetURL(a *asset.Asset) string {
 	uuid := a.UUID()
-	url, _ := url.JoinPath(r.host, uuid[:2], uuid[2:], fileName(a.FileName()))
+	url, _ := url.JoinPath(r.host, gcsAssetBasePath, uuid[:2], uuid[2:], fileName(a.FileName()))
 	return url
 }
 
@@ -196,7 +196,7 @@ func getGCSObjectPath(uuid, objectName string) string {
 		return ""
 	}
 
-	return path.Join(uuid[:2], uuid[2:], fileName(objectName))
+	return path.Join(gcsAssetBasePath, uuid[:2], uuid[2:], fileName(objectName))
 }
 
 func (f *fileRepo) bucket(ctx context.Context) (*storage.BucketHandle, error) {
