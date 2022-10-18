99ad3a2bfeb5b2d699106b8e3a82020cd588c079
KaWaite <34051327+KaWaite@users.noreply.github.com>
2022-10-18 11:42:28 +0900

    fix(web): header and project state management (#293)
    * UI for accessibility page
    
    * regen i18n
    
    * Regen i18n
    
    * Fix graphQL changes
    
    * Rename var
    
    * model public state functionality, refactors, etc
    
    * save button disable
    
    * finish accessibility functionality
    
    * regen i18n
    
    * add public to model declarations
    
    * remove console log
    
    * fix state management around selected project
    
    Co-authored-by: basel.issmail <basel.issmail@gmail.com>

--- web/src/components/organisms/Project/Accessibility/index.tsx
+++ web/src/components/organisms/Project/Accessibility/index.tsx
@@ -1,13 +1,9 @@
-import { useParams } from "react-router-dom";
-
 import AccessibilityMolecule from "@reearth-cms/components/molecules/Accessibility";
 
 import useHooks from "./hooks";
 
 const Accessibility: React.FC = () => {
-  const { projectId } = useParams();
-
-  const { projectScope, models, handleAccessibilityUpdate } = useHooks({ projectId });
+  const { projectScope, models, handleAccessibilityUpdate } = useHooks();
 
   return (
     <AccessibilityMolecule
