cd43a4ea981110987f2bb67bc0c6b5207072dca2
Nour Balaha <nourbalaha0@gmail.com>
2022-10-21 12:21:23 +0900

    feat(server): update thread domain models (#302)
    * updated thread domain models
    
    * increased codecov
    
    * PR fixes
    
    * test: fix unit tests

--- server/pkg/thread/common.go
+++ server/pkg/thread/common.go
@@ -5,5 +5,7 @@ import (
 )
 
 var (
-	ErrNoWorkspaceID = errors.New("WorkspaceID is required")
+	ErrNoWorkspaceID       = errors.New("workspace id is required")
+	ErrCommentAlreadyExist = errors.New("comment already exist in this thread")
+	ErrCommentDoesNotExist = errors.New("comment does not exist in this thread")
 )
