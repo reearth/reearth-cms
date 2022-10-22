b27e1eb7ed5f21b00503e916117bb539dc579ac1
basel.issmail <basel.issmail@gmail.com>
2022-10-22 16:42:42 +0300

    Add account hooks

--- web/src/components/molecules/WorkspaceSettings/DangerZone.tsx
+++ web/src/components/molecules/WorkspaceSettings/DangerZone.tsx
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
+import { useCallback } from "react";
 
 import Button from "@reearth-cms/components/atoms/Button";
+import Icon from "@reearth-cms/components/atoms/Icon";
+import Modal from "@reearth-cms/components/atoms/Modal";
 import { useT } from "@reearth-cms/i18n";
 
 export type Props = {
@@ -9,17 +12,32 @@ export type Props = {
 
 const DangerZone: React.FC<Props> = ({ onWorkspaceDelete }) => {
   const t = useT();
+  const { confirm } = Modal;
+
+  const handleWorkspaceDeleteConfirmation = useCallback(() => {
+    confirm({
+      title: t("Are you sure you want to delete this workspace?"),
+      icon: <Icon icon="exclamationCircle" />,
+      onOk() {
+        onWorkspaceDelete();
+      },
+    });
+  }, [confirm, onWorkspaceDelete, t]);
 
   return (
     <>
       <Title>{t("Remove Workspace")}</Title>
       <Text>
         {t(
-          "Permanently remove your Workspace and all of its contents from the Re:Earth CMS. This action is not reversible – please continue with caution.",
+          "Because at the current level of development, service A and service B are interdependent, and the workspace and personal accounts will be synchronized.",
+        )}
+        <br />
+        {t(
+          "So when you delete or create a workspace in service A and service B, they will all be synchronized with each other. Make sure you understand what you're doing.",
         )}
       </Text>
 
-      <Button onClick={onWorkspaceDelete} type="primary" danger>
+      <Button onClick={handleWorkspaceDeleteConfirmation} type="primary" danger>
         {t("Remove Workspace")}
       </Button>
     </>
@@ -39,5 +57,6 @@ const Text = styled.p`
   font-weight: 400;
   font-size: 14px;
   line-height: 22px;
+  color: #ff4d4f;
   margin: 24px 0;
 `;
