a851c467ef1e3abb0eb2d82c937238b4246de121
nourbalaha <nourbalaha0@gmail.com>
2022-10-24 21:06:13 +0900

    refactor: AssetListBody

--- web/src/components/molecules/Asset/AssetList/index.tsx
+++ web/src/components/molecules/Asset/AssetList/index.tsx
@@ -1,20 +1,111 @@
 import styled from "@emotion/styled";
 
-import PageHeader, { Props as PageHeaderProps } from "@reearth-cms/components/atoms/PageHeader";
-import AssetListTable, {
-  AssetListTableProps,
-} from "@reearth-cms/components/molecules/Asset/AssetListTable";
+import PageHeader from "@reearth-cms/components/atoms/PageHeader";
+import { UploadProps } from "@reearth-cms/components/atoms/Upload";
+import { Asset } from "@reearth-cms/components/molecules/Asset/asset.type";
+import AssetListTable from "@reearth-cms/components/molecules/Asset/AssetListTable";
+import UploadAsset from "@reearth-cms/components/molecules/Asset/UploadAsset";
+import { fileFormats, imageFormats } from "@reearth-cms/components/molecules/Common/Asset";
 
 type Props = {
-  pageHeader: PageHeaderProps;
-  tableProps: AssetListTableProps;
+  workspaceId: string | undefined;
+  projectId: string | undefined;
+  assetList: any;
+  createAssets: any;
+  assetsPerPage: any;
+  navigate: any;
+  handleSearchTerm: any;
+  selection: any;
+  setSelection: any;
+  fileList: any;
+  setFileList: any;
+  uploading: any;
+  setUploading: any;
+  uploadModalVisibility: any;
+  setUploadModalVisibility: any;
 };
 
-const AssetList: React.FC<Props> = ({ pageHeader, tableProps }) => {
+const AssetList: React.FC<Props> = ({
+  workspaceId,
+  projectId,
+  assetList,
+  createAssets,
+  assetsPerPage,
+  navigate,
+  handleSearchTerm,
+  selection,
+  setSelection,
+  fileList,
+  setFileList,
+  uploading,
+  setUploading,
+  uploadModalVisibility,
+  setUploadModalVisibility,
+}) => {
+  const displayUploadModal = () => {
+    setUploadModalVisibility(true);
+  };
+  const hideUploadModal = () => {
+    setUploadModalVisibility(false);
+    setUploading(false);
+    setFileList([]);
+  };
+
+  const handleUpload = () => {
+    setUploading(true);
+    createAssets(fileList).finally(() => {
+      hideUploadModal();
+    });
+  };
+
+  const handleEdit = (asset: Asset) => {
+    navigate(`/workspace/${workspaceId}/project/${projectId}/asset/${asset.id}`);
+  };
+
+  const uploadProps: UploadProps = {
+    name: "file",
+    multiple: true,
+    directory: false,
+    showUploadList: true,
+    accept: imageFormats + "," + fileFormats,
+    listType: "picture",
+    onRemove: file => {
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      setFileList(newFileList);
+    },
+    beforeUpload: (_file, files) => {
+      setFileList([...fileList, ...files]);
+      return false;
+    },
+    fileList,
+  };
+
   return (
     <Wrapper>
-      <PageHeader {...pageHeader} />
-      <AssetListTable {...tableProps} />
+      <PageHeader
+        title="Asset"
+        extra={
+          <UploadAsset
+            fileList={fileList}
+            uploading={uploading}
+            uploadProps={uploadProps}
+            uploadModalVisibility={uploadModalVisibility}
+            displayUploadModal={displayUploadModal}
+            hideUploadModal={hideUploadModal}
+            handleUpload={handleUpload}
+          />
+        }
+      />
+      <AssetListTable
+        assetList={assetList}
+        assetsPerPage={assetsPerPage}
+        handleEdit={handleEdit}
+        handleSearchTerm={handleSearchTerm}
+        selection={selection}
+        setSelection={setSelection}
+      />
     </Wrapper>
   );
 };
--- web/src/components/organisms/Asset/AssetList/index.tsx
+++ web/src/components/organisms/Asset/AssetList/index.tsx
@@ -1,10 +1,6 @@
 import { useParams } from "react-router-dom";
 
-import { UploadProps } from "@reearth-cms/components/atoms/Upload";
-import { Asset } from "@reearth-cms/components/molecules/Asset/asset.type";
 import AssetListBody from "@reearth-cms/components/molecules/Asset/AssetList";
-import UploadAsset from "@reearth-cms/components/molecules/Asset/UploadAsset";
-import { fileFormats, imageFormats } from "@reearth-cms/components/molecules/Common/Asset";
 
 import useHooks from "./hooks";
 
@@ -26,70 +22,23 @@ const AssetList: React.FC = () => {
     setUploadModalVisibility,
   } = useHooks(projectId);
 
-  const displayUploadModal = () => {
-    setUploadModalVisibility(true);
-  };
-  const hideUploadModal = () => {
-    setUploadModalVisibility(false);
-    setUploading(false);
-    setFileList([]);
-  };
-
-  const handleUpload = () => {
-    setUploading(true);
-    createAssets(fileList).finally(() => {
-      hideUploadModal();
-    });
-  };
-
-  const handleEdit = (asset: Asset) => {
-    navigate(`/workspace/${workspaceId}/project/${projectId}/asset/${asset.id}`);
-  };
-
-  const uploadProps: UploadProps = {
-    name: "file",
-    multiple: true,
-    directory: false,
-    showUploadList: true,
-    accept: imageFormats + "," + fileFormats,
-    listType: "picture",
-    onRemove: file => {
-      const index = fileList.indexOf(file);
-      const newFileList = fileList.slice();
-      newFileList.splice(index, 1);
-      setFileList(newFileList);
-    },
-    beforeUpload: (_file, files) => {
-      setFileList([...fileList, ...files]);
-      return false;
-    },
-    fileList,
-  };
-
   return (
     <AssetListBody
-      pageHeader={{
-        title: "Asset",
-        extra: (
-          <UploadAsset
-            fileList={fileList}
-            uploading={uploading}
-            uploadProps={uploadProps}
-            uploadModalVisibility={uploadModalVisibility}
-            displayUploadModal={displayUploadModal}
-            hideUploadModal={hideUploadModal}
-            handleUpload={handleUpload}
-          />
-        ),
-      }}
-      tableProps={{
-        assetList,
-        assetsPerPage,
-        handleEdit,
-        handleSearchTerm,
-        selection,
-        setSelection,
-      }}
+      workspaceId={workspaceId}
+      projectId={projectId}
+      assetList={assetList}
+      createAssets={createAssets}
+      assetsPerPage={assetsPerPage}
+      navigate={navigate}
+      handleSearchTerm={handleSearchTerm}
+      selection={selection}
+      setSelection={setSelection}
+      fileList={fileList}
+      setFileList={setFileList}
+      uploading={uploading}
+      setUploading={setUploading}
+      uploadModalVisibility={uploadModalVisibility}
+      setUploadModalVisibility={setUploadModalVisibility}
     />
   );
 };
