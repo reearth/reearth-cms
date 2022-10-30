71b05824309ab4870d144cfe45a3042007b5f534
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-10-30 14:23:35 +0300

    imp

---
+++ server/internal/adapter/integration/api.go
@@ -0,0 +1,12 @@
+package integration
+
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=server.cfg.yaml ../../../schemas/integration.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=types.cfg.yaml ../../../schemas/integration.yaml
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+// var _ StrictServerInterface = (*Server)(nil)
