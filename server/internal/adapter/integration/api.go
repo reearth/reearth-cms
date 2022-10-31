4fbe12f06789f1d7c23b839084395dbbed983caa
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-10-31 11:32:45 +0300

    fix

--- server/internal/adapter/integration/api.go
+++ server/internal/adapter/integration/api.go
@@ -1,7 +1,6 @@
 package integration
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=server.cfg.yaml ../../../schemas/integration.yaml
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=types.cfg.yaml ../../../schemas/integration.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=server.cfg.yml ../../../schemas/integration.yml
 
 type Server struct{}
 
