f5b58a4a4814f4e633e34673c823cb38f6eb01ac
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-10-31 12:24:59 +0300

    fix

--- server/internal/usecase/interactor/project.go
+++ server/internal/usecase/interactor/project.go
@@ -132,7 +132,7 @@ func (i *Project) Delete(ctx context.Context, projectID id.ProjectID, operator *
 	}
 	return Run0(ctx, operator, i.repos, Usecase().WithWritableWorkspaces(proj.Workspace()).Transaction(),
 		func() error {
-			if !operator.OwningWorkspaces.Has(proj.Workspace()) {
+			if !operator.IsOwningWorkspace(proj.Workspace()) {
 				return interfaces.ErrOperationDenied
 			}
 			if err := i.repos.Project.Remove(ctx, projectID); err != nil {
