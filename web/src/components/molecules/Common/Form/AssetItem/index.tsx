0e58e59f21279159a720af2fdd38d92718ba3509
nourbalaha <nourbalaha0@gmail.com>
2022-11-01 18:54:23 +0900

    wip: asset list table

--- web/src/components/molecules/Common/Form/AssetItem/index.tsx
+++ web/src/components/molecules/Common/Form/AssetItem/index.tsx
@@ -1,13 +1,16 @@
+import styled from "@emotion/styled";
 import { FormItemProps } from "antd/lib/form/FormItem";
 import { FormItemLabelProps } from "antd/lib/form/FormItemLabel";
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 import Button from "@reearth-cms/components/atoms/Button";
 import Form from "@reearth-cms/components/atoms/Form";
 import Icon from "@reearth-cms/components/atoms/Icon";
+import useHooks from "@reearth-cms/components/organisms/Asset/AssetList/hooks";
 import { useT } from "@reearth-cms/i18n";
 
-// import LinkToAssetModal from "../../LinkToAssetModal/linkToAssetModal";
+import LinkToAssetModal from "../../LinkToAssetModal/linkToAssetModal";
 
 type Props = FormItemProps & FormItemLabelProps;
 
@@ -15,19 +18,44 @@ const AssetItem: React.FC<Props> = ({ name, label, extra, rules }) => {
   const t = useT();
   const { Item } = Form;
 
-  const handleClick = () => { };
+  const { projectId } = useParams();
+  const { assetList, handleSearchTerm, handleAssetsReload, loading } = useHooks(projectId);
+
+  const [visible, setVisible] = useState(false);
+  const handleClick = () => {
+    setVisible(true);
+  };
+  const handleCancel = () => {
+    setVisible(false);
+  };
+  const handleConnect = (asset: any) => {
+    console.log(asset);
+  };
 
   return (
     <Item name={name} label={label} extra={extra} rules={rules}>
-      <Button onClick={handleClick}>
+      <AssetButton onClick={handleClick}>
         <div>
           <Icon icon="link" />
           <div style={{ marginTop: 8 }}>{t("Asset")}</div>
         </div>
-      </Button>
-      {/* <LinkToAssetModal /> */}
+      </AssetButton>
+      <LinkToAssetModal
+        visible={visible}
+        onCancel={handleCancel}
+        assetList={assetList}
+        onConnect={handleConnect}
+        onSearchTerm={handleSearchTerm}
+        onAssetsReload={handleAssetsReload}
+        loading={loading}
+      />
     </Item>
   );
 };
+
+const AssetButton = styled(Button)`
+  width: 100px;
+  height: 100px;
+`;
 
 export default AssetItem;
--- web/src/components/molecules/Common/LinkToAssetModal/linkToAssetModal.tsx
+++ web/src/components/molecules/Common/LinkToAssetModal/linkToAssetModal.tsx
@@ -1,115 +1,120 @@
-import styled from "@emotion/styled";
-import { Dispatch, Key, SetStateAction } from "react";
-
+import Button from "@reearth-cms/components/atoms/Button";
+import Icon from "@reearth-cms/components/atoms/Icon";
 import Modal from "@reearth-cms/components/atoms/Modal";
-import { UploadFile, UploadProps } from "@reearth-cms/components/atoms/Upload";
+import ProTable, {
+  ProColumns,
+  ListToolBarProps,
+  OptionConfig,
+} from "@reearth-cms/components/atoms/ProTable";
 import { Asset } from "@reearth-cms/components/molecules/Asset/asset.type";
-import UploadAsset from "@reearth-cms/components/molecules/Asset/UploadAsset";
 import { useT } from "@reearth-cms/i18n";
-
-import AssetListTable from "../../Asset/AssetListTable";
+import { dateTimeFormat, bytesFormat } from "@reearth-cms/utils/format";
 
 type Props = {
   // modal props
   visible: boolean;
-  handleCancel: () => void;
+  onCancel: () => void;
   // table props
   assetList: Asset[];
-  assetsPerPage: number | undefined;
-  onEdit: (asset: Asset) => void;
-  selection: {
-    selectedRowKeys: Key[];
-  };
+  onConnect: (asset: Asset) => void;
   onSearchTerm: (term?: string) => void;
-  setSelection: Dispatch<
-    SetStateAction<{
-      selectedRowKeys: Key[];
-    }>
-  >;
   onAssetsReload: () => void;
   loading: boolean;
-  // upload asset props
-  fileList: UploadFile<File>[];
-  uploading: boolean;
-  uploadProps: UploadProps;
-  uploadModalVisibility: boolean;
-  displayUploadModal: () => void;
-  hideUploadModal: () => void;
-  handleUpload: () => void;
 };
 
 const LinkToAssetModal: React.FC<Props> = ({
   visible,
-  handleCancel,
-  fileList,
-  uploading,
-  uploadProps,
-  uploadModalVisibility,
-  displayUploadModal,
-  hideUploadModal,
-  handleUpload,
+  onCancel,
   assetList,
-  assetsPerPage,
-  onEdit,
-  selection,
+  onConnect,
   onSearchTerm,
-  setSelection,
   onAssetsReload,
   loading,
 }) => {
   const t = useT();
 
+  const options: OptionConfig = {
+    search: true,
+    reload: onAssetsReload,
+  };
+
+  const handleToolbarEvents: ListToolBarProps | undefined = {
+    search: {
+      onSearch: (value: string) => {
+        if (value) {
+          onSearchTerm(value);
+        } else {
+          onSearchTerm();
+        }
+      },
+    },
+  };
+
+  const columns: ProColumns<Asset>[] = [
+    {
+      title: "",
+      render: (_, asset) => (
+        <Button type="link" icon={<Icon icon="link" />} onClick={() => onConnect(asset)} />
+      ),
+    },
+    {
+      title: t("File"),
+      dataIndex: "fileName",
+      key: "fileName",
+    },
+    {
+      title: t("Size"),
+      dataIndex: "size",
+      key: "size",
+      render: (_text, record) => bytesFormat(record.size),
+    },
+    {
+      title: t("Preview Type"),
+      dataIndex: "previewType",
+      key: "previewType",
+    },
+    {
+      title: t("Created At"),
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (_text, record) => dateTimeFormat(record.createdAt),
+    },
+    {
+      title: t("Created By"),
+      dataIndex: "createdBy",
+      key: "createdBy",
+    },
+  ];
+
   return (
     <Modal
+      title={t("Link To Asset")}
       centered
       visible={visible}
-      onCancel={handleCancel}
-      footer={null}
-      width="50vw"
+      onCancel={onCancel}
+      footer={[
+        <Button key="upload" icon={<Icon icon="upload" />}>
+          {t("Upload Asset")}
+        </Button>,
+      ]}
+      width="70vw"
       bodyStyle={{
         minHeight: "50vh",
         position: "relative",
         paddingBottom: "80px",
       }}>
-      <Header>
-        <h2>{t("Link To Asset")}</h2>
-      </Header>
-      <Body className="body">
-        <AssetListTable
-          assetList={assetList}
-          assetsPerPage={assetsPerPage}
-          onEdit={onEdit}
-          onSearchTerm={onSearchTerm}
-          selection={selection}
-          setSelection={setSelection}
-          onAssetsReload={onAssetsReload}
-          loading={loading}
-        />
-      </Body>
-      <Footer>
-        <UploadAsset
-          fileList={fileList}
-          uploading={uploading}
-          uploadProps={uploadProps}
-          uploadModalVisibility={uploadModalVisibility}
-          displayUploadModal={displayUploadModal}
-          hideUploadModal={hideUploadModal}
-          handleUpload={handleUpload}
-        />
-      </Footer>
+      <ProTable
+        dataSource={assetList}
+        columns={columns}
+        search={false}
+        rowKey="id"
+        options={options}
+        toolbar={handleToolbarEvents}
+        tableStyle={{ overflowX: "scroll" }}
+        loading={loading}
+      />
     </Modal>
   );
 };
-
-const Header = styled.div``;
-
-const Body = styled.div``;
-
-const Footer = styled.div`
-  position: absolute;
-  bottom: 20px;
-  right: 20px;
-  padding: 10px;
-`;
 
 export default LinkToAssetModal;
