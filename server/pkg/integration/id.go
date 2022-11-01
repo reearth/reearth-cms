d10fcacaa67cff70296d8b3e219745409f2eb29e
HideBa <baba.papa1120.ba@gmail.com>
2022-11-01 13:53:14 +0900

    impl test for integration list

--- server/pkg/integration/id.go
+++ server/pkg/integration/id.go
@@ -10,6 +10,7 @@ type UserID = id.UserID
 type ModelID = id.ModelID
 
 var NewID = id.NewIntegrationID
+var NewWebhookID = id.NewWebhookID
 var MustID = id.MustIntegrationID
 var IDFrom = id.IntegrationIDFrom
 var IDFromRef = id.IntegrationIDFromRef
