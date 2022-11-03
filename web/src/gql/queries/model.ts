293c07ca895de266fa9a297673519299983684ca
basel.issmail <basel.issmail@gmail.com>
2022-11-03 10:37:37 +0200

    Add unique/required/multivaue options

--- web/src/gql/queries/model.ts
+++ web/src/gql/queries/model.ts
@@ -19,6 +19,7 @@ export const GET_MODELS = gql`
             description
             required
             unique
+            multiValue
             typeProperty {
               ... on SchemaFieldText {
                 defaultValue
