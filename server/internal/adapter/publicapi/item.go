e27c0aa889c07c732deff22d77768f4a24e982ac
Go nakagawa <nakagawago0614@gmail.com>
2022-11-04 18:54:08 +0900

    feat(internal): imp item.go

--- server/internal/adapter/publicapi/item.go
+++ server/internal/adapter/publicapi/item.go
@@ -1,11 +1,41 @@
 package publicApi
 
-import "context"
+import (
+	"context"
 
-func (*Controller) GetItem(ctx context.Context, prj, id string) (Item, error) {
-	panic("implement")
+	"github.com/reearth/reearth-cms/server/internal/usecase/interfaces"
+)
+
+type PublicAPIController struct {
+	usecase interfaces.PublicAPI
 }
 
-func (*Controller) GetItems(ctx context.Context, prj, schema string) (ListResult[Item], error) {
-	panic("implement")
+func NewPublicAPIController(usecase interfaces.PublicAPI) *PublicAPIController {
+	return &PublicAPIController{
+		usecase: usecase,
+	}
+}
+
+func (c *Controller) GetItem(ctx context.Context, prj, id string) (Item, error) {
+	i, err := c.GetItem(ctx, prj, id)
+	if err != nil {
+		return Item{}, err
+	}
+	return Item{
+		ID:     i.ID,
+		Fields: i.Fields,
+	}, nil
+}
+
+func (c *Controller) GetItems(ctx context.Context, prj, schema string) (ListResult[Item], error) {
+	i, err := c.GetItems(ctx, prj, schema)
+	if err != nil {
+		return ListResult[Item]{}, err
+	}
+	return ListResult[Item]{
+		Results:    i.Results,
+		TotalCount: i.TotalCount,
+		Limit:      i.Limit,
+		Offset:     i.Offset,
+	}, nil
 }
