1dbd4510107e5c0d55d8975846a1389a242f67fa
HideBa <baba.papa1120.ba@gmail.com>
2022-11-11 22:05:43 +0900

    fix

--- server/internal/adapter/integration/asset_comment_api.go
+++ server/internal/adapter/integration/asset_comment_api.go
@@ -5,23 +5,20 @@ import (
 )
 
 func (s Server) AssetCommentDelete(ctx context.Context, request AssetCommentDeleteRequestObject) (AssetCommentDeleteResponseObject error) {
-	panic("hoge")
+	panic("implement me")
 
 }
 
-// (GET /projects/{projectId}/assets/{assetId}/comments)
 func (s Server) AssetCommentFilter(ctx context.Context, request AssetCommentFilterRequestObject) (AssetCommentFilterResponseObject error) {
-	panic("hoge")
+	panic("implement me")
 
 }
 
-// (PATCH /projects/{projectId}/assets/{assetId}/comments)
 func (s Server) AssetCommentUpdate(ctx context.Context, request AssetCommentUpdateRequestObject) (AssetCommentUpdateResponseObject error) {
-	panic("hoge")
+	panic("implement me")
 
 }
 
-// (POST /projects/{projectId}/assets/{assetId}/comments)
 func (s Server) AssetCommentCreate(ctx context.Context, request AssetCommentCreateRequestObject) (AssetCommentCreateResponseObject error) {
-	panic("hoge")
+	panic("implement me")
 }
