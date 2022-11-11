f13960563f21ecaa1d0afca49bffec095bc5a197
HideBa <baba.papa1120.ba@gmail.com>
2022-11-11 22:16:38 +0900

    fix

--- server/internal/adapter/integration/asset_comment_api.go
+++ server/internal/adapter/integration/asset_comment_api.go
@@ -4,21 +4,18 @@ import (
 	"context"
 )
 
-func (s Server) AssetCommentDelete(ctx context.Context, request AssetCommentDeleteRequestObject) (AssetCommentDeleteResponseObject error) {
+func (s Server) AssetCommentFilter(ctx context.Context, request AssetCommentFilterRequestObject) (AssetCommentFilterResponseObject, error) {
 	panic("implement me")
-
 }
 
-func (s Server) AssetCommentFilter(ctx context.Context, request AssetCommentFilterRequestObject) (AssetCommentFilterResponseObject error) {
+func (s Server) AssetCommentCreate(ctx context.Context, request AssetCommentCreateRequestObject) (AssetCommentCreateResponseObject, error) {
 	panic("implement me")
-
 }
 
-func (s Server) AssetCommentUpdate(ctx context.Context, request AssetCommentUpdateRequestObject) (AssetCommentUpdateResponseObject error) {
+func (s Server) AssetCommentUpdate(ctx context.Context, request AssetCommentUpdateRequestObject) (AssetCommentUpdateResponseObject, error) {
 	panic("implement me")
-
 }
 
-func (s Server) AssetCommentCreate(ctx context.Context, request AssetCommentCreateRequestObject) (AssetCommentCreateResponseObject error) {
+func (s Server) AssetCommentDelete(ctx context.Context, request AssetCommentDeleteRequestObject) (AssetCommentDeleteResponseObject, error) {
 	panic("implement me")
 }
