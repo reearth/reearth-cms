4765e91ef0105f3b692ea6a3869389a0cba4ed73
KaWaite <kyle.waite@outlook.com>
2022-11-14 15:24:41 +0900

    Add collapsing to Content page

--- web/src/components/molecules/Content/List/index.tsx
+++ web/src/components/molecules/Content/List/index.tsx
@@ -2,31 +2,36 @@ import styled from "@emotion/styled";
 
 import Button from "@reearth-cms/components/atoms/Button";
 import Icon from "@reearth-cms/components/atoms/Icon";
+import ComplexInnerContents from "@reearth-cms/components/atoms/InnerContents/complex";
 import PageHeader from "@reearth-cms/components/atoms/PageHeader";
 import { ProColumns } from "@reearth-cms/components/atoms/ProTable";
+import Sidebar from "@reearth-cms/components/molecules/Common/Sidebar";
 import ContentTable from "@reearth-cms/components/molecules/Content/Table";
 import { ContentTableField } from "@reearth-cms/components/molecules/Content/types";
-import ContentWrapper from "@reearth-cms/components/molecules/Content/Wrapper";
 import { Model } from "@reearth-cms/components/molecules/Schema/types";
 import { useT } from "@reearth-cms/i18n";
 
 export type Props = {
+  collapsed?: boolean;
   model?: Model;
   contentTableFields?: ContentTableField[];
   itemsDataLoading: boolean;
   contentTableColumns?: ProColumns<ContentTableField>[];
   modelsMenu: React.ReactNode;
+  onCollapse?: (collapse: boolean) => void;
   onItemAdd: () => void;
   onItemsReload: () => void;
   onItemEdit: (itemId: string) => void;
 };
 
 const ContentListMolecule: React.FC<Props> = ({
+  collapsed,
   model,
   contentTableFields,
   contentTableColumns,
-  modelsMenu: ModelsMenu,
+  modelsMenu,
   itemsDataLoading,
+  onCollapse,
   onItemAdd,
   onItemsReload,
   onItemEdit,
@@ -34,31 +39,48 @@ const ContentListMolecule: React.FC<Props> = ({
   const t = useT();
 
   return (
-    <ContentWrapper modelsMenu={ModelsMenu}>
-      <PageHeader
-        title={model?.name}
-        subTitle={model?.key ? `#${model.key}` : null}
-        extra={
-          <Button type="primary" onClick={onItemAdd} icon={<Icon icon="plus" />} disabled={!model}>
-            {t("New Item")}
-          </Button>
-        }
-      />
-      <StyledContentTable
-        loading={itemsDataLoading}
-        onItemsReload={onItemsReload}
-        onItemEdit={onItemEdit}
-        contentTableFields={contentTableFields}
-        contentTableColumns={contentTableColumns}
-      />
-    </ContentWrapper>
+    <ComplexInnerContents
+      left={
+        <Sidebar
+          collapsed={collapsed}
+          onCollapse={onCollapse}
+          collapsedWidth={54}
+          width={208}
+          trigger={<Icon icon={collapsed ? "panelToggleRight" : "panelToggleLeft"} />}>
+          {modelsMenu}
+        </Sidebar>
+      }
+      center={
+        <Content>
+          <PageHeader
+            title={model?.name}
+            subTitle={model?.key ? `#${model.key}` : null}
+            extra={
+              <Button
+                type="primary"
+                onClick={onItemAdd}
+                icon={<Icon icon="plus" />}
+                disabled={!model}>
+                {t("New Item")}
+              </Button>
+            }
+          />
+          <ContentTable
+            loading={itemsDataLoading}
+            onItemsReload={onItemsReload}
+            onItemEdit={onItemEdit}
+            contentTableFields={contentTableFields}
+            contentTableColumns={contentTableColumns}
+          />
+        </Content>
+      }
+    />
   );
 };
 
-const StyledContentTable = styled(ContentTable)`
-  flex: 1;
+const Content = styled.div`
+  width: 100%;
   background-color: #fff;
-  padding: 24px;
 `;
 
 export default ContentListMolecule;
