20459b7d2836c2bb303c2ff196a327700cdeb425
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-11-14 13:55:28 +0300

    fix(server): integration bugs (#420)
    * fixs
    
    * fetch type from schema field
    
    * fix auth
    
    * skip int values with whitespaces
    
    * return nil on white spaces
    
    * fix condition
    
    * fix condition
    
    * value can be null
    
    * fix
    
    * fix auth
    
    * fix
    
    Co-authored-by: maherhamoui6 <hammimo022@gmail.com>

--- server/internal/adapter/integration/item_converter.go
+++ server/internal/adapter/integration/item_converter.go
@@ -5,13 +5,11 @@ import (
 	"github.com/reearth/reearth-cms/server/internal/usecase/interfaces"
 	"github.com/reearth/reearth-cms/server/pkg/integrationapi"
 	"github.com/reearth/reearth-cms/server/pkg/item"
-	"github.com/reearth/reearth-cms/server/pkg/model"
-	"github.com/reearth/reearth-cms/server/pkg/schema"
 	"github.com/reearth/reearth-cms/server/pkg/version"
 	"github.com/samber/lo"
 )
 
-func toItem(i *item.Item, ver *version.Value[*item.Item], mId model.ID) integrationapi.Item {
+func toItem(i *item.Item, ver *version.Value[*item.Item]) integrationapi.Item {
 	fs := lo.Map(i.Fields(), func(f *item.Field, _ int) integrationapi.Field {
 		return integrationapi.Field{
 			Id:    f.SchemaFieldID().Ref(),
@@ -27,7 +25,7 @@ func toItem(i *item.Item, ver *version.Value[*item.Item], mId model.ID) integrat
 	})
 	return integrationapi.Item{
 		Id:        lo.ToPtr(i.ID()),
-		ModelId:   lo.ToPtr(mId),
+		ModelId:   lo.ToPtr(i.Model()),
 		Archived:  lo.ToPtr(ver.Value() == nil),
 		Fields:    &fs,
 		CreatedAt: &types.Date{Time: i.Timestamp()},
@@ -39,43 +37,12 @@ func toItem(i *item.Item, ver *version.Value[*item.Item], mId model.ID) integrat
 }
 
 func toItemFieldParam(f integrationapi.Field) interfaces.ItemFieldParam {
+	var v any = f.Value
+	if f.Value != nil {
+		v = *f.Value
+	}
 	return interfaces.ItemFieldParam{
 		SchemaFieldID: *f.Id,
-		ValueType:     fromSchemaFieldType(f.Type),
-		Value:         f.Value,
-	}
-}
-
-func fromSchemaFieldType(t *integrationapi.FieldType) schema.Type {
-	if t == nil {
-		return ""
-	}
-	switch *t {
-	case integrationapi.FieldTypeText:
-		return schema.TypeText
-	case integrationapi.FieldTypeTextArea:
-		return schema.TypeTextArea
-	case integrationapi.FieldTypeRichText:
-		return schema.TypeRichText
-	case integrationapi.FieldTypeMarkdown:
-		return schema.TypeMarkdown
-	case integrationapi.FieldTypeAsset:
-		return schema.TypeAsset
-	case integrationapi.FieldTypeDate:
-		return schema.TypeDate
-	case integrationapi.FieldTypeBool:
-		return schema.TypeBool
-	case integrationapi.FieldTypeSelect:
-		return schema.TypeSelect
-	case integrationapi.FieldTypeTag:
-		return schema.TypeTag
-	case integrationapi.FieldTypeInteger:
-		return schema.TypeInteger
-	case integrationapi.FieldTypeReference:
-		return schema.TypeReference
-	case integrationapi.FieldTypeUrl:
-		return schema.TypeURL
-	default:
-		return ""
+		Value:         v,
 	}
 }
