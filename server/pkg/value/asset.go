4caad414da35572d8d7fe0b2a8daf6c0a80bf728
rot1024 <aayhrot@gmail.com>
2022-11-18 16:50:13 +0900

    refactor: add value package (#436)

---
+++ server/pkg/value/asset.go
@@ -0,0 +1,43 @@
+package value
+
+import (
+	"github.com/reearth/reearth-cms/server/pkg/id"
+)
+
+const TypeAsset Type = "asset"
+
+type propertyAsset struct{}
+
+type Asset = id.AssetID
+
+func (p *propertyAsset) ToValue(i any) (any, bool) {
+	if v, ok := i.(string); ok {
+		if u, err := id.AssetIDFrom(v); err == nil {
+			return u, true
+		}
+	} else if v, ok := i.(id.AssetID); ok {
+		return v, true
+	} else if v, ok := i.(*string); ok && v != nil {
+		return p.ToValue(*v)
+	} else if v, ok := i.(*id.AssetID); ok && v != nil {
+		return p.ToValue(*v)
+	}
+	return nil, false
+}
+
+func (*propertyAsset) ToInterface(v any) (any, bool) {
+	return v.(Asset).String(), true
+}
+
+func (*propertyAsset) Validate(i any) bool {
+	_, ok := i.(Asset)
+	return ok
+}
+
+func (v *Value) ValueAsset() (vv Asset, ok bool) {
+	if v == nil {
+		return
+	}
+	vv, ok = v.v.(Asset)
+	return
+}
