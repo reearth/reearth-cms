d075a7f93a683b69db6cf46e457b5e9c59acc4be
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2022-11-22 10:03:19 +0300

    e2e for item list

---
+++ server/e2e/common_test.go
@@ -0,0 +1,79 @@
+package e2e
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"testing"
+
+	"github.com/gavv/httpexpect/v2"
+	"github.com/reearth/reearth-cms/server/internal/app"
+	"github.com/reearth/reearth-cms/server/internal/infrastructure/memory"
+	"github.com/reearth/reearth-cms/server/internal/infrastructure/mongo"
+	"github.com/reearth/reearth-cms/server/internal/usecase/gateway"
+	"github.com/reearth/reearth-cms/server/internal/usecase/repo"
+	"github.com/reearth/reearthx/mongox/mongotest"
+	"github.com/samber/lo"
+)
+
+type seeder func(ctx context.Context, r *repo.Container) error
+
+func init() {
+	mongotest.Env = "REEARTH_DB"
+}
+
+func StartServer(t *testing.T, cfg *app.Config, useMongo bool, seeder seeder) *httpexpect.Expect {
+	t.Helper()
+
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	ctx := context.Background()
+
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatalf("server failed to listen: %v", err)
+	}
+
+	var repos *repo.Container
+	if useMongo {
+		db := mongotest.Connect(t)(t)
+		repos = lo.Must(mongo.New(ctx, db.Client(), db.Name()))
+	} else {
+		repos = memory.New()
+	}
+
+	if seeder != nil {
+		if err := seeder(ctx, repos); err != nil {
+			t.Fatalf("seed: faild to seed the db")
+		}
+	}
+
+	srv := app.NewServer(ctx, &app.ServerConfig{
+		Config:   cfg,
+		Repos:    repos,
+		Gateways: &gateway.Container{},
+	})
+
+	ch := make(chan error)
+	go func() {
+		if err := srv.Serve(l); err != http.ErrServerClosed {
+			ch <- err
+		}
+		close(ch)
+	}()
+
+	t.Cleanup(func() {
+		if err := srv.Shutdown(context.Background()); err != nil {
+			t.Fatalf("server shutdown: %v", err)
+		}
+
+		if err := <-ch; err != nil {
+			t.Fatalf("server serve: %v", err)
+		}
+
+	})
+
+	return httpexpect.New(t, "http://"+l.Addr().String())
+}
---
+++ server/e2e/integration_item_test.go
@@ -0,0 +1,132 @@
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/reearth/reearth-cms/server/internal/app"
+	"github.com/reearth/reearth-cms/server/internal/usecase/repo"
+	"github.com/reearth/reearth-cms/server/pkg/id"
+	"github.com/reearth/reearth-cms/server/pkg/integration"
+	"github.com/reearth/reearth-cms/server/pkg/item"
+	"github.com/reearth/reearth-cms/server/pkg/key"
+	"github.com/reearth/reearth-cms/server/pkg/model"
+	"github.com/reearth/reearth-cms/server/pkg/project"
+	"github.com/reearth/reearth-cms/server/pkg/schema"
+	"github.com/reearth/reearth-cms/server/pkg/user"
+	"github.com/samber/lo"
+)
+
+var mId = id.NewModelID()
+
+func baseSeeder(ctx context.Context, r *repo.Container) error {
+
+	u := user.New().NewID().
+		Name("e2e").
+		Email("e2e@e2e.com").
+		MustBuild()
+	if err := r.User.Save(ctx, u); err != nil {
+		return err
+	}
+
+	i := integration.New().NewID().
+		Type(integration.TypePublic).
+		Name("i1").
+		Description("i1 desc").
+		LogoUrl(lo.Must(url.Parse("https://test.com"))).
+		Token("secret_1234567890").
+		Developer(id.NewUserID()).
+		MustBuild()
+	if err := r.Integration.Save(ctx, i); err != nil {
+		return err
+	}
+
+	w := user.NewWorkspace().NewID().
+		Name("e2e").
+		Personal(false).
+		Integrations(map[user.IntegrationID]user.Member{i.ID(): {Role: user.RoleOwner, InvitedBy: u.ID()}}).
+		MustBuild()
+	if err := r.Workspace.Save(ctx, w); err != nil {
+		return err
+	}
+
+	p := project.New().NewID().
+		Name("p1").
+		Description("p1 desc").
+		ImageURL(lo.Must(url.Parse("https://test.com"))).
+		Workspace(w.ID()).
+		MustBuild()
+	if err := r.Project.Save(ctx, p); err != nil {
+		return err
+	}
+
+	sf := schema.NewFieldText(nil, nil).NewID().RandomKey().MustBuild()
+	s := schema.New().NewID().
+		Workspace(w.ID()).
+		Project(p.ID()).
+		Fields([]*schema.Field{sf}).
+		MustBuild()
+	if err := r.Schema.Save(ctx, s); err != nil {
+		return err
+	}
+
+	m := model.New().
+		ID(mId).
+		Name("m1").
+		Description("m1 desc").
+		Public(true).
+		Key(key.Random()).
+		Project(p.ID()).
+		Schema(s.ID()).
+		MustBuild()
+	if err := r.Model.Save(ctx, m); err != nil {
+		return err
+	}
+
+	itm := item.New().NewID().
+		Schema(s.ID()).
+		Model(m.ID()).
+		Project(p.ID()).
+		Thread(id.NewThreadID()).
+		MustBuild()
+	if err := r.Item.Save(ctx, itm); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Get|/models/{modelId}/items
+func TestIntegrationItemListAPI(t *testing.T) {
+	e := StartServer(t, &app.Config{}, true, baseSeeder)
+
+	r := e.GET(fmt.Sprintf("/api/models/%s/items", id.NewModelID())).
+		Expect()
+	r.Status(http.StatusUnauthorized)
+
+	r = e.GET(fmt.Sprintf("/api/models/%s/items", id.NewModelID())).
+		WithHeader("authorization", "secret_abc").
+		Expect()
+	r.Status(http.StatusUnauthorized)
+
+	r = e.GET(fmt.Sprintf("/api/models/%s/items", id.NewModelID())).
+		WithHeader("authorization", "Bearer secret_1234567890").
+		WithQuery("page", 1).
+		WithQuery("perPage", 5).
+		Expect()
+	r.Status(http.StatusNotFound)
+
+	r = e.GET(fmt.Sprintf("/api/models/%s/items", mId)).
+		WithHeader("authorization", "Bearer secret_1234567890").
+		WithQuery("page", 1).
+		WithQuery("perPage", 5).
+		Expect()
+	r.Status(http.StatusOK).
+		JSON().
+		Object().Keys().
+		Contains("items", "page", "perPage", "totalCount")
+
+}
