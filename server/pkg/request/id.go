3c1018128f873b62f24fc994fd1c955a76a37c16
maherhamoui6 <hammimo022@gmail.com>
2022-11-22 07:41:00 +0400

    add item to request

--- server/pkg/request/id.go
+++ server/pkg/request/id.go
@@ -3,6 +3,7 @@ package request
 import "github.com/reearth/reearth-cms/server/pkg/id"
 
 type ID = id.RequestID
+type ItemID = id.ItemID
 type UserID = id.UserID
 
 var NewID = id.NewRequestID
--- server/pkg/request/request.go
+++ server/pkg/request/request.go
@@ -6,6 +6,7 @@ import (
 
 type Request struct {
 	id          ID
+	item        ItemID
 	title       string
 	description string
 	createdBy   UserID
