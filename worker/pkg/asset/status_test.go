5cdf4288341d224b76a028364ad0cd0e2efe1ad0
nourbalaha <nourbalaha0@gmail.com>
2022-12-02 15:32:25 +0900

    pr fixes

--- worker/pkg/asset/status_test.go
+++ worker/pkg/asset/status_test.go
@@ -7,34 +7,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStatus_StatusFrom(t *testing.T) {
+func TestArchiveExtractionStatusFrom(t *testing.T) {
 	s := ArchiveExtractionStatusPending
-	res, ok := StatusFrom("pending")
+	res, ok := ArchiveExtractionStatusFrom("pending")
 	assert.Equal(t, s, res)
 	assert.True(t, ok)
 
 	s = ArchiveExtractionStatusPending
-	res, ok = StatusFrom("PENDING")
+	res, ok = ArchiveExtractionStatusFrom("PENDING")
 	assert.Equal(t, s, res)
 	assert.True(t, ok)
 
 	s = ArchiveExtractionStatusInProgress
-	res, ok = StatusFrom("in_progress")
+	res, ok = ArchiveExtractionStatusFrom("in_progress")
 	assert.Equal(t, s, res)
 	assert.True(t, ok)
 
 	s = ArchiveExtractionStatusDone
-	res, ok = StatusFrom("done")
+	res, ok = ArchiveExtractionStatusFrom("done")
 	assert.Equal(t, s, res)
 	assert.True(t, ok)
 
 	s = ArchiveExtractionStatusFailed
-	res, ok = StatusFrom("failed")
+	res, ok = ArchiveExtractionStatusFrom("failed")
 	assert.Equal(t, s, res)
 	assert.True(t, ok)
 
 	s = ArchiveExtractionStatus("")
-	res, ok = StatusFrom("")
+	res, ok = ArchiveExtractionStatusFrom("")
 	assert.Equal(t, s, res)
 	assert.False(t, ok)
 }
@@ -46,27 +46,27 @@ func TestStatus_StatusFromRef(t *testing.T) {
 	f := ArchiveExtractionStatusFailed
 
 	s := lo.ToPtr("pending")
-	res := StatusFromRef(s)
+	res := ArchiveExtractionStatusFromRef(s)
 	assert.Equal(t, &p, res)
 
 	s = lo.ToPtr("PENDING")
-	res = StatusFromRef(s)
+	res = ArchiveExtractionStatusFromRef(s)
 	assert.Equal(t, &p, res)
 
 	s = lo.ToPtr("in_progress")
-	res = StatusFromRef(s)
+	res = ArchiveExtractionStatusFromRef(s)
 	assert.Equal(t, &ip, res)
 
 	s = lo.ToPtr("done")
-	res = StatusFromRef(s)
+	res = ArchiveExtractionStatusFromRef(s)
 	assert.Equal(t, &d, res)
 
 	s = lo.ToPtr("failed")
-	res = StatusFromRef(s)
+	res = ArchiveExtractionStatusFromRef(s)
 	assert.Equal(t, &f, res)
 
 	s = nil
-	res = StatusFromRef(s)
+	res = ArchiveExtractionStatusFromRef(s)
 	assert.Nil(t, res)
 }
 
