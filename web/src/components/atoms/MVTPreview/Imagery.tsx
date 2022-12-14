06fc7dd0a460b28740c2705cb29cc80355d7eac6
nourbalaha <nourbalaha0@gmail.com>
2022-12-14 15:30:51 +0900

    wip: imagery.tsx

---
+++ web/src/components/atoms/MVTPreview/Imagery.tsx
@@ -0,0 +1,46 @@
+import { MVTImageryProvider } from "cesium-mvt-imagery-provider";
+import { FC, useEffect, useState } from "react";
+import { useCesium } from "resium";
+
+// import { ImageryProviderOption } from "./index";
+
+// type Props = {
+//   imageryOption: ImageryProviderOption;
+// };
+export const Imagery: FC = () => {
+  const { viewer } = useCesium();
+  const [isFeatureSelected, setIsFeatureSelected] = useState<boolean>(false);
+  useEffect(() => {
+    const imageryProvider = new MVTImageryProvider({
+      urlTemplate: "http://localhost:8080/sample_mvt/{z}/{x}/{y}.mvt",
+      layerName: "layerName",
+      style: _feature => {
+        if (isFeatureSelected) {
+          return {
+            strokeStyle: "orange",
+            fillStyle: "orange",
+            lineWidth: 1,
+          };
+        }
+        return {
+          strokeStyle: "green",
+          fillStyle: "green",
+          lineWidth: 1,
+        };
+      },
+      onSelectFeature: _feature => {
+        setIsFeatureSelected(v => !v);
+      },
+      credit: "cesium.js",
+    });
+
+    const layers = viewer.scene.imageryLayers;
+    const currentLayer = layers.addImageryProvider(imageryProvider);
+    currentLayer.alpha = 0.5;
+
+    return () => {
+      layers.remove(currentLayer);
+    };
+  }, [viewer, isFeatureSelected]);
+  return <div />;
+};
