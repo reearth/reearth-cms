b1ecfea9de71b9766fd4e1a3eb441b1af00ce649
basel.issmail <basel.issmail@gmail.com>
2022-12-14 06:13:46 +0300

    Add requests routing

---
+++ web/src/components/organisms/Project/Request/RequestList/index.tsx
@@ -0,0 +1,12 @@
+// import RequestDetailsMolecule from "@reearth-cms/components/molecules/Request/Details";
+
+// import useHooks from "./hooks";
+
+const RequestList: React.FC = () => {
+  //   const { currentRequest } = useHooks();
+
+  return <h1>Details</h1>;
+  //   return <RequestDetailsMolecule currentRequest={currentRequest} />;
+};
+
+export default RequestList;
