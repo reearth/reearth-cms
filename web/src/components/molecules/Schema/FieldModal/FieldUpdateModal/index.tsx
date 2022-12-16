76d72ffc3c2f7a5964bc65c33c174184b9f9275a
Nour Balaha <nourbalaha0@gmail.com>
2022-12-16 11:19:01 +0900

    fix(web): integer field min, max and default value (#513)
    * fix: update int field
    
    * fix field creation modal
    
    * null instead of undefined
    
    * InputNumber instead of Input
    
    * fix: add type to change event

--- web/src/components/molecules/Schema/FieldModal/FieldUpdateModal/index.tsx
+++ web/src/components/molecules/Schema/FieldModal/FieldUpdateModal/index.tsx
@@ -165,7 +165,11 @@ const FieldUpdateModal: React.FC<Props> = ({
           };
         } else if (selectedType === "Integer") {
           values.typeProperty = {
-            integer: { defaultValue: +values.defaultValue, min: +values.min, max: +values.max },
+            integer: {
+              defaultValue: values.defaultValue ?? null,
+              min: values.min ?? null,
+              max: values.max ?? null,
+            },
           };
         } else if (selectedType === "URL") {
           values.typeProperty = {
