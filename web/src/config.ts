e655a55f950e8fed82c7c6dc2948cbb70c386179
HideBa <baba.papa1120.ba@gmail.com>
2022-12-19 09:54:14 +0900

    fix(web): default value of config

--- web/src/config.ts
+++ web/src/config.ts
@@ -14,9 +14,8 @@ export const defaultConfig: Config = {
   auth0Audience: env.REEARTH_CMS_AUTH0_AUDIENCE || "https://api.test.reearth.dev",
   auth0Domain: env.REEARTH_CMS_AUTH0_DOMAIN || "reearth-oss-test.eu.auth0.com",
   auth0ClientId: env.REEARTH_CMS_AUTH0_CLIENT_ID || "k6F1sgFikzVkkcW9Cpz7Ztvwq5cBRXlv",
-  logoUrl: env.REEARTH_CMS_LOGO_URL || "https://reearth.io/img/logo.svg",
-  coverImageUrl:
-    env.REEARTH_CMS_COVER_URL || "https://cms.plateau.reearth.io/assets/cover-image.png",
+  logoUrl: env.REEARTH_CMS_LOGO_URL,
+  coverImageUrl: env.REEARTH_CMS_COVER_URL,
 };
 
 export default async function loadConfig() {
