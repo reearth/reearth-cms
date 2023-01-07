065beb9e88b44a26b88dc120f78afc722f3a3250
basel.issmail <basel.issmail@gmail.com>
2023-01-08 02:45:27 +0400

    Add delete update comments hooks

--- web/src/components/organisms/Common/CommentsPanel/hooks.ts
+++ web/src/components/organisms/Common/CommentsPanel/hooks.ts
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 
 import Notification from "@reearth-cms/components/atoms/Notification";
-import { useAddCommentMutation } from "@reearth-cms/gql/graphql-client-api";
+import { useAddCommentMutation, useDeleteCommentMutation, useUpdateCommentMutation } from "@reearth-cms/gql/graphql-client-api";
 import { useT } from "@reearth-cms/i18n";
 
 type Params = {
@@ -33,7 +33,55 @@ export default ({ threadId }: Params) => {
     [createComment, threadId, t],
   );
 
+  const [updateComment] = useUpdateCommentMutation({
+    refetchQueries: ["GetAsset", "GetAssets", "GetItems", "GetRequests"],
+  });
+
+  const handleCommentUpdate = useCallback(
+    async (commentId: string, content: string) => {
+      if (!threadId) return;
+      const comment = await updateComment({
+        variables: {
+          threadId,
+          commentId,
+          content
+        },
+      });
+      if (comment.errors || !comment.data?.updateComment) {
+        Notification.error({ message: t("Failed to update comment.") });
+        return;
+      }
+      Notification.success({ message: t("Successfully updated comment!") });
+    },
+    [updateComment, threadId, t],
+  );
+
+  const [deleteComment] = useDeleteCommentMutation({
+    refetchQueries: ["GetAsset", "GetAssets", "GetItems", "GetRequests"],
+  });
+
+  const handleCommentDelete = useCallback(
+    async (commentId: string) => {
+      if (!threadId) return;
+      const comment = await deleteComment({
+        variables: {
+          threadId,
+          commentId
+        },
+      });
+      if (comment.errors || !comment.data?.deleteComment) {
+        Notification.error({ message: t("Failed to delete comment.") });
+        return;
+      }
+      Notification.success({ message: t("Successfully deleted comment!") });
+    },
+    [deleteComment, threadId, t],
+  );
+
+
   return {
     handleCommentCreate,
+    handleCommentUpdate,
+    handleCommentDelete
   };
 };
