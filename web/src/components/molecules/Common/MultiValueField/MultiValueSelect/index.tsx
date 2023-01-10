d1195b57adb1fe7002b3fd443fe2a7ab15cdf21b
basel.issmail <basel.issmail@gmail.com>
2023-01-11 02:08:55 +0400

    Change request UI

--- web/src/components/molecules/Common/MultiValueField/MultiValueSelect/index.tsx
+++ web/src/components/molecules/Common/MultiValueField/MultiValueSelect/index.tsx
@@ -1,9 +1,10 @@
+import { PropertySafetyFilled } from "@ant-design/icons";
 import styled from "@emotion/styled";
 import { useCallback } from "react";
 
 import Button from "@reearth-cms/components/atoms/Button";
 import Icon from "@reearth-cms/components/atoms/Icon";
-import Select from "@reearth-cms/components/atoms/Select";
+import Select, { SelectProps } from "@reearth-cms/components/atoms/Select";
 import { useT } from "@reearth-cms/i18n";
 
 import { moveItemInArray } from "../moveItemArray";
@@ -13,9 +14,16 @@ type Props = {
   selectedValues: string[];
   value?: string[];
   onChange?: (value: string[]) => void;
+  disabled?: boolean;
 };
 
-const MultiValueSelect: React.FC<Props> = ({ className, selectedValues, value = [], onChange }) => {
+const MultiValueSelect: React.FC<Props> = ({
+  className,
+  selectedValues,
+  value = [],
+  onChange,
+  disabled,
+}) => {
   const t = useT();
   const { Option } = Select;
   const handleInput = useCallback(
@@ -41,18 +49,22 @@ const MultiValueSelect: React.FC<Props> = ({ className, selectedValues, value = 
       {Array.isArray(value) &&
         value?.map((valueItem, key) => (
           <FieldWrapper key={key}>
-            <FieldButton
-              type="link"
-              icon={<Icon icon="arrowUp" />}
-              onClick={() => onChange?.(moveItemInArray(value, key, key - 1))}
-              disabled={key === 0}
-            />
-            <FieldButton
-              type="link"
-              icon={<Icon icon="arrowDown" />}
-              onClick={() => onChange?.(moveItemInArray(value, key, key + 1))}
-              disabled={key === value.length - 1}
-            />
+            {!disabled && (
+              <>
+                <FieldButton
+                  type="link"
+                  icon={<Icon icon="arrowUp" />}
+                  onClick={() => onChange?.(moveItemInArray(value, key, key - 1))}
+                  disabled={key === 0}
+                />
+                <FieldButton
+                  type="link"
+                  icon={<Icon icon="arrowDown" />}
+                  onClick={() => onChange?.(moveItemInArray(value, key, key + 1))}
+                  disabled={key === value.length - 1}
+                />
+              </>
+            )}
             <Select
               style={{ flex: 1 }}
               value={valueItem}
@@ -63,22 +75,26 @@ const MultiValueSelect: React.FC<Props> = ({ className, selectedValues, value = 
                 </Option>
               ))}
             </Select>
-            <FieldButton
-              type="link"
-              icon={<Icon icon="delete" />}
-              onClick={() => handleInputDelete(key)}
-            />
+            {!disabled && (
+              <FieldButton
+                type="link"
+                icon={<Icon icon="delete" />}
+                onClick={() => handleInputDelete(key)}
+              />
+            )}
           </FieldWrapper>
         ))}
-      <Button
-        icon={<Icon icon="plus" />}
-        type="primary"
-        onClick={() => {
-          if (!value) value = [];
-          onChange?.([...value, ""]);
-        }}>
-        {t("New")}
-      </Button>
+      {!disabled && (
+        <Button
+          icon={<Icon icon="plus" />}
+          type="primary"
+          onClick={() => {
+            if (!value) value = [];
+            onChange?.([...value, ""]);
+          }}>
+          {t("New")}
+        </Button>
+      )}
     </div>
   );
 };
