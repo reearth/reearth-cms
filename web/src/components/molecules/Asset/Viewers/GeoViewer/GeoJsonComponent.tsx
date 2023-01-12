7326153bcecd8a0e8cb5f7ec0af5b18a8a10a058
nourbalaha <nourbalaha0@gmail.com>
2023-01-12 22:03:50 +0900

    wip: info box

--- web/src/components/molecules/Asset/Viewers/GeoViewer/GeoJsonComponent.tsx
+++ web/src/components/molecules/Asset/Viewers/GeoViewer/GeoJsonComponent.tsx
@@ -1,14 +1,25 @@
-import { GeoJsonDataSource } from "cesium";
-import { ComponentProps } from "react";
-import { GeoJsonDataSource as ResiumGeoJsonDataSource, useCesium } from "resium";
+import { GeoJsonDataSource, JulianDate } from "cesium";
+import { ComponentProps, useState } from "react";
+import { JSONTree } from "react-json-tree";
+import {
+  CesiumMovementEvent,
+  GeoJsonDataSource as ResiumGeoJsonDataSource,
+  useCesium,
+} from "resium";
 
 type Props = ComponentProps<typeof ResiumGeoJsonDataSource>;
 
-const GeoJsonComponent: React.FC<Props> = ({ data }) => {
+const GeoJsonComponent: React.FC<Props> = () => {
   const { viewer } = useCesium();
+  const [dataSource, setDataSource] = useState<GeoJsonDataSource>();
+  const [infoBoxProps, setInfoBoxProps] = useState({});
+  const [infoBoxVisibility, setInfoBoxVisibility] = useState<boolean>(false);
 
   const onLoad = async (ds: GeoJsonDataSource) => {
+    setDataSource(ds);
     try {
+      await viewer?.dataSources.removeAll();
+      await viewer?.dataSources.add(ds);
       await viewer?.zoomTo(ds);
       ds.show = true;
     } catch (error) {
@@ -16,7 +27,29 @@ const GeoJsonComponent: React.FC<Props> = ({ data }) => {
     }
   };
 
-  return <ResiumGeoJsonDataSource data={data} onLoad={onLoad} />;
+  const handleClick = (_movement: CesiumMovementEvent, target: any) => {
+    const id = target?.id.id;
+    const entity = dataSource?.entities.getById(id);
+    const now = JulianDate.now();
+    const props = entity?.properties?.getValue(now);
+    setInfoBoxProps(props);
+    setInfoBoxVisibility(true);
+  };
+
+  return (
+    <div style={{ position: "absolute" }}>
+      <ResiumGeoJsonDataSource
+        data="http://localhost:8080/assets/29/242ad5-264a-4695-bddb-be558c68b7e6/sample.geojson"
+        onLoad={onLoad}
+        onClick={handleClick}
+      />
+      {infoBoxVisibility && (
+        <div style={{ position: "relative", top: 0, right: 0 }}>
+          <JSONTree data={infoBoxProps} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default GeoJsonComponent;
