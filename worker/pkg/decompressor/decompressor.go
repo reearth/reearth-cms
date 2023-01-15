13bf092b85d0c4db773772a8acff54ca65c90495
pyshx <piyushchauhan1004@gmail.com>
2023-01-16 01:17:38 +0530

    chore: remove numWorkers and queueSize from `.env`
    After a testing session on the cloud run deployment we decided to take the numWorkes as 500 and workerQueueSize as 20k, any more would have taken more memory & less would have increase the decompression time.

--- worker/pkg/decompressor/decompressor.go
+++ worker/pkg/decompressor/decompressor.go
@@ -29,6 +29,11 @@ const (
 	configPrefix     = "REEARTH_CMS_WORKER"
 )
 
+const (
+	workersNumber    = 500
+	workerQueueDepth = 20000
+)
+
 type decompressor struct {
 	zr  *zip.Reader
 	sr  *sevenzip.Reader
@@ -115,12 +120,13 @@ func (uz *decompressor) readConcurrentGCSFile(zfs []*zip.File, assetBasePath str
 	if cerr != nil {
 		log.Fatal(cerr)
 	}
-	var wg sync.WaitGroup  
+	var wg sync.WaitGroup
 	ctx := context.Background()
 	client, _ := storage.NewClient(ctx)
 	db := client.Bucket(conf.BucketName)
-	workQueue := make(chan *zip.File, conf.DecompressorWorkerQueueDepth)
-	for i := 0; i < int(conf.DecompressionWorkers); i++ {
+	workQueue := make(chan *zip.File, workerQueueDepth)
+	assetBasePath, _ = filepath.Split(assetBasePath)
+	for i := 0; i < workersNumber; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
@@ -159,9 +165,7 @@ func (uz *decompressor) readConcurrentGCSFile(zfs []*zip.File, assetBasePath str
 }
 
 type DecompressorConfig struct {
-	DecompressionWorkers         int64  `envconfig:"DECOMPRESSION_NUM_WORKERS"`
-	DecompressorWorkerQueueDepth int64  `envconfig:"DECOMPRESSION_WORKQUEUE_DEPTH"`	
-	BucketName                   string `envconfig:"GCS_BUCKET_NAME"`
+	BucketName string `envconfig:"GCS_BUCKET_NAME"`
 }
 
 func ReadDecompressorConfig() (*DecompressorConfig, error) {
