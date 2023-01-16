1884ec635ac20e69f12424ab8560600a10c8b8e7
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2023-01-16 15:52:32 +0300

    fix

--- server/pkg/item/sort_test.go
+++ server/pkg/item/sort_test.go
@@ -9,5 +9,5 @@ import (
 func TestSortTypeFrom(t *testing.T) {
 	assert.Equal(t, SortTypeCreationDate, SortTypeFrom("creation_date"))
 	assert.Equal(t, SortTypeModificationDate, SortTypeFrom("modification_date"))
-	assert.Equal(t, SortType(""), SortTypeFrom("xxx"))
+	assert.Equal(t, SortTypeModificationDate, SortTypeFrom("xxx"))
 }
