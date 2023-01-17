dc2e60f1ace32c1e58442e87d0323e4fe23bdda4
Nour Balaha <nourbalaha0@gmail.com>
2023-01-17 10:20:33 +0900

    feat(web): implement custom infobox (#573)
    * yarn add "react-json-tree"
    
    * wip: info box
    
    * Squashed commit of the following:
    
    commit fd896a7757d423626c2d66213ff87540747acc58
    Author: nourbalaha <nourbalaha0@gmail.com>
    Date:   Sun Jan 15 17:50:15 2023 +0900
    
        add TileCoordinates type
    
    commit 86ee93aec6ef096ba002804582f7e85dfbb02ccf
    Author: nourbalaha <nourbalaha0@gmail.com>
    Date:   Sun Jan 15 15:37:20 2023 +0900
    
        added a todo
    
    commit 7cf073cbc117cfe4680f7c014a409bdf43948d07
    Merge: 2996f366 f81d8f69
    Author: Nour Balaha <nourbalaha0@gmail.com>
    Date:   Sun Jan 15 15:30:06 2023 +0900
    
        Merge branch 'main' into refactor/resium-viewer
    
    commit 2996f366484aa7bbe814919d6a0655c08edcd7b0
    Author: nourbalaha <nourbalaha0@gmail.com>
    Date:   Fri Jan 13 20:22:22 2023 +0900
    
        add todo
    
    commit d35db544f8e1a5c60a16c4d8a9906c6644095737
    Author: nourbalaha <nourbalaha0@gmail.com>
    Date:   Fri Jan 13 20:18:44 2023 +0900
    
        add useCallback
    
    commit 22faf86ec0f137b26890f0473c59611d50e01ded
    Author: nourbalaha <nourbalaha0@gmail.com>
    Date:   Fri Jan 13 17:05:29 2023 +0900
    
        created one viewer component
    
    * custom infoBox for geo viewer
    
    * wip: standalone infobox
    
    * style: used styled components on info box
    
    * removed unwanted code
    
    * removed hard codded urls
    
    * connect infobox with resium viewer
    
    * refactor
    
    * fix infobox style
    
    * mvt property
    
    * fix lint
    
    Co-authored-by: hideba <baba.papa1120.ba@gmail.com>
    Co-authored-by: HideBa <49897538+HideBa@users.noreply.github.com>

--- web/src/components/atoms/ResiumViewer/index.tsx
+++ web/src/components/atoms/ResiumViewer/index.tsx
@@ -1,37 +1,89 @@
-import { createWorldTerrain, Viewer } from "cesium";
-import { ComponentProps } from "react";
-import { Viewer as RViewer } from "resium";
+import { Cesium3DTileFeature, createWorldTerrain, Viewer, JulianDate, Entity } from "cesium";
+import { ComponentProps, useCallback, useState } from "react";
+import { CesiumMovementEvent, RootEventTarget, Viewer as RViewer } from "resium";
+
+import InfoBox from "@reearth-cms/components/molecules/Asset/InfoBox";
 
 type Props = {
   onGetViewer: (viewer: Viewer | undefined) => void;
   children?: React.ReactNode;
+  properties?: any;
+  entitySelected?: boolean;
 } & ComponentProps<typeof RViewer>;
 
-const ResiumViewer: React.FC<Props> = ({ onGetViewer, children, ...props }) => {
+const ResiumViewer: React.FC<Props> = ({
+  onGetViewer,
+  children,
+  properties: passedProps,
+  entitySelected,
+  ...props
+}) => {
   let viewer: Viewer | undefined;
+  const [properties, setProperties] = useState<any>();
+  const [infoBoxVisibility, setInfoBoxVisibility] = useState(false);
+  const [title, setTitle] = useState("");
+
+  const handleClick = useCallback((_movement: CesiumMovementEvent, target: RootEventTarget) => {
+    if (!target) {
+      setProperties(undefined);
+      return;
+    }
+
+    let props: any = {};
+    if (target instanceof Cesium3DTileFeature) {
+      const propertyIds = target.getPropertyIds();
+      const length = propertyIds.length;
+      for (let i = 0; i < length; ++i) {
+        const propertyId = propertyIds[i];
+        props[propertyId] = target.getProperty(propertyId);
+      }
+      setTitle(props["name"]);
+    } else if (target.id instanceof Entity) {
+      const entity = target.id;
+      setTitle(entity.id);
+      props = entity.properties?.getValue(JulianDate.now());
+    }
+
+    setInfoBoxVisibility(true);
+    setProperties(props);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setInfoBoxVisibility(false);
+  }, []);
 
   return (
-    <RViewer
-      terrainProvider={createWorldTerrain()}
-      navigationHelpButton={false}
-      homeButton={false}
-      projectionPicker={false}
-      sceneModePicker={false}
-      baseLayerPicker={false}
-      fullscreenButton={false}
-      vrButton={false}
-      selectionIndicator={false}
-      timeline={false}
-      animation={false}
-      geocoder={false}
-      shouldAnimate={true}
-      ref={e => {
-        viewer = e?.cesiumElement;
-        onGetViewer(viewer);
-      }}
-      {...props}>
-      {children}
-    </RViewer>
+    <div style={{ position: "relative" }}>
+      <RViewer
+        terrainProvider={createWorldTerrain()}
+        navigationHelpButton={false}
+        homeButton={false}
+        projectionPicker={false}
+        sceneModePicker={false}
+        baseLayerPicker={false}
+        fullscreenButton={false}
+        vrButton={false}
+        selectionIndicator={false}
+        timeline={false}
+        animation={false}
+        geocoder={false}
+        shouldAnimate={true}
+        onClick={handleClick}
+        infoBox={false}
+        ref={e => {
+          viewer = e?.cesiumElement;
+          onGetViewer(viewer);
+        }}
+        {...props}>
+        {children}
+      </RViewer>
+      <InfoBox
+        infoBoxProps={passedProps ?? properties}
+        infoBoxVisibility={infoBoxVisibility || !!entitySelected}
+        title={title}
+        onClose={handleClose}
+      />
+    </div>
   );
 };
 
