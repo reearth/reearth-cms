f5cf040f7fb125677f928f59f1f5a23bb966b426
rot1024 <aayhrot@gmail.com>
2023-01-17 18:16:14 +0900

    feat(worker): prevent webhook resend (#594)

--- worker/internal/usecase/interactor/webhook.go
+++ worker/internal/usecase/interactor/webhook.go
@@ -4,8 +4,25 @@ import (
 	"context"
 
 	"github.com/reearth/reearth-cms/worker/pkg/webhook"
+	"github.com/reearth/reearthx/log"
 )
 
 func (u *Usecase) SendWebhook(ctx context.Context, w *webhook.Webhook) error {
-	return webhook.Send(ctx, w)
+	eid := w.EventID
+	found, err := u.webhook.Get(ctx, eid)
+	if err != nil {
+		log.Errorf("webhook usecase: failed to get webhook sent: %v", err)
+	}
+	if found {
+		return nil
+	}
+
+	if err := webhook.Send(ctx, w); err != nil {
+		return err
+	}
+
+	if err := u.webhook.Set(ctx, eid); err != nil {
+		log.Errorf("webhook usecase: failed to set webhook sent: %v", err)
+	}
+	return nil
 }
