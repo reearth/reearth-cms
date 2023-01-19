f71069bbcc4d7741fcd408af2b4f058ddcb96d66
mimoham24 <69579255+mimoham24@users.noreply.github.com>
2023-01-19 14:10:44 +0400

    feat(server): update multiple fields (#570)
    * add updateFieldsOrder mutation
    
    * remove duplicated schema check
    
    * updateOrders -> updateFields
    
    * Update server/internal/usecase/interactor/schema.go
    
    Co-authored-by: rot1024 <aayhrot@gmail.com>
    
    Co-authored-by: rot1024 <aayhrot@gmail.com>

--- server/internal/usecase/interactor/schema.go
+++ server/internal/usecase/interactor/schema.go
@@ -88,53 +88,9 @@ func (i Schema) UpdateField(ctx context.Context, param interfaces.UpdateFieldPar
 			return nil, interfaces.ErrFieldNotFound
 		}
 
-		if param.TypeProperty != nil {
-			if param.DefaultValue != nil {
-				_ = f.SetDefaultValue(nil)
-			}
-			if err := f.SetTypeProperty(param.TypeProperty); err != nil {
-				return nil, err
-			}
+		if err := updateField(param, f); err != nil {
+			return nil, err
 		}
-
-		if param.DefaultValue != nil {
-			if err := f.SetDefaultValue(param.DefaultValue); err != nil {
-				return nil, err
-			}
-		}
-
-		if param.Key != nil {
-			k := key.New(*param.Key)
-			if !k.IsValid() {
-				return nil, interfaces.ErrInvalidKey
-			}
-			f.SetKey(k)
-		}
-
-		if param.Name != nil {
-			f.SetName(*param.Name)
-		}
-
-		if param.Description != nil {
-			f.SetDescription(*param.Description)
-		}
-
-		if param.Order != nil {
-			f.SetOrder(*param.Order)
-		}
-
-		if param.Required != nil {
-			f.SetRequired(*param.Required)
-		}
-
-		if param.Unique != nil {
-			f.SetUnique(*param.Unique)
-		}
-
-		if param.Multiple != nil {
-			f.SetMultiple(*param.Multiple)
-		}
-
 		if err := i.repos.Schema.Save(ctx, s); err != nil {
 			return nil, err
 		}
@@ -158,4 +114,82 @@ func (i Schema) DeleteField(ctx context.Context, schemaId id.SchemaID, fieldID i
 			s.RemoveField(fieldID)
 			return i.repos.Schema.Save(ctx, s)
 		})
+}
+
+func (i Schema) UpdateFields(ctx context.Context, sid id.SchemaID, params []interfaces.UpdateFieldParam, operator *usecase.Operator) (schema.FieldList, error) {
+	return Run1(ctx, operator, i.repos, Usecase().Transaction(), func() (schema.FieldList, error) {
+		s, err := i.repos.Schema.FindByID(ctx, sid)
+		if err != nil {
+			return nil, err
+		}
+		if !operator.IsMaintainingProject(s.Project()) {
+			return nil, interfaces.ErrOperationDenied
+		}
+
+		for _, param := range params {
+			f := s.Field(param.FieldId)
+			if f == nil {
+				return nil, interfaces.ErrFieldNotFound
+			}
+			err = updateField(param, f)
+			if err != nil {
+				return nil, err
+			}
+		}
+		if err := i.repos.Schema.Save(ctx, s); err != nil {
+			return nil, err
+		}
+
+		return nil, nil
+	})
+}
+
+func updateField(param interfaces.UpdateFieldParam, f *schema.Field) error {
+	if param.TypeProperty != nil {
+		if param.DefaultValue != nil {
+			_ = f.SetDefaultValue(nil)
+		}
+		if err := f.SetTypeProperty(param.TypeProperty); err != nil {
+			return err
+		}
+	}
+
+	if param.DefaultValue != nil {
+		if err := f.SetDefaultValue(param.DefaultValue); err != nil {
+			return err
+		}
+	}
+
+	if param.Key != nil {
+		k := key.New(*param.Key)
+		if !k.IsValid() {
+			return interfaces.ErrInvalidKey
+		}
+		f.SetKey(k)
+	}
+
+	if param.Name != nil {
+		f.SetName(*param.Name)
+	}
+
+	if param.Description != nil {
+		f.SetDescription(*param.Description)
+	}
+
+	if param.Order != nil {
+		f.SetOrder(*param.Order)
+	}
+
+	if param.Required != nil {
+		f.SetRequired(*param.Required)
+	}
+
+	if param.Unique != nil {
+		f.SetUnique(*param.Unique)
+	}
+
+	if param.Multiple != nil {
+		f.SetMultiple(*param.Multiple)
+	}
+	return nil
 }
