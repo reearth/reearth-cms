61f6ede9608b059df0e5735e99ce8d50563e606a
nourbalaha <nourbalaha0@gmail.com>
2023-01-25 17:52:25 +0900

    fix 2

--- web/src/components/molecules/Asset/Viewers/Geo3dViewer/index.tsx
+++ web/src/components/molecules/Asset/Viewers/Geo3dViewer/index.tsx
@@ -1,8 +1,7 @@
 import { Viewer as CesiumViewer } from "cesium";
-import { ComponentProps, useEffect } from "react";
+import { ComponentProps } from "react";
 
 import ResiumViewer from "@reearth-cms/components/atoms/ResiumViewer";
-import { compressedFileFormats } from "@reearth-cms/components/molecules/Common/Asset";
 
 import Cesium3dTileSetComponent from "./Cesium3dTileSetComponent";
 
@@ -14,20 +13,12 @@ type Props = {
   setAssetUrl: (url: string) => void;
 };
 
-const Geo3dViewer: React.FC<Props> = ({
-  viewerProps,
-  url,
-  setAssetUrl,
-  assetFileExt,
-  onGetViewer,
-}) => {
-  useEffect(() => {
-    if (assetFileExt && compressedFileFormats.includes(assetFileExt)) {
-      const nameRegex = /\.\w+$/;
-      const base = url.replace(nameRegex, "");
-      setAssetUrl(`${base}/tileset.json`);
-    }
-  });
+const Geo3dViewer: React.FC<Props> = ({ viewerProps, url, setAssetUrl, onGetViewer }) => {
+  const nameRegex = /\.zip|7z$/;
+  if (url.match(nameRegex)) {
+    const base = url.replace(nameRegex, "");
+    setAssetUrl(`${base}/tileset.json`);
+  }
 
   return (
     <ResiumViewer {...viewerProps} onGetViewer={onGetViewer}>
