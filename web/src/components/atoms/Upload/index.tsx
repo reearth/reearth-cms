8442edfd4d68b5c026260a212206233c8c538788
Basel Issmail <78056580+issmail-basel@users.noreply.github.com>
2023-01-30 09:48:20 +0300

    feat(web): add skip compression (#624)
    * Add gql changes
    
    * Add state to list
    
    * Add file upload card
    
    * Add translations
    
    * Add changes
    
    * Save
    
    * Save
    
    * Save
    
    * Update web/src/i18n/translations/ja.yml
    
    * Update web/src/i18n/translations/ja.yml
    
    ---------
    
    Co-authored-by: basel.issmail <basel.issmail@gmail.com>
    Co-authored-by: HideBa <49897538+HideBa@users.noreply.github.com>

--- web/src/components/atoms/Upload/index.tsx
+++ web/src/components/atoms/Upload/index.tsx
@@ -1,5 +1,13 @@
 import { Upload } from "antd";
-import { UploadChangeParam, UploadFile, UploadProps } from "antd/lib/upload/interface";
+import {
+  UploadChangeParam,
+  UploadFile as ANTDFileUpload,
+  UploadProps,
+} from "antd/lib/upload/interface";
+
+interface UploadFile<T = any> extends ANTDFileUpload<T> {
+  skipDecompression?: boolean;
+}
 
 export default Upload;
 export type { UploadChangeParam, UploadFile, UploadProps };
