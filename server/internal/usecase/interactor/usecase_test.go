b864096db93616635884e7d877462465f8b27573
awakot <kawamura.estyle@gmail.com>
2023-05-22 09:52:37 +0900

    fix(sever): add test

--- server/internal/usecase/interactor/usecase_test.go
+++ server/internal/usecase/interactor/usecase_test.go
@@ -98,6 +98,8 @@ func TestUc(t *testing.T) {
 	assert.Equal(t, &uc{}, Usecase())
 	assert.Equal(t, &uc{readableWorkspaces: workspaces}, (&uc{}).WithReadableWorkspaces(workspaces...))
 	assert.Equal(t, &uc{writableWorkspaces: workspaces}, (&uc{}).WithWritableWorkspaces(workspaces...))
+	assert.Equal(t, &uc{maintainableWorkspaces: workspaces}, (&uc{}).WithMaintainableWorkspaces(workspaces...))
+	assert.Equal(t, &uc{ownableWorkspaces: workspaces}, (&uc{}).WithOwnableWorkspaces(workspaces...))
 	assert.Equal(t, &uc{tx: true}, (&uc{}).Transaction())
 }
 
