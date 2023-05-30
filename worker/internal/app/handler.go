3ea2652dd944a38d27aaef224e6b38d0745e3172
nourbalaha <nourbalaha0@gmail.com>
2023-05-30 18:38:21 +0900

    update DecompressHandler

--- worker/internal/app/handler.go
+++ worker/internal/app/handler.go
@@ -19,8 +19,36 @@ func NewHandler(c *rhttp.Controller) *Handler {
 	return &Handler{Controller: c}
 }
 
+type SubscriptionConfirmation struct {
+	Type             string `json:"Type"`
+	MessageId        string `json:"MessageId"`
+	Token            string `json:"Token"`
+	TopicArn         string `json:"TopicArn"`
+	Message          string `json:"Message"`
+	SubscribeURL     string `json:"SubscribeURL"`
+	Timestamp        string `json:"Timestamp"`
+	SignatureVersion string `json:"SignatureVersion"`
+	Signature        string `json:"Signature"`
+	SigningCertURL   string `json:"SigningCertURL"`
+}
+
 func (h Handler) DecompressHandler() echo.HandlerFunc {
 	return func(c echo.Context) error {
+		// SNS Subscription Confirmation
+		req := c.Request()
+		t := req.Header.Get("x-amz-sns-message-type")
+		if t == "SubscriptionConfirmation" {
+			var confirmation SubscriptionConfirmation
+			if err := c.Bind(&confirmation); err != nil {
+				log.Errorf("failed to bind request body: %s", err.Error())
+				return err
+			}
+
+			log.Infof("SubscriberURL: %s", confirmation.SubscribeURL)
+			return c.JSON(http.StatusOK, "ok")
+		}
+		
+		// Decompress
 		var input rhttp.DecompressInput
 		if err := c.Bind(&input); err != nil {
 			log.Errorf("failed to decompress: err=%s", err.Error())
