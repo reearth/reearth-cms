9b62b84a7dfd2997c9a076aeb40a334f85dc19bd
nourbalaha <nourbalaha0@gmail.com>
2023-06-29 16:49:38 +0900

    refactor: NotifyHandler

--- server/internal/app/m2m.go
+++ server/internal/app/m2m.go
@@ -11,77 +11,44 @@ import (
 	"github.com/reearth/reearthx/log"
 )
 
-const (
-	MessageHeader            = "X-Amz-Sns-Message-Type"
-	SubscriptionConfirmation = "SubscriptionConfirmation"
-	Notification             = "Notification"
-)
-
 func NotifyHandler() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		switch c.Request().Header.Get(MessageHeader) {
-		case string(SubscriptionConfirmation):
-			return subscriptionConfirmationHandler(c)
-		case string(Notification):
-			return notificationHandler(c)
-		default:
-			return defaultHandler(c)
+		header := c.Request().Header.Get("X-Amz-Sns-Message-Type")
+		var input rhttp.NotifyInput
+		if header == "Notification" {
+			var req struct {
+				Message string
+			}
+			if err :=  json.NewDecoder(c.Request().Body).Decode(&req); err != nil {
+				return err
+			}
+			if err := json.Unmarshal([]byte(req.Message), &input); err != nil {
+				return err
+			}
+		} else {
+			var b pubsubBody
+			if err := c.Bind(&b); err != nil {
+				if err := c.Bind(&input); err != nil {
+					return err
+				}
+			} else if data, err := b.Data(); err != nil {
+				return err
+			} else if err := json.Unmarshal(data, &input); err != nil {
+				return err
+			}
 		}
-	}
-}
+		ctx := c.Request().Context()
 
-func subscriptionConfirmationHandler(c echo.Context) error {
-	var req SubscriptionConfirmationRequest
-	if err := req.Bind(c.Request()); err != nil {
-		return err
-	}
-	log.Infof("SubscribeURL: %#v", req.SubscribeURL)
-	return c.NoContent(http.StatusOK)
-}
-
-func notificationHandler(c echo.Context) error {
-	var req NotificationRequest
-	if err := req.Bind(c.Request()); err != nil {
-		return err
-	}
-	var input rhttp.NotifyInput
-	if err := json.Unmarshal([]byte(req.Message), &input); err != nil {
-		return err
-	}
-	ctx := c.Request().Context()
-	log.Infof("notified and updating files begin: assetID=%s type=%s status=%s", input.AssetID, input.Type, input.Status)
-	assetUC := adapter.Usecases(ctx).Asset
-	controller := rhttp.NewTaskController(assetUC)
-	if err := controller.Notify(ctx, input); err != nil {
-		log.Errorf("failed to update files: assetID=%s, type=%s, status=%s", input.AssetID, input.Type, input.Status)
-		return err
-	}
-	log.Infof("successfully notified and files has been updated: assetID=%s, type=%s, status=%s", input.AssetID, input.Type, input.Status)
-	return c.NoContent(http.StatusOK)
-}
-
-func defaultHandler(c echo.Context) error {
-	var input rhttp.NotifyInput
-	var b pubsubBody
-	if err := c.Bind(&b); err != nil {
-		if err := c.Bind(&input); err != nil {
+		log.Infof("notified and updating files begin: assetID=%s type=%s status=%s", input.AssetID, input.Type, input.Status)
+		assetUC := adapter.Usecases(ctx).Asset
+		controller := rhttp.NewTaskController(assetUC)
+		if err := controller.Notify(ctx, input); err != nil {
+			log.Errorf("failed to update files: assetID=%s, type=%s, status=%s", input.AssetID, input.Type, input.Status)
 			return err
 		}
-	} else if data, err := b.Data(); err != nil {
-		return err
-	} else if err := json.Unmarshal(data, &input); err != nil {
-		return err
+		log.Infof("successfully notified and files has been updated: assetID=%s, type=%s, status=%s", input.AssetID, input.Type, input.Status)
+		return c.JSON(http.StatusOK, "OK")
 	}
-	ctx := c.Request().Context()
-	log.Infof("notified and updating files begin: assetID=%s type=%s status=%s", input.AssetID, input.Type, input.Status)
-	assetUC := adapter.Usecases(ctx).Asset
-	controller := rhttp.NewTaskController(assetUC)
-	if err := controller.Notify(ctx, input); err != nil {
-		log.Errorf("failed to update files: assetID=%s, type=%s, status=%s", input.AssetID, input.Type, input.Status)
-		return err
-	}
-	log.Infof("successfully notified and files has been updated: assetID=%s, type=%s, status=%s", input.AssetID, input.Type, input.Status)
-	return c.NoContent(http.StatusOK)
 }
 
 type pubsubBody struct {
@@ -94,21 +61,6 @@ func (b pubsubBody) Data() ([]byte, error) {
 	if b.Message.Data == "" {
 		return nil, nil
 	}
+
 	return base64.StdEncoding.DecodeString(b.Message.Data)
-}
-
-type SubscriptionConfirmationRequest struct {
-	SubscribeURL string
-}
-
-type NotificationRequest struct {
-	Message string
-}
-
-func (s *SubscriptionConfirmationRequest) Bind(r *http.Request) error {
-	return json.NewDecoder(r.Body).Decode(s)
-}
-
-func (n *NotificationRequest) Bind(r *http.Request) error {
-	return json.NewDecoder(r.Body).Decode(n)
 }
