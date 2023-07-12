2ceefdaaec4e461bb730608cd5e1e5e74a3d5008
Nour Balaha <nourbalaha0@gmail.com>
2023-07-12 12:32:21 +0900

    feat(worker): add AWS support to decompress and webhook handlers (#729)
    * update DecompressHandler
    
    * wip: decompress handler
    
    * refactor DecompressHandler
    
    * fix webhook handler
    
    * fix: return err
    
    * refactor: decompress and webhook handlers
    
    * refactor: SNSMessageHeader
    
    * refactor request
    
    * removed extra line
    
    * refactor the handler
    
    * refactor 2
    
    * refactor: DecompressHandler
    
    * add validation for message signature
    
    * add signature validation for webhook handler
    
    * refactor: decompress and webhook handlers
    
    * fix
    
    * refactor
    
    * use json.NewDecoder instead of c.Bind
    
    * fix: isGCP function

--- worker/internal/app/handler.go
+++ worker/internal/app/handler.go
@@ -3,12 +3,16 @@ package app
 import (
 	"encoding/base64"
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	rhttp "github.com/reearth/reearth-cms/worker/internal/adapter/http"
 	"github.com/reearth/reearth-cms/worker/pkg/webhook"
 	"github.com/reearth/reearthx/log"
+	sns "github.com/robbiet480/go.sns"
 )
 
 type Handler struct {
@@ -22,11 +26,20 @@ func NewHandler(c *rhttp.Controller) *Handler {
 func (h Handler) DecompressHandler() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var input rhttp.DecompressInput
-		if err := c.Bind(&input); err != nil {
-			log.Errorf("failed to decompress: err=%s", err.Error())
+		var err error
+
+		if h.isAWS(c.Request()) {
+			input, err = parseSNSDecompressMessage(c.Request().Body)
+		} else if h.isGCP(c.Request()) {
+			input, err = parsePubSubDecompressMessage(c.Request().Body)
+		} else {
+			err = errors.New("unsupported request source")
+		}
+
+		if err != nil {
+			log.Errorf("failed to parse request body: %s", err.Error())
 			return err
 		}
-		log.Infof("decompression start: Asset=%s, Path=%s", input.AssetID, input.Path)
 
 		if err := h.Controller.DecompressController.Decompress(c.Request().Context(), input); err != nil {
 			log.Errorf("failed to decompress. input: %#v err:%s", input, err.Error())
@@ -39,26 +52,112 @@ func (h Handler) DecompressHandler() echo.HandlerFunc {
 
 func (h Handler) WebhookHandler() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var msg msgBody
-		var w webhook.Webhook
-		if err := c.Bind(&msg); err != nil {
-			if err := c.Bind(&w); err != nil {
-				return err
-			}
-		} else if data, err := msg.Data(); err != nil {
-			return err
-		} else if err := json.Unmarshal(data, &w); err != nil {
+		var webhook webhook.Webhook
+		var err error
+
+		if h.isAWS(c.Request()) {
+			webhook, err = parseSNSWebhookMessage(c.Request().Body)
+		} else if h.isGCP(c.Request()) {
+			webhook, err = parsePubSubWebhookMessage(c.Request().Body)
+		} else {
+			err = errors.New("unsupported request source")
+		}
+
+		if err != nil {
+			log.Errorf("failed to parse request body: %s", err.Error())
 			return err
 		}
 
-		if err := h.Controller.WebhookController.Webhook(c.Request().Context(), &w); err != nil {
-			log.Errorf("failed to send webhook. webhook: %#v err:%s", w, err.Error())
+		if err := h.Controller.WebhookController.Webhook(c.Request().Context(), &webhook); err != nil {
+			log.Errorf("failed to send webhook. webhook: %#v err:%s", webhook, err.Error())
 			return err
 		}
 
-		log.Info("webhook has been sent: %#v", w)
+		log.Infof("webhook has been sent: %#v", webhook)
 		return c.NoContent(http.StatusOK)
 	}
+}
+
+func (h Handler) isAWS(r *http.Request) bool {
+	return r.Header.Get("X-Amz-Sns-Message-Type") == "Notification"
+}
+
+func (h Handler) isGCP(r *http.Request) bool {
+	// TODO: need to find a better way to detect GCP requests
+	for headerName := range r.Header {
+		if strings.HasPrefix(strings.ToLower(headerName), "x-goog-") {
+			return true
+		}
+	}
+	return false
+}
+
+func parseSNSDecompressMessage(body io.Reader) (rhttp.DecompressInput, error) {
+	var payload sns.Payload
+	var input rhttp.DecompressInput
+	
+	if err := json.NewDecoder(body).Decode(&payload); err != nil {
+		return input, err
+	}
+	
+	if err := json.Unmarshal([]byte(payload.Message), &input); err != nil {
+		return input, err
+	}
+	
+	// Validates payload's signature
+	if err := payload.VerifyPayload(); err != nil {
+		return input, err
+	}
+	
+	return input, nil
+}
+
+func parsePubSubDecompressMessage(body io.Reader) (rhttp.DecompressInput, error) {
+	var input rhttp.DecompressInput
+	
+	if err := json.NewDecoder(body).Decode(&input); err != nil {
+		log.Errorf("failed to decompress: err=%s", err.Error())
+		return input, err
+	}
+	
+	return input, nil
+}
+
+func parseSNSWebhookMessage(body io.Reader) (webhook.Webhook, error) {
+	var payload sns.Payload
+	var w webhook.Webhook
+
+	if err := json.NewDecoder(body).Decode(&payload); err != nil {
+		return w, err
+	}
+
+	if err := json.Unmarshal([]byte(payload.Message), &w); err != nil {
+		return w, err
+	}
+
+	// Validates payload's signature
+	if err := payload.VerifyPayload(); err != nil {
+		return w, err
+	}
+
+	return w, nil
+}
+
+func parsePubSubWebhookMessage(body io.Reader) (webhook.Webhook, error) {
+	var msg msgBody
+	var w webhook.Webhook
+
+	if err := json.NewDecoder(body).Decode(&msg); err != nil {
+		if err := json.NewDecoder(body).Decode(&w); err != nil {
+			return w, err
+		}
+	} else if data, err := msg.Data(); err != nil {
+		return w, err
+	} else if err := json.Unmarshal(data, &w); err != nil {
+		return w, err
+	}
+
+	return w, nil
 }
 
 type msgBody struct {
