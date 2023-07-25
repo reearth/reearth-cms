c3a200e4016443601ca648c0fec15c61269c2b35
nourbalaha <nourbalaha0@gmail.com>
2023-07-25 18:03:09 +0900

    readd the copy icon next to decompressed files

--- web/src/components/molecules/Asset/Asset/AssetBody/UnzipFileList.tsx
+++ web/src/components/molecules/Asset/Asset/AssetBody/UnzipFileList.tsx
@@ -45,13 +45,24 @@ const UnzipFileList: React.FC<Props> = ({
         }
 
         return {
-          title: file.name,
+          title: (
+            <>
+              {file.name}
+              <CopyIcon
+                selected={selectedKeys[0] === key}
+                icon="copy"
+                onClick={() => {
+                  navigator.clipboard.writeText(assetBaseUrl + file.path);
+                }}
+              />
+            </>
+          ),
           key: key,
           children: children,
           file: file,
         };
       }) || [],
-    [],
+    [selectedKeys, assetBaseUrl],
   );
 
   useEffect(() => {
@@ -130,6 +141,14 @@ const ExtractionFailedWrapper = styled.div`
 
 const ExtractionFailedIcon = styled(Icon)`
   margin-bottom: 28px;
+`;
+
+const CopyIcon = styled(Icon)<{ selected?: boolean }>`
+  margin-left: 16px;
+  visibility: ${({ selected }) => (selected ? "visible" : "hidden")};
+  &:active {
+    color: #096dd9;
+  }
 `;
 
 const ExtractionFailedText = styled.p`
