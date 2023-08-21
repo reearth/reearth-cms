254733762b91cc43ef37bd1a0800a5c00924ce75
nourbalaha <nourbalaha0@gmail.com>
2023-08-21 17:06:51 +0900

    fix: add TypeCheckbox to ToValueType

--- server/internal/adapter/gql/gqlmodel/convert_value.go
+++ server/internal/adapter/gql/gqlmodel/convert_value.go
@@ -18,6 +18,8 @@ func ToValueType(t value.Type) SchemaFieldType {
 		return SchemaFieldTypeDate
 	case value.TypeBool:
 		return SchemaFieldTypeBool
+	case value.TypeCheckbox:
+		return SchemaFieldTypeCheckbox
 	case value.TypeSelect:
 		return SchemaFieldTypeSelect
 	case value.TypeNumber:
