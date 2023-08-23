b21588d9a9046684a40971aceb7ffd016ac35f72
Nour Balaha <nourbalaha0@gmail.com>
2023-08-23 18:10:22 +0900

    feat(server): publish multiple items at once (#799)
    * feat: publish multiple items at once
    
    * pr fixes
    
    * add space between test cases
    
    * fix: lo.EveryBy condition

--- server/internal/usecase/interfaces/item.go
+++ server/internal/usecase/interfaces/item.go
@@ -62,6 +62,6 @@ type Item interface {
 	Create(context.Context, CreateItemParam, *usecase.Operator) (item.Versioned, error)
 	Update(context.Context, UpdateItemParam, *usecase.Operator) (item.Versioned, error)
 	Delete(context.Context, id.ItemID, *usecase.Operator) error
-	PublishOneItem(context.Context, id.ItemID, *usecase.Operator) (item.Versioned, error)
+	Publish(context.Context, id.ItemIDList, *usecase.Operator) (item.VersionedList, error)
 	Unpublish(context.Context, id.ItemIDList, *usecase.Operator) (item.VersionedList, error)
 }
