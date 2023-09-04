51a91d39159c2bc37066d33dbf388e75ed3ff3dc
issmail.basel <issmail.basel@gmail.com>
2023-09-04 08:36:43 +0300

    comment out check item reference

--- web/src/components/molecules/Content/LinkItemModal/hooks.ts
+++ web/src/components/molecules/Content/LinkItemModal/hooks.ts
@@ -1,19 +1,21 @@
 import { useCallback } from "react";
 
-import { useCheckIfItemIsReferencedLazyQuery } from "@reearth-cms/gql/graphql-client-api";
+// import { useCheckIfItemIsReferencedLazyQuery } from "@reearth-cms/gql/graphql-client-api";
 
 export default () => {
-  const [checkIfItemIsReferenced, { data }] = useCheckIfItemIsReferencedLazyQuery({
-    fetchPolicy: "no-cache",
-  });
+  // const [checkIfItemIsReferenced, { data }] = useCheckIfItemIsReferencedLazyQuery({
+  //   fetchPolicy: "no-cache",
+  // });
 
-  const handleCheckItemReference = useCallback(
-    async (value?: string) => await checkIfItemIsReferenced({ variables: { itemId: value ?? "" } }),
-    [checkIfItemIsReferenced],
-  );
+  // const handleCheckItemReference = useCallback(
+  //   async (value?: string) => await checkIfItemIsReferenced({ variables: { itemId: value ?? "" } }),
+  //   [checkIfItemIsReferenced],
+  // );
+
+  const handleCheckItemReference = useCallback(async (value?: string) => true, []);
 
   return {
-    isReferenced: data?.checkIfItemIsReferenced,
+    isReferenced: true, // data?.checkIfItemIsReferenced,
     handleCheckItemReference,
   };
 };
