c764b84d05b402bddf79dd145959340ce7db8021
issmail.basel <issmail.basel@gmail.com>
2023-09-06 22:20:31 +0300

    Fix linting

--- web/src/components/molecules/Schema/index.tsx
+++ web/src/components/molecules/Schema/index.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 
 import Icon from "@reearth-cms/components/atoms/Icon";
 import ComplexInnerContents from "@reearth-cms/components/atoms/InnerContents/complex";
