df8f316ec8123906f611038cab3bf61094b3214f
caichi <54824604+caichi-t@users.noreply.github.com>
2023-11-08 01:17:22 +0900

    fix(web): Saving filter conditions to views (#933)
    * changed the view type
    
    * created the currentView state
    
    * added columns field to currentView
    
    * completed sorting and columns with views menu
    
    * deleted server changes
    
    * removed useless codes
    
    * removed useless codes
    
    * updated translation files
    
    * removed useless codes
    
    * fixed type bug
    
    * fixed type bug
    
    * fixed type bug
    
    * Fix the issue of modal closing
    
    * Hide the input field when Selecting is(Not)Empty
    
    * Fix changing the sorting is removing the filters
    
    * Add placeholder and fix inputNumber width
    
    * Remove filter param from URL
    
    * Add filter condition to creating view
    
    * changed the graphQL
    
    * Add saving filter and fix some issues
    
    * Fix conflict
    
    * Fix small bug
    
    ---------
    
    Co-authored-by: Petar Ristic <ratep.r95@gmail.com>

--- web/src/components/organisms/Project/Content/ContentList/utils.ts
+++ web/src/components/organisms/Project/Content/ContentList/utils.ts
@@ -1,3 +1,5 @@
+import { AndCondition } from "@reearth-cms/gql/graphql-client-api";
+
 export function fileName(url: string | undefined): string {
   if (!url) return "";
   let files: string[];
@@ -7,4 +9,43 @@ export function fileName(url: string | undefined): string {
     files = url.split("/");
   }
   return files.length ? files[files.length - 1] : "";
+}
+
+export function filterConvert(filter: AndCondition) {
+  if (!filter || filter?.conditions?.length === 0) return;
+
+  const convertedFilter: { conditions: any[] } = { conditions: [] };
+  let key;
+  for (const c of filter.conditions as any) {
+    switch (c.__typename) {
+      case "NullableFieldCondition":
+        key = "nullable";
+        break;
+      case "MultipleFieldCondition":
+        key = "multiple";
+        break;
+      case "BoolFieldCondition":
+        key = "bool";
+        break;
+      case "StringFieldCondition":
+        key = "string";
+        break;
+      case "NumberFieldCondition":
+        key = "number";
+        break;
+      case "TimeFieldCondition":
+        key = "time";
+        break;
+      default:
+        key = "basic";
+    }
+    convertedFilter.conditions.push({
+      [key]: {
+        fieldId: { id: c.fieldId.id, type: c.fieldId.type },
+        operator: c[key + "Operator"],
+        value: c[key + "Value"],
+      },
+    });
+  }
+  return convertedFilter;
 }
