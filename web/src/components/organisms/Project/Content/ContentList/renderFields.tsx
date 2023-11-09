e22462bf761e8966aa23920e48fabe4513a80ed9
nourbalaha <nourbalaha0@gmail.com>
2023-11-09 14:49:47 +0900

    refactor: render tags function

--- web/src/components/organisms/Project/Content/ContentList/renderFields.tsx
+++ web/src/components/organisms/Project/Content/ContentList/renderFields.tsx
@@ -1,10 +1,11 @@
 import Tag from "@reearth-cms/components/atoms/Tag";
 
 export const renderTags = (el: any, field: any) => {
+  const value = el.props.children as string;
+  if (value === "-") return <span>-</span>;
+
   type Tag = { id: string; name: string; color: string };
   const tags: Tag[] | undefined = field.typeProperty?.tags;
-  const value: string = el.props.children as string;
-  if (value === "-") return value;
   const tagIds: Set<string> = new Set(value.split(", "));
   const filteredTags: Tag[] = tags?.filter((tag: Tag) => tagIds.has(tag.id)) || [];
   return (
