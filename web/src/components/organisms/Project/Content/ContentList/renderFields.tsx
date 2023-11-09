d00326d1acc09ff77da92c9c1bc205b7a72665c2
nourbalaha <nourbalaha0@gmail.com>
2023-11-09 14:25:59 +0900

    return dash for empty tag fields

--- web/src/components/organisms/Project/Content/ContentList/renderFields.tsx
+++ web/src/components/organisms/Project/Content/ContentList/renderFields.tsx
@@ -4,7 +4,8 @@ export const renderTags = (el: any, field: any) => {
   type Tag = { id: string; name: string; color: string };
   const tags: Tag[] | undefined = field.typeProperty?.tags;
   const value: string = el.props.children as string;
-  const tagIds: Set<string> = new Set(value !== "-" ? value.split(", ") : []);
+  if (value === "-") return value;
+  const tagIds: Set<string> = new Set(value.split(", "));
   const filteredTags: Tag[] = tags?.filter((tag: Tag) => tagIds.has(tag.id)) || [];
   return (
     <>
