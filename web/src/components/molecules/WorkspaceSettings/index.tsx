f35b773311f1538edd3ab55f9a1746e61dbdd435
Petar Ristic <ratep.r95@gmail.com>
2023-11-15 06:30:15 -0500

    changed the workspaceform to workspaceGeneralForm

--- web/src/components/molecules/WorkspaceSettings/index.tsx
+++ web/src/components/molecules/WorkspaceSettings/index.tsx
@@ -1,7 +1,8 @@
 import InnerContent from "@reearth-cms/components/atoms/InnerContents/basic";
 import ContentSection from "@reearth-cms/components/atoms/InnerContents/ContentSection";
+import { Workspace } from "@reearth-cms/components/molecules/Workspace/types";
 import DangerZone from "@reearth-cms/components/molecules/WorkspaceSettings/DangerZone";
-import WorkspaceForm from "@reearth-cms/components/molecules/WorkspaceSettings/Form";
+import WorkspaceGeneralForm from "@reearth-cms/components/molecules/WorkspaceSettings/Form";
 import { useT } from "@reearth-cms/i18n";
 
 export type Props = {
@@ -11,16 +12,54 @@ export type Props = {
 };
 
 const WorkspaceSettings: React.FC<Props> = ({
-  workspaceName,
+  // workspaceName,
   onWorkspaceUpdate,
   onWorkspaceDelete,
 }) => {
   const t = useT();
 
+  const workspace: Workspace = {
+    id: "addf",
+    name: "WorkSpace1",
+    tiles: {
+      list: [
+        {
+          id: "1",
+          name: "tile1",
+          url: "http://127.0.0.1:3000/",
+          image: "http://127.0.0.1:3000/",
+        },
+        {
+          id: "1",
+          name: "tile1",
+          url: "http://127.0.0.1:3000/",
+          image: "http://127.0.0.1:3000/",
+        },
+        {
+          id: "1",
+          name: "tile1",
+          url: "http://127.0.0.1:3000/",
+          image: "http://127.0.0.1:3000/",
+        },
+      ],
+      default: "1",
+      switching: false,
+    },
+  };
+
   return (
     <InnerContent title={t("Workspace Settings")}>
       <ContentSection title={t("General")}>
-        <WorkspaceForm workspaceName={workspaceName} onWorkspaceUpdate={onWorkspaceUpdate} />
+        <WorkspaceGeneralForm
+          workspaceName={workspace.name}
+          onWorkspaceUpdate={onWorkspaceUpdate}
+        />
+      </ContentSection>
+      <ContentSection title={t("Tiles")}>
+        <WorkspaceGeneralForm
+          workspaceName={workspace.name}
+          onWorkspaceUpdate={onWorkspaceUpdate}
+        />
       </ContentSection>
       <DangerZone onWorkspaceDelete={onWorkspaceDelete} />
     </InnerContent>
