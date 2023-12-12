c434bd8f359a6ab62a957e90da1823baffde6fe3
Kazuma Tsuchiya <caichi0424@gmail.com>
2023-12-12 17:11:39 +0900

    add: tile and terrain types

--- web/src/components/molecules/Workspace/types.ts
+++ web/src/components/molecules/Workspace/types.ts
@@ -45,3 +45,22 @@ export type Workspace = {
   members?: Member[];
   tiles?: Tiles;
 };
+
+export enum TileType {
+  DEFAULT = "Default",
+  LABELLED = "Labelled",
+  ROAD_MAP = "Road Map",
+  STAMEN_WATERCOLOR = "Stamen Watercolor",
+  STAMEN_TONER = "Stamen Toner",
+  OPEN_STREET_MAP = "OpenStreetMap",
+  ESRI_TOPOGRAPHY = "ESRI Topography",
+  EARTH_AT_NIGHT = "Earth at night",
+  JAPAN_GSI_STANDARD_MAP = "Japan GSI Standard Map",
+  URL = "URL",
+}
+
+export enum TerrainType {
+  CESIUM_WORLD_TERRAIN = "Cesium World Terrain",
+  ARC_GIS_TERRAIN = "ArcGIS Terrain",
+  CESIUM_ION = "Cesium Ion",
+}
