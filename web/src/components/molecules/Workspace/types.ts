944e9d7102c06381386467bc33183554eb214e8a
caichi <54824604+caichi-t@users.noreply.github.com>
2024-01-17 15:45:32 +0900

    fix(web,server): fix and improve the workspace settings (#1022)
    * add: validation for terrain toggle
    
    * remove the Stamen Watercolor option
    
    * add: drag and drop
    
    * add: draggable icon
    
    * add: refetching workspaceSettings
    
    * add: image preview
    
    * add: url validate
    
    * remove the Stamen Watercolor option
    
    * gql generation and cleaning up
    
    * removing stamen_toner tile
    
    * remove STAMEN_TONER option
    
    ---------
    
    Co-authored-by: Maher Alhamoui <hammimo022@gmail.com>
    Co-authored-by: mimoham24 <69579255+mimoham24@users.noreply.github.com>

--- web/src/components/molecules/Workspace/types.ts
+++ web/src/components/molecules/Workspace/types.ts
@@ -70,8 +70,6 @@ export type TileType =
   | "DEFAULT"
   | "LABELLED"
   | "ROAD_MAP"
-  | "STAMEN_WATERCOLOR"
-  | "STAMEN_TONER"
   | "OPEN_STREET_MAP"
   | "ESRI_TOPOGRAPHY"
   | "EARTH_AT_NIGHT"
