a516c1a6a8a54b16247518e8837a66e42c245623
nourbalaha <nourbalaha0@gmail.com>
2024-01-19 15:35:58 +0900

    remove unused props

--- web/src/components/organisms/Settings/General/index.tsx
+++ web/src/components/organisms/Settings/General/index.tsx
@@ -3,22 +3,12 @@ import SettingsMolecule from "@reearth-cms/components/molecules/Settings";
 import useHooks from "./hooks";
 
 const WorkspaceSettings: React.FC = () => {
-  const {
-    workspaceSettings,
-    tiles,
-    terrains,
-    handleWorkspaceSettingsUpdate,
-    handleTerrainToggle,
-    hasPrivilege,
-  } = useHooks();
+  const { workspaceSettings, handleWorkspaceSettingsUpdate, hasPrivilege } = useHooks();
 
   return (
     <SettingsMolecule
       workspaceSettings={workspaceSettings}
-      tiles={tiles}
-      terrains={terrains}
       onWorkspaceSettingsUpdate={handleWorkspaceSettingsUpdate}
-      onTerrainToggle={handleTerrainToggle}
       hasPrivilege={hasPrivilege}
     />
   );
