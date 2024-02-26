43eada01e9ce3d42bd1fdb6125fc4f1c55c248a7
Kazuma Tsuchiya <caichi0424@gmail.com>
2024-02-26 11:40:13 +0900

    small fix

--- web/e2e/general/auth.spec.ts
+++ web/e2e/general/auth.spec.ts
@@ -4,5 +4,5 @@ test("Logout has succeeded", async ({ reearth, page }) => {
   await reearth.goto("/", { waitUntil: "domcontentloaded" });
   await page.locator("a").nth(1).click();
   await page.getByText("Logout").click();
-  await expect(page.getByLabel("Log In").locator("span")).toContainText("Log In");
+  await expect(page.getByLabel("Log In")).toBeVisible();
 });
