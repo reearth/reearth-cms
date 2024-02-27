6844aa1159edb6bdc3e9b62e00d830ba5c1d856b
Kazuma Tsuchiya <caichi0424@gmail.com>
2024-02-27 17:38:32 +0900

    add: schema test

---
+++ web/e2e/project/schema.spec.ts
@@ -0,0 +1,55 @@
+import { Page } from "@playwright/test";
+
+import { expect, test } from "@reearth-cms/e2e/utils";
+
+import { createProject, deleteProject, createModel, crudModel, crudGroup } from "./utils";
+
+async function handleFieldForm(page: Page, name: string, key = name) {
+  await page.getByLabel("Display name").click();
+  await page.getByLabel("Display name").fill(name);
+  await page.getByLabel("Settings").locator("#key").click();
+  await page.getByLabel("Settings").locator("#key").fill(key);
+  await page.getByRole("button", { name: "OK" }).click();
+  await expect(page.getByText(`${name} #${key}`)).toBeVisible();
+}
+
+async function deleteField(page: Page, name: string, key = name) {
+  await page.getByLabel("delete").locator("svg").click();
+  await page.getByRole("button", { name: "OK" }).click();
+  await expect(page.locator("body")).toContainText("Successfully deleted field!");
+  await expect(page.getByText(`${name} #${key}`)).not.toBeVisible();
+}
+
+test("Model CRUD has succeeded", async ({ reearth, page }) => {
+  await reearth.goto("/", { waitUntil: "domcontentloaded" });
+  await createProject(page);
+  await crudModel(page);
+  await deleteProject(page);
+});
+
+test("Group CRUD has succeeded", async ({ reearth, page }) => {
+  await reearth.goto("/", { waitUntil: "domcontentloaded" });
+  await createProject(page);
+  await crudGroup(page);
+  await deleteProject(page);
+});
+
+test("Text field CRUD has succeeded", async ({ reearth, page }) => {
+  await reearth.goto("/", { waitUntil: "domcontentloaded" });
+  await createProject(page);
+  await createModel(page);
+  await page
+    .locator("li")
+    .filter({ hasText: "TextHeading and titles, one-" })
+    .locator("div")
+    .first()
+    .click();
+  await handleFieldForm(page, "text");
+  await expect(page.getByRole("alert").last()).toContainText("Successfully created field!");
+  await page.getByRole("img", { name: "ellipsis" }).locator("svg").click();
+  await handleFieldForm(page, "new text", "new-text");
+  await expect(page.getByRole("alert").last()).toContainText("Successfully updated field!");
+  await deleteField(page, "new text", "new-text");
+
+  await deleteProject(page);
+});
--- web/e2e/project/utils.ts
+++ web/e2e/project/utils.ts
@@ -44,6 +44,86 @@ export async function createModel(page: Page) {
   await page.getByLabel("Model key").click();
   await page.getByLabel("Model key").fill("e2e-model-key");
   await page.getByRole("button", { name: "OK" }).click();
+  await expect(page.getByRole("alert").last()).toContainText("Successfully created model!");
+  await expect(page.getByTitle("e2e model name")).toBeVisible();
+  await expect(page.getByText("#e2e-model-key")).toBeVisible();
+  await expect(
+    page.getByRole("menuitem", { name: "e2e model name" }).locator("span"),
+  ).toBeVisible();
+}
+
+const updateModelName = "new e2e model name";
+
+async function updateModel(page: Page) {
+  await page.getByRole("button", { name: "more" }).hover();
+  await page.getByText("Edit", { exact: true }).click();
+  await page.getByLabel("Update Model").locator("#name").click();
+  await page.getByLabel("Update Model").locator("#name").fill(updateModelName);
+  await page.getByLabel("Update Model").locator("#key").click();
+  await page.getByLabel("Update Model").locator("#key").fill("new-e2e-model-key");
+  await page.getByRole("button", { name: "OK" }).click();
+  await expect(page.getByRole("alert").last()).toContainText("Successfully updated model!");
+  await expect(page.getByTitle(updateModelName)).toBeVisible();
+  await expect(page.getByText("#new-e2e-model-key")).toBeVisible();
+  await expect(page.getByRole("menuitem", { name: updateModelName }).locator("span")).toBeVisible();
+}
+
+async function deleteModel(page: Page) {
+  await page.getByRole("button", { name: "more" }).hover();
+  await page.getByText("Delete").click();
+  await page.getByRole("button", { name: "Delete Model" }).click();
+  await expect(page.getByRole("alert").last()).toContainText("Successfully deleted model!");
+  await expect(page.getByTitle(updateModelName)).not.toBeVisible();
+}
+
+export async function crudModel(page: Page) {
+  await createModel(page);
+  await updateModel(page);
+  await deleteModel(page);
+}
+
+async function createGroup(page: Page) {
+  await page.getByText("Schema").click();
+  await page.getByRole("button", { name: "plus Add" }).last().click();
+  await page.getByLabel("Group name").click();
+  await page.getByLabel("Group name").fill("e2e group name");
+  await page.getByLabel("Group key").click();
+  await page.getByLabel("Group key").fill("e2e-group-key");
+  await page.getByRole("button", { name: "OK" }).click();
+  await expect(page.getByRole("alert").last()).toContainText("Successfully created group!");
+  await page.getByText("e2e group name").click();
+  await expect(page.getByTitle("e2e group name")).toBeVisible();
+  await expect(page.getByText("#e2e-group-key")).toBeVisible();
+}
+
+const updateGroupName = "new e2e group name";
+
+async function updateGroup(page: Page) {
+  await page.getByRole("button", { name: "more" }).hover();
+  await page.getByText("Edit", { exact: true }).click();
+  await page.getByLabel("Update Group").locator("#name").click();
+  await page.getByLabel("Update Group").locator("#name").fill(updateGroupName);
+  await page.getByLabel("Update Group").locator("#key").click();
+  await page.getByLabel("Update Group").locator("#key").fill("new-e2e-group-key");
+  await page.getByRole("button", { name: "OK" }).click();
+  await expect(page.getByRole("alert").last()).toContainText("Successfully updated group!");
+  await expect(page.getByTitle(updateGroupName)).toBeVisible();
+  await expect(page.getByText("#new-e2e-group-key")).toBeVisible();
+  await expect(page.getByRole("menuitem", { name: updateGroupName }).locator("span")).toBeVisible();
+}
+
+async function deleteGroup(page: Page) {
+  await page.getByRole("button", { name: "more" }).hover();
+  await page.getByText("Delete").click();
+  await page.getByRole("button", { name: "Delete Group" }).click();
+  await expect(page.getByRole("alert").last()).toContainText("Successfully deleted group!");
+  await expect(page.getByTitle(updateGroupName)).not.toBeVisible();
+}
+
+export async function crudGroup(page: Page) {
+  await createGroup(page);
+  await updateGroup(page);
+  await deleteGroup(page);
 }
 
 async function createComment(page: Page) {
