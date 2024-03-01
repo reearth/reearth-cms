f828cf75829e38966bc422cbbabf3023aa83f890
rot1024 <aayhrot@gmail.com>
2024-03-01 18:14:35 +0900

    feat(server): integration API to create asset upload (#1084)
    feat(server): add a new integration api to create asset upload

--- server/tools.go
+++ server/tools.go
@@ -4,7 +4,7 @@ package tools
 
 import (
 	_ "github.com/99designs/gqlgen"
-	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
+	_ "github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/reearth/reearthx/tools"
 	_ "github.com/vektah/dataloaden"
