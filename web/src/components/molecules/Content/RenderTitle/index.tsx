1878283c83cd7d274af6a5e9a1c8ec93213094c3
Kazuma Tsuchiya <caichi0424@gmail.com>
2024-03-07 18:13:43 +0900

    enable url metadata to edit from table

--- web/src/components/molecules/Content/RenderTitle/index.tsx
+++ web/src/components/molecules/Content/RenderTitle/index.tsx
@@ -3,14 +3,10 @@ import Space from "@reearth-cms/components/atoms/Space";
 import { Field } from "@reearth-cms/components/molecules/Schema/types";
 
 export const renderTitle = (field: Field) => {
-  if (field.type === "URL") {
-    return field.title;
-  } else {
-    return (
-      <Space size={4}>
-        {field.title}
-        <Icon icon="edit" />
-      </Space>
-    );
-  }
+  return (
+    <Space size={4}>
+      {field.title}
+      <Icon icon="edit" />
+    </Space>
+  );
 };
