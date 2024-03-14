f42c53ef4376a1a13197ff6f044a418fe1588434
caichi <54824604+caichi-t@users.noreply.github.com>
2024-03-14 19:14:07 +0900

    fix(web): adding e2e tests for items and optimizing queries (#1096)
    * fix: content test
    
    * add: text field test
    
    * add: asset test
    
    * add: date test
    
    * add: boolean test
    
    * add: option test
    
    * add: int test
    
    * fix: int test
    
    * add: url test
    
    * add; group test
    
    * add reference
    test and fix reference modal behavior
    
    * move files
    
    * add: text matadata test
    
    * small fix
    
    * add: tag test
    
    * add: boolean metadata test
    
    * add: checkbox test
    
    * add: date metadata test
    
    * add: url metadata test
    
    * fix: reduce api when updating item
    
    * fix: throw searchItem when opening the reference modal
    
    * fix: throw GetAssets when selecting asset field
    
    * fix: throw GetModalRequests when opening the add to Request modal
    
    * fix: unify GetAsset and GetAssetItem
    
    * fix: GetModels and GetModel
    
    * fix: tests
    
    * fix: refetchQueries name
    
    * fix: behavior after deleting model
    
    * fix: account settting test
    
    * fix group test
    
    * add: retries property to playwright.config
    
    * fix: randomizing project alias
    
    * add: beforeEach and afterEach
    
    * remove metadata tests
    
    * add: reverting back to the original username and email
    
    * fix: account name and email bug

--- web/e2e/project/utils/project.ts
+++ web/e2e/project/utils/project.ts
@@ -4,17 +4,18 @@ import { closeNotification } from "@reearth-cms/e2e/common/notification";
 import { expect } from "@reearth-cms/e2e/utils";
 
 export async function createProject(page: Page) {
+  const id = Math.ceil(Math.random() * (100000 - 10000) + 10000).toString();
   await page.getByRole("button", { name: "plus New Project" }).first().click();
   await page.getByRole("dialog").locator("#name").click();
-  await page.getByRole("dialog").locator("#name").fill("e2e project name");
+  await page.getByRole("dialog").locator("#name").fill(id);
   await page.getByLabel("Project alias").click();
-  await page.getByLabel("Project alias").fill("e2e-project-alias");
+  await page.getByLabel("Project alias").fill(id);
   await page.getByLabel("Project description").click();
   await page.getByLabel("Project description").fill("e2e project description");
   await page.getByRole("button", { name: "OK" }).click();
   await expect(page.getByRole("alert").last()).toContainText("Successfully created project!");
   await closeNotification(page);
-  await page.getByText("e2e project name", { exact: true }).click();
+  await page.getByText(id, { exact: true }).click();
 }
 
 export async function deleteProject(page: Page) {
