d5b7219134ffdafa149b47a857b33bcde54a9272
Nour Balaha <nourbalaha0@gmail.com>
2024-06-05 13:31:25 +0900

    chore(web): migrate eslint to v9 (#1168)
    * migrate eslint to v9
    
    * add config for playwright
    
    * add story book to eslint.config
    
    * add type: module to package.json
    
    * add prettier config and update to v 3.3.0

---
+++ web/eslint.config.js
@@ -0,0 +1,65 @@
+import config from "eslint-config-reearth";
+import playwright from "eslint-plugin-playwright";
+import storybook from "eslint-plugin-storybook";
+import globals from "globals";
+
+/** @type { import("eslint").Linter.FlatConfig[] } */
+
+export default [
+  ...config,
+  {
+    rules: {
+      "import/order": [
+        "warn",
+        {
+          pathGroups: [
+            {
+              pattern: "@reearth-cms/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "node/no-extraneous-import": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-invalid-void-type": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/consistent-type-definitions": "warn",
+      "@typescript-eslint/array-type": "warn",
+      "@typescript-eslint/consistent-indexed-object-style": "warn",
+    },
+  },
+  {
+    files: ["*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+    plugins: {
+      storybook: storybook,
+    },
+  },
+  {
+    ...playwright.configs["flat/recommended"],
+    files: ["e2e/**/*"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    ignores: [
+      "dist/*",
+      "coverage/*",
+      "node_modules/*",
+      "storybook-static/*",
+      "!.storybook/",
+      ".storybook/public/*",
+      "src/gql/graphql-client-api.tsx",
+    ],
+  },
+];
