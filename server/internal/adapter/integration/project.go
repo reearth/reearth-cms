739cbe1207c5d3a7f281d6ae7445e4c0d41d2472
ymk <81808708+yk-eukarya@users.noreply.github.com>
2024-06-19 07:44:29 +0200

    imp

---
+++ server/internal/adapter/integration/project.go
@@ -0,0 +1,18 @@
+package integration
+
+import (
+	"context"
+)
+
+func (s *Server) ProjectFilter(ctx context.Context, request ProjectFilterRequestObject) (ProjectFilterResponseObject, error) {
+	//uc := adapter.Usecases(ctx)
+	//op := adapter.Operator(ctx)
+	//
+	//p, pi, err := uc.Project.FindByWorkspace(ctx, nil, op)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//return ProjectFilter200JSONResponse(integrationapi.NewProject(p)), err
+	panic("implement me")
+}
