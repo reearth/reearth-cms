e86defb31c3175fe5faccad3290dfaf0bf23eb94
ymk <81808708+yk-eukarya@users.noreply.github.com>
2024-06-28 10:03:52 +0200

    imp

--- server/internal/adapter/integration/project.go
+++ server/internal/adapter/integration/project.go
@@ -2,17 +2,48 @@ package integration
 
 import (
 	"context"
+
+	"github.com/reearth/reearth-cms/server/internal/adapter"
+	"github.com/reearth/reearth-cms/server/pkg/integrationapi"
+	"github.com/reearth/reearth-cms/server/pkg/project"
+	"github.com/reearth/reearthx/rerror"
+	"github.com/samber/lo"
 )
 
 func (s *Server) ProjectFilter(ctx context.Context, request ProjectFilterRequestObject) (ProjectFilterResponseObject, error) {
-	//uc := adapter.Usecases(ctx)
-	//op := adapter.Operator(ctx)
-	//
-	//p, pi, err := uc.Project.FindByWorkspace(ctx, nil, op)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//return ProjectFilter200JSONResponse(integrationapi.NewProject(p)), err
-	panic("implement me")
+	uc := adapter.Usecases(ctx)
+	op := adapter.Operator(ctx)
+
+	if request.WorkspaceId.IsEmpty() {
+		return ProjectFilter400Response{}, rerror.ErrInvalidParams
+	}
+
+	if !op.AllReadableWorkspaces().Has(request.WorkspaceId) {
+		return ProjectFilter404Response{}, rerror.ErrNotFound
+	}
+
+	p := fromPagination(request.Params.Page, request.Params.PerPage)
+	res, pi, err := uc.Project.FindByWorkspace(ctx, request.WorkspaceId, p, op)
+	if err != nil {
+		return nil, err
+	}
+	if res == nil || len(res) == 0 {
+		return ProjectFilter200JSONResponse{
+			Projects:   nil,
+			Page:       lo.ToPtr(Page(*p.Offset)),
+			PerPage:    lo.ToPtr(int(p.Offset.Limit)),
+			TotalCount: lo.ToPtr(0),
+		}, nil
+	}
+
+	projects := lo.Map(res, func(p *project.Project, _ int) integrationapi.Project {
+		return integrationapi.NewProject(p)
+	})
+
+	return ProjectFilter200JSONResponse{
+		Projects:   &projects,
+		Page:       lo.ToPtr(Page(*p.Offset)),
+		PerPage:    lo.ToPtr(int(p.Offset.Limit)),
+		TotalCount: lo.ToPtr(int(pi.TotalCount)),
+	}, err
 }
