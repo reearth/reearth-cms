a86540f6b774232eceef7ffe8913d8f4e971c03f
nourbalaha <nourbalaha0@gmail.com>
2024-07-18 17:39:34 +0900

    refactor

--- server/pkg/integrationapi/geojson.go
+++ server/pkg/integrationapi/geojson.go
@@ -15,86 +15,85 @@ import (
 var noGeometryFieldError = rerror.NewE(i18n.T("no geometry field in this model"))
 
 func NewFeatureCollection(ver item.VersionedList, sp *schema.Package, idOrKey *schema.FieldIDOrKey) (*FeatureCollection, error) {
-	// if model has no geometry field, return and error.
-	if sp.Schema().FieldsByType(value.TypeGeometryEditor) == nil || sp.Schema().FieldsByType(value.TypeGeometryObject) == nil {
+	if !hasGeometryFields(sp) {
 		return nil, noGeometryFieldError
 	}
 
-	// if there are multiple geometry fields
-	//   - Idea1: use the first geometry field and ignore other geometry fields ← OK for now!
-	//   - idea2: specify geometry field key or ID in URL
-	var geoField *schema.Field
-	if idOrKey != nil {
-		sIdOrKey := string(*idOrKey)
-		var idd id.FieldID
-		var key id.Key
-		res, err := id.FieldIDFrom(sIdOrKey)
-		if err == nil {
-			idd = res
-		} else {
-			key = id.NewKey(sIdOrKey)
-		}
-		geoField = sp.Schema().FieldByIDOrKey(&idd, &key)
-	} else {
-		geoObjectFields := sp.Schema().FieldsByType(value.TypeGeometryObject)
-		geoEditorFields := sp.Schema().FieldsByType(value.TypeGeometryEditor)
-		var geoFields []*schema.Field
-		if len(geoObjectFields) > 0 {
-			geoFields = append(geoFields, geoObjectFields...)
-		}
-		if len(geoEditorFields) > 0 {
-			geoFields = append(geoFields, geoEditorFields...)
-		}
-		geoField = geoFields[0]
+	geoField, err := getGeometryField(sp, idOrKey)
+	if err != nil {
+		return nil, err
 	}
 
 	fl := lo.Map(ver, func(v item.Versioned, _ int) Feature {
 		return NewFeature(v, sp, geoField)
 	})
+
 	return &FeatureCollection{
 		Type:     lo.ToPtr(FeatureCollectionTypeFeatureCollection),
 		Features: &fl,
 	}, nil
 }
 
+func hasGeometryFields(sp *schema.Package) bool {
+	return sp.Schema().FieldsByType(value.TypeGeometryEditor) != nil && sp.Schema().FieldsByType(value.TypeGeometryObject) != nil
+}
+
+func getGeometryField(sp *schema.Package, idOrKey *schema.FieldIDOrKey) (*schema.Field, error) {
+	if idOrKey != nil {
+		return getFieldByIDOrKey(sp, idOrKey), nil
+	}
+
+	geoFields := append(sp.Schema().FieldsByType(value.TypeGeometryObject), sp.Schema().FieldsByType(value.TypeGeometryEditor)...)
+	if len(geoFields) == 0 {
+		return nil, noGeometryFieldError
+	}
+
+	return geoFields[0], nil
+}
+
+func getFieldByIDOrKey(sp *schema.Package, idOrKey *schema.FieldIDOrKey) *schema.Field {
+	sIdOrKey := string(*idOrKey)
+	idd, key := parseIDOrKey(sIdOrKey)
+	return sp.Schema().FieldByIDOrKey(idd, key)
+}
+
+func parseIDOrKey(sIdOrKey string) (*id.FieldID, *id.Key) {
+	if res, err := id.FieldIDFrom(sIdOrKey); err == nil {
+		return &res, nil
+	}
+	return nil, id.NewKey(sIdOrKey).Ref()
+}
+
 func NewFeature(ver item.Versioned, sp *schema.Package, geo *schema.Field) Feature {
 	itm := ver.Value()
-	var g Geometry
-	var p map[string]interface{}
-
-	// for geometry
-	// if the geometry field is multiple, use the first one.
 	geoField := itm.Field(geo.ID())
 	vv := *geoField.Value().First()
 	ss, _ := vv.ValueString()
 	geometry, _ := StringToGeometry(ss)
-	g = *geometry
-
-	// for properties:
-	// get the key from the model schema
-	// and get the value from the item schema
-	otherFields := lo.Filter(sp.Schema().Fields(), func(f *schema.Field, _ int) bool {
-		return f.Type() != value.TypeGeometryObject && f.Type() != value.TypeGeometryEditor
-	})
-	if len(otherFields) > 0 {
-		for _, f := range sp.Schema().Fields() {
-			if f.Type() != value.TypeGeometryObject && f.Type() != value.TypeGeometryEditor {
-				key := f.Name()
-				value := itm.Field(f.ID()).Value()
-				p[key] = value
-			}
-		}
-	}
 
 	return Feature{
 		Type:       lo.ToPtr(FeatureTypeFeature),
 		Id:         itm.ID().Ref(),
-		Geometry:   &g,
-		Properties: &p,
+		Geometry:   geometry,
+		Properties: getProperties(itm, sp),
 	}
 }
 
-// Function to convert a stringified Geometry to a Geometry type
+func getProperties(itm *item.Item, sp *schema.Package) *map[string]interface{} {
+	p := make(map[string]interface{})
+	otherFields := lo.Filter(sp.Schema().Fields(), func(f *schema.Field, _ int) bool {
+		return f.Type() != value.TypeGeometryObject && f.Type() != value.TypeGeometryEditor
+	})
+
+	for _, f := range otherFields {
+		key := f.Name()
+		val := itm.Field(f.ID()).Value()
+		p[key] = val
+	}
+
+	return &p
+}
+
 func StringToGeometry(geoString string) (*Geometry, error) {
 	var geo Geometry
 	if err := json.Unmarshal([]byte(geoString), &geo); err != nil {
