6dafcd022610ee879a3cab1de4b2f1909e068f9a
Kazuma Tsuchiya <caichi0424@gmail.com>
2024-08-23 13:32:50 +0900

    fix: title icon

--- web/src/components/organisms/Project/ModelsMenu/index.tsx
+++ web/src/components/organisms/Project/ModelsMenu/index.tsx
@@ -13,6 +13,7 @@ interface Props {
   collapsed: boolean;
   selectedSchemaType: SelectedSchemaType;
   displayGroups?: boolean;
+  titleIcon: string;
   onModelSelect: (modelId: string) => void;
   onGroupSelect?: (groupId: string) => void;
 }
@@ -22,6 +23,7 @@ const ModelsMenu: React.FC<Props> = ({
   collapsed,
   selectedSchemaType,
   displayGroups,
+  titleIcon,
   onModelSelect,
   onGroupSelect,
 }) => {
@@ -48,7 +50,7 @@ const ModelsMenu: React.FC<Props> = ({
 
   return (
     <>
-      <ModelListHeader title={title} collapsed={collapsed} />
+      <ModelListHeader title={title} collapsed={collapsed} titleIcon={titleIcon} />
       <ModelListBody>
         <Models
           title={title}
