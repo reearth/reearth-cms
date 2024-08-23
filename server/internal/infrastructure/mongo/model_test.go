3058f05cb8dedea7b046d846362deb3ff3123805
ymk <81808708+yk-eukarya@users.noreply.github.com>
2024-08-23 12:23:09 +0200

    imp

--- server/internal/infrastructure/mongo/model_test.go
+++ server/internal/infrastructure/mongo/model_test.go
@@ -499,3 +499,156 @@ func TestModelRepo_FindByProject(t *testing.T) {
 		})
 	}
 }
+
+func TestModelRepo_FindByProjectAndKeyword(t *testing.T) {
+	mocknow := time.Now().Truncate(time.Millisecond).UTC()
+	pid1 := id.NewProjectID()
+	id1 := id.NewModelID()
+	id2 := id.NewModelID()
+	sid1 := id.NewSchemaID()
+	sid2 := id.NewSchemaID()
+	k := key.New("T123456")
+	m1 := model.New().ID(id1).Project(pid1).Schema(sid1).Key(k).Name("m1").UpdatedAt(mocknow).MustBuild()
+	m2 := model.New().ID(id2).Project(pid1).Schema(sid2).Key(k).Name("m2").UpdatedAt(mocknow).MustBuild()
+
+	type args struct {
+		pId     id.ProjectID
+		keyword string
+		pInfo   *usecasex.Pagination
+	}
+	tests := []struct {
+		name    string
+		seeds   model.List
+		args    args
+		filter  *repo.ProjectFilter
+		want    model.List
+		wantErr error
+	}{
+		{
+			name:    "0 count in empty db",
+			seeds:   model.List{},
+			args:    args{id.NewProjectID(), "", nil},
+			want:    nil,
+			wantErr: nil,
+		},
+		{
+			name: "0 count with model for another projects",
+			seeds: model.List{
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+			},
+			args:    args{id.NewProjectID(), "", nil},
+			want:    nil,
+			wantErr: nil,
+		},
+		{
+			name: "1 count with single model",
+			seeds: model.List{
+				m1,
+			},
+			args:    args{pid1, "", usecasex.CursorPagination{First: lo.ToPtr(int64(2))}.Wrap()},
+			want:    model.List{m1},
+			wantErr: nil,
+		},
+		{
+			name: "1 count with multi models",
+			seeds: model.List{
+				m1,
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+			},
+			args:    args{pid1, "", usecasex.CursorPagination{First: lo.ToPtr(int64(2))}.Wrap()},
+			want:    model.List{m1},
+			wantErr: nil,
+		},
+		{
+			name: "2 count with multi models",
+			seeds: model.List{
+				m1,
+				m2,
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+			},
+			args:    args{pid1, "", usecasex.CursorPagination{First: lo.ToPtr(int64(2))}.Wrap()},
+			want:    model.List{m1, m2},
+			wantErr: nil,
+		},
+		{
+			name: "1 count with multi models with keyword",
+			seeds: model.List{
+				m1,
+				m2,
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+			},
+			args:    args{pid1, "1", usecasex.CursorPagination{First: lo.ToPtr(int64(2))}.Wrap()},
+			want:    model.List{m1},
+			wantErr: nil,
+		},
+		{
+			name: "1 count with multi models with keyword",
+			seeds: model.List{
+				m1,
+				m2,
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+			},
+			args:    args{pid1, "2", usecasex.CursorPagination{First: lo.ToPtr(int64(2))}.Wrap()},
+			want:    model.List{m2},
+			wantErr: nil,
+		},
+		{
+			name: "project filter operation success",
+			seeds: model.List{
+				m1,
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+			},
+			args:    args{pid1, "", usecasex.CursorPagination{First: lo.ToPtr(int64(1))}.Wrap()},
+			filter:  &repo.ProjectFilter{Readable: []id.ProjectID{pid1}, Writable: []id.ProjectID{pid1}},
+			want:    model.List{m1},
+			wantErr: nil,
+		},
+		{
+			name: "project filter operation denied",
+			seeds: model.List{
+				m1,
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+				model.New().NewID().Project(id.NewProjectID()).Schema(sid1).Key(k).UpdatedAt(mocknow).MustBuild(),
+			},
+			args:    args{pid1, "", usecasex.CursorPagination{First: lo.ToPtr(int64(2))}.Wrap()},
+			filter:  &repo.ProjectFilter{Readable: []id.ProjectID{}, Writable: []id.ProjectID{}},
+			want:    nil,
+			wantErr: nil,
+		},
+	}
+
+	initDB := mongotest.Connect(t)
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			client := mongox.NewClientWithDatabase(initDB(t))
+
+			r := NewModel(client)
+			ctx := context.Background()
+			for _, a := range tc.seeds {
+				err := r.Save(ctx, a)
+				assert.NoError(t, err)
+			}
+
+			if tc.filter != nil {
+				r = r.Filtered(*tc.filter)
+			}
+
+			got, _, err := r.FindByProjectAndKeyword(ctx, tc.args.pId, tc.args.keyword, tc.args.pInfo)
+			if tc.wantErr != nil {
+				assert.ErrorIs(t, err, tc.wantErr)
+				return
+			}
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
