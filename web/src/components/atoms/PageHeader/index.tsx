9c3e0827d9aab21e6065063e5d0bc99048cf783d
Kazuma Tsuchiya <caichi0424@gmail.com>
2024-09-17 18:31:34 +0900

    fix: background color

--- web/src/components/atoms/PageHeader/index.tsx
+++ web/src/components/atoms/PageHeader/index.tsx
@@ -8,7 +8,7 @@ const Header: React.FC<Props> = props => {
 };
 
 const StyledPageHeader = styled(PageHeader)`
-  background-color: #fff;
+  background-color: #fff !important;
   padding: 16px 24px;
   .ant-page-header-heading {
     flex-wrap: inherit;
