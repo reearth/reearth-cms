518e65dbe65063883d804bf6176df5fd16771b1f
nourbalaha <nourbalaha0@gmail.com>
2024-10-01 18:35:24 +0900

    fix request resolver

--- server/internal/adapter/gql/resolver_request.go
+++ server/internal/adapter/gql/resolver_request.go
@@ -32,7 +32,7 @@ func (r *mutationResolver) CreateRequest(ctx context.Context, input gqlmodel.Cre
 			return nil, err
 		}
 
-		return request.NewItem(iid)
+		return request.NewItem(iid, i.Version)
 	})
 	if err != nil {
 		return nil, err
@@ -73,7 +73,7 @@ func (r *mutationResolver) UpdateRequest(ctx context.Context, input gqlmodel.Upd
 			return nil, err
 		}
 
-		return request.NewItem(iid)
+		return request.NewItem(iid, i.Version)
 	})
 	if err != nil {
 		return nil, err
