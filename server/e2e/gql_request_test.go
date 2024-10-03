8198fea4c617f7317925a8169ece36646a8a899f
nourbalaha <nourbalaha0@gmail.com>
2024-10-03 17:42:34 +0900

    refactor 2

--- server/e2e/gql_request_test.go
+++ server/e2e/gql_request_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/reearth/reearth-cms/server/internal/app"
 	"github.com/reearth/reearth-cms/server/pkg/item"
+	"github.com/samber/lo"
 )
 
 func createRequest(e *httpexpect.Expect, projectId, title string, description, state *string, reviewersId []string, items []any) *httpexpect.Value {
@@ -195,43 +196,31 @@ func TestRequest(t *testing.T) {
 	iid1, i1 := createItem(e, mId, sId, nil, []map[string]any{
 		{"schemaFieldId": fid, "value": "test", "type": "Text"},
 	})
-	ver := i1.Path("$.data.createItem.item.version").Raw().(string)
+	ver1 := i1.Path("$.data.createItem.item.version").Raw().(string)
 
 	// Create request
-	title := "test"
-	description := "test"
-	state := "DRAFT"
-	reviewersId := []string{uId1.String()}
-	itm1 := map[string]any{"itemId": iid1, "version": ver}
-	items := []any{itm1}
-
-	res := createRequest(e, pId, title, &description, &state, reviewersId, items)
+	res := createRequest(e, pId, "test", lo.ToPtr("test"), lo.ToPtr("DRAFT"), []string{uId1.String()}, []any{map[string]any{"itemId": iid1, "version": ver1}})
 	req := res.Path("$.data.createRequest.request").Object()
 	rid := req.Value("id").String().Raw()
 
-	req.Value("title").IsEqual(title)
-	req.Value("description").IsEqual(description)
-	req.Value("state").IsEqual(state)
-	req.Value("reviewersId").IsEqual(reviewersId)
-	req.Value("items").IsEqual([]any{map[string]any{"itemId": iid1, "ref": nil,  "version": ver}})
+	req.Value("title").IsEqual("test")
+	req.Value("description").IsEqual("test")
+	req.Value("state").IsEqual("DRAFT")
+	req.Value("reviewersId").IsEqual([]string{uId1.String()})
+	req.Value("items").IsEqual([]any{map[string]any{"itemId": iid1, "ref": nil, "version": ver1}})
 
 	// Update request
-	title2 := "test2"
-	description2 := "test2"
-	state2 := "WAITING"
-	reviewersId2 := []string{uId1.String(), uId4.String()}
 	iid2 := item.NewID().String()
 	ver2 := uuid.New().String()
-	items2 := []any{itm1, map[string]any{"itemId": iid2, "version": ver2}}
-	res2 := updateRequest(e, rid, title2, &description2, &state2, reviewersId2, items2)
+	res2 := updateRequest(e, rid, "test2", lo.ToPtr("test2"), lo.ToPtr("WAITING"), []string{uId1.String(), uId4.String()}, []any{map[string]any{"itemId": iid1, "version": ver1}, map[string]any{"itemId": iid2, "version": ver2}})
 	req2 := res2.Path("$.data.updateRequest.request").Object()
 
-	req2.Value("title").IsEqual(title2)
-	req2.Value("description").IsEqual(description2)
-	req2.Value("state").IsEqual(state2)
-	req2.Value("reviewersId").IsEqual(reviewersId2)
+	req2.Value("title").IsEqual("test2")
+	req2.Value("description").IsEqual("test2")
+	req2.Value("state").IsEqual("WAITING")
+	req2.Value("reviewersId").IsEqual([]string{uId1.String(), uId4.String()})
 	req2.Value("items").Array().Length().IsEqual(2)
-	req2.Value("items").IsEqual([]any{map[string]any{"itemId": iid1, "ref": nil,  "version": ver},map[string]any{"itemId": iid2, "ref": nil,  "version": ver2}})
+	req2.Value("items").IsEqual([]any{map[string]any{"itemId": iid1, "ref": nil, "version": ver1}, map[string]any{"itemId": iid2, "ref": nil, "version": ver2}})
 
 	// Approve
 	res3 := approveRequest(e, rid)
@@ -239,8 +228,34 @@ func TestRequest(t *testing.T) {
 	req3.Value("id").IsEqual(rid)
 	req3.Value("state").IsEqual("APPROVED")
 
-	// Close All
-	res4 := closeAllRequests(e, pId, []string{rid})
-	req4 := res4.Path("$.data.deleteRequest.requests").Array()
-	req4.Value(0).IsEqual(rid)
+	_, itm_public := getItem(e, iid1)
+	itm_public.Path("$.data.node").Object().Value("version").IsEqual(ver1)
+	itm_public.Path("$.data.node").Object().Value("status").IsEqual("PUBLIC")
+}
+
+func TestCloseRequest(t *testing.T) {
+	e := StartServer(t, &app.Config{}, true, baseSeederUser)
+
+	pId, _ := createProject(e, wId.String(), "test", "test", "test-1")
+	mId, _ := createModel(e, pId, "test", "test", "test-1")
+	fid, _ := createField(e, mId, "text", "text", "text",
+		false, false, false, false, "Text",
+		map[string]any{
+			"text": map[string]any{},
+		})
+	sId, _, _ := getModel(e, mId)
+	iid1, i1 := createItem(e, mId, sId, nil, []map[string]any{
+		{"schemaFieldId": fid, "value": "test", "type": "Text"},
+	})
+	ver1 := i1.Path("$.data.createItem.item.version").Raw().(string)
+
+	res := createRequest(e, pId, "test", lo.ToPtr("test"), lo.ToPtr("DRAFT"), []string{uId1.String()}, []any{map[string]any{"itemId": iid1, "version": ver1}})
+	req := res.Path("$.data.createRequest.request").Object()
+	rid := req.Value("id").String().Raw()
+
+	res2 := closeAllRequests(e, pId, []string{rid})
+	res2.Path("$.data.deleteRequest.requests").Array().Value(0).IsEqual(rid)
+
+	_, itm_closed := getItem(e, iid1)
+	itm_closed.Path("$.data.node").Object().Value("version").IsEqual(ver1)
 }
