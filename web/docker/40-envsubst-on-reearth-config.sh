25ecfe16d7653995d33e26974165e5cd94c38a25
KeisukeYamashita <19yamashita15@gmail.com>
2024-10-10 22:29:57 +0200

    feat(web): wrap environment variables with '""' for Re:Earth config (#1258)
    feat(web): wrap env var with ""
    
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- web/docker/40-envsubst-on-reearth-config.sh
+++ web/docker/40-envsubst-on-reearth-config.sh
@@ -5,4 +5,13 @@ set -e
 _REEARTH_CONFIG_TEMPLATE_FILE="/opt/reearth-cms/reearth_config.json.template"
 _REEARTH_CONFIG_OUTPUT_FILE="/usr/share/nginx/html/reearth_config.json"
 
+# Wrap with "" if the value doesn't start with '{' and end with '}' (JSON) or "null".
+wrap_reearth_cms_variables() {
+    for var in $(env | grep '^REEARTH_CMS_' | cut -d= -f1); do
+        value=$(printenv "$var")
+        [ "$value" != "null" ] && ! echo "$value" | grep -qE '^\{.*\}$' && export "$var=\"${value}\""
+    done
+}
+
+wrap_reearth_cms_variables $@
 envsubst < "$_REEARTH_CONFIG_TEMPLATE_FILE" > "$_REEARTH_CONFIG_OUTPUT_FILE"
