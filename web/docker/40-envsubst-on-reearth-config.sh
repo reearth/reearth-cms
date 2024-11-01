cdc25a6a0f12d9b9109d33743ea738f76ba5be9c
rot1024 <aayhrot@gmail.com>
2024-11-01 16:49:35 +0900

    fix(web): fix errors on Docker startup

--- web/docker/40-envsubst-on-reearth-config.sh
+++ web/docker/40-envsubst-on-reearth-config.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+# rewrite index.html to change title and favicon
+_REEARTH_HTML_FILE="/usr/share/nginx/html/index.html"
+
+# Rewrite title tag in index.html only if REEARTH_CMS_TITLE is set
+if [ -n "$REEARTH_CMS_TITLE" ]; then
+  sed -i -e "s|<title>.*</title>|<title>${REEARTH_CMS_TITLE}</title>|g" "$_REEARTH_HTML_FILE"
+fi
+
+# Rewrite favicon in index.html only if REEARTH_CMS_FAVICON_URL is set
+if [ -n "$REEARTH_CMS_FAVICON_URL" ]; then
+  sed -i -e "s|<link rel=\"icon\" href=\"[^\"]*\" type=\"image/x-icon\" />|<link rel=\"icon\" href=\"${REEARTH_CMS_FAVICON_URL}\" type=\"image/x-icon\" />|g" "$_REEARTH_HTML_FILE"
+fi
+
+# generate reearth_config.json
 _REEARTH_CONFIG_TEMPLATE_FILE="/opt/reearth-cms/reearth_config.json.template"
 _REEARTH_CONFIG_OUTPUT_FILE="/usr/share/nginx/html/reearth_config.json"
 
@@ -9,23 +23,13 @@ _REEARTH_CONFIG_OUTPUT_FILE="/usr/share/nginx/html/reearth_config.json"
 wrap_reearth_cms_variables() {
     for var in $(env | grep '^REEARTH_CMS_' | cut -d= -f1); do
         value=$(printenv "$var")
-        [ "$value" != "null" ] && ! echo "$value" | grep -qE '^\{.*\}$' && export "$var=\"${value}\""
+        if [ -z "$value" ]; then
+            eval "export $var='\"\"'"
+        elif [ "$value" != "null" ] && ! echo "$value" | grep -qE '^\{.*\}$'; then
+            eval "export $var='${value}'"
+        fi
     done
 }
 
-wrap_reearth_cms_variables $@
+wrap_reearth_cms_variables "$@"
 envsubst < "$_REEARTH_CONFIG_TEMPLATE_FILE" > "$_REEARTH_CONFIG_OUTPUT_FILE"
-
-# rewrite index.html to change title and favicon
-_REEARTH_HTML_FILE="/usr/share/nginx/html/index.html"
-
-# Rewrite title tag in index.html only if REEARTH_CMS_TITLE is set
-if [ -n "$REEARTH_CMS_TITLE" ]; then
-  sed -i '' -e "s|<title>.*</title>|<title>${REEARTH_CMS_TITLE}</title>|g" "$_REEARTH_HTML_FILE"
-fi
-
-# Rewrite favicon in index.html only if REEARTH_CMS_FAVICON is set
-#
-if [ -n "$REEARTH_CMS_FAVICON" ]; then
-  sed -i '' -e "s|<link rel=\"icon\" href=\"[^\"]*\" type=\"image/x-icon\" />|<link rel=\"icon\" href=\"${REEARTH_CMS_FAVICON}\" type=\"image/x-icon\" />|g" "$_REEARTH_HTML_FILE"
-fi
