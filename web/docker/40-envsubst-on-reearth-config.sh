1db1bff61aef3ea5c156c3ada0d53ac8930e3028
rot1024 <aayhrot@gmail.com>
2024-11-01 13:34:02 +0900

    chore(web): rewrite title and favicon in docker image

--- web/docker/40-envsubst-on-reearth-config.sh
+++ web/docker/40-envsubst-on-reearth-config.sh
@@ -15,3 +15,17 @@ wrap_reearth_cms_variables() {
 
 wrap_reearth_cms_variables $@
 envsubst < "$_REEARTH_CONFIG_TEMPLATE_FILE" > "$_REEARTH_CONFIG_OUTPUT_FILE"
+
+# rewrite index.html to change title and favicon
+_REEARTH_HTML_FILE="/usr/share/nginx/html/index.html"
+
+# Rewrite title tag in index.html only if REEARTH_CMS_TITLE is set
+if [ -n "$REEARTH_CMS_TITLE" ]; then
+  sed -i '' -e "s|<title>.*</title>|<title>${REEARTH_CMS_TITLE}</title>|g" "$_REEARTH_HTML_FILE"
+fi
+
+# Rewrite favicon in index.html only if REEARTH_CMS_FAVICON is set
+#
+if [ -n "$REEARTH_CMS_FAVICON" ]; then
+  sed -i '' -e "s|<link rel=\"icon\" href=\"[^\"]*\" type=\"image/x-icon\" />|<link rel=\"icon\" href=\"${REEARTH_CMS_FAVICON}\" type=\"image/x-icon\" />|g" "$_REEARTH_HTML_FILE"
+fi
