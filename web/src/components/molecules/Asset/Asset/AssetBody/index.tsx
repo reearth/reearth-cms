828bce74ed7269289cb75c85248f8bc3847300d4
caichi <caichi0424@gmail.com>
2024-11-19 18:05:59 +0900

    impl

--- web/src/components/molecules/Asset/Asset/AssetBody/index.tsx
+++ web/src/components/molecules/Asset/Asset/AssetBody/index.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Viewer as CesiumViewer } from "cesium";
 
 import Button from "@reearth-cms/components/atoms/Button";
 import ComplexInnerContents from "@reearth-cms/components/atoms/InnerContents/complex";
@@ -29,6 +30,7 @@ type Props = {
   onChangeToFullScreen: () => void;
   onBack: () => void;
   onSave: () => void;
+  onGetViewer: (viewer?: CesiumViewer) => void;
   workspaceSettings: WorkspaceSettings;
 };
 
@@ -51,6 +53,7 @@ const AssetWrapper: React.FC<Props> = ({
   onChangeToFullScreen,
   onBack,
   onSave,
+  onGetViewer,
   workspaceSettings,
 }) => {
   const t = useT();
@@ -82,6 +85,7 @@ const AssetWrapper: React.FC<Props> = ({
             onTypeChange={onTypeChange}
             onModalCancel={onModalCancel}
             onChangeToFullScreen={onChangeToFullScreen}
+            onGetViewer={onGetViewer}
             workspaceSettings={workspaceSettings}
           />
         </Wrapper>
