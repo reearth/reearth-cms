33997a4f90007a7d985002b5cf43596892f78be4
caichi <caichi0424@gmail.com>
2024-11-28 21:07:06 +0900

    fix: validator logic

--- web/src/components/molecules/Content/Form/fields/utils.ts
+++ web/src/components/molecules/Content/Form/fields/utils.ts
@@ -1,10 +1,13 @@
-import { Rule } from "@reearth-cms/components/atoms/Form";
+import { RuleObject } from "@reearth-cms/components/atoms/Form";
 
 export const checkIfEmpty = (value: unknown) =>
   value === undefined || value === null || value === "";
 
-export const requiredValidator = (_: Rule, value: unknown) => {
-  if (checkIfEmpty(value) || (Array.isArray(value) && value.every(v => checkIfEmpty(v)))) {
+export const requiredValidator = (rule: RuleObject, value: unknown) => {
+  if (
+    rule.required &&
+    (checkIfEmpty(value) || (Array.isArray(value) && value.every(v => checkIfEmpty(v))))
+  ) {
     return Promise.reject();
   }
   return Promise.resolve();
