2e6e3f11d3e992431dfae7d6d9edb1f53118fefd
Frans Caisar Ramadhan <franz.ramadhan@gmail.com>
2025-02-26 17:28:19 +0700

    fix(oss): revert debug and test to quote (#1420)
    * Revert "fix(oss): check why getting invalid JSON (#1419)"
    
    This reverts commit fb2ac1c81b654d98b213cd74a35e5f257266b4fe.
    
    * fix(web): test quote cesiumIonAccessToken value
    
    * fix(web): debug jsonfile result

--- web/docker/40-envsubst-on-reearth-config.sh
+++ web/docker/40-envsubst-on-reearth-config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
+
 # rewrite index.html to change title and favicon
 _REEARTH_HTML_FILE="/usr/share/nginx/html/index.html"
 
@@ -30,14 +31,11 @@ wrap_reearth_cms_variables() {
     done
 }
 
-
-
 wrap_reearth_cms_variables "$@"
 envsubst < "$_REEARTH_CONFIG_TEMPLATE_FILE" > "$_REEARTH_CONFIG_OUTPUT_FILE"
-
 # TODO: remove after debugging completed
 cat $_REEARTH_CONFIG_OUTPUT_FILE
-
+# TODO: remove after debugging completed
 if ! jq empty "$_REEARTH_CONFIG_OUTPUT_FILE" > /dev/null 2>&1; then
   echo "Invalid JSON configuration file $_REEARTH_CONFIG_OUTPUT_FILE" >&2
   exit 1
