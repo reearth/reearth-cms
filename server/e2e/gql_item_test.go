40a3177734cd0c31c73f6efdc99d036253197e16
Jason Karel <jasonkarel@Jasons-MacBook-Pro.local>
2025-03-05 11:12:38 +0700

    revert clearItemValues fix

--- server/e2e/gql_item_test.go
+++ server/e2e/gql_item_test.go
@@ -1,7 +1,6 @@
 package e2e
 
 import (
-	"fmt"
 	"net/http"
 	"testing"
 	"time"
@@ -170,7 +169,7 @@ func getItem(e *httpexpect.Expect, iID string) (string, *httpexpect.Value) {
 		Status(http.StatusOK).
 		JSON()
 
-	return fmt.Sprintf("%v", res.Path("$.data.createItem.item.id").Raw()), res
+	return res.Path("$.data.node.version").Raw().(string), res
 }
 
 func SearchItem(e *httpexpect.Expect, query, sort, filter, pagination map[string]any) *httpexpect.Value {
