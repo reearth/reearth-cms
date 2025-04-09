5e17ea40fe49c4ea77013c0093c26e8ba22dbe38
ymk <81808708+yk-eukarya@users.noreply.github.com>
2025-04-09 14:35:25 +0200

    fix test

--- server/e2e/internal_project_test.go
+++ server/e2e/internal_project_test.go
@@ -2,14 +2,9 @@ package e2e
 
 import (
 	"testing"
-
-	"github.com/reearth/reearth-cms/server/internal/app"
 )
 
 // GET /assets/{assetId}
 func TestInternalGetProjectsAPI(t *testing.T) {
-	e := StartServer(t, &app.Config{}, true, baseSeeder)
-
-	e = e
-
+	// TODO: Implement the test
 }
