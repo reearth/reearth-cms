6505474e8b3fd8f4bef61d01fac8c3a7511b8e9b
nourbalaha <nourbalaha0@gmail.com>
2025-04-22 12:06:28 +0900

    add REEARTH_E2E_ACCESS_TOKEN to useAuthHeader

--- web/src/gql/useAuthHeader.ts
+++ web/src/gql/useAuthHeader.ts
@@ -4,7 +4,7 @@ export const useAuthHeader = () => {
   const { getAccessToken } = useAuth();
 
   const getHeader = async () => {
-    const token = await getAccessToken();
+    const token = window.REEARTH_E2E_ACCESS_TOKEN || (await getAccessToken());
     return {
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
     };
