8cb72f9679a4ae08ce8d8e8c7db6bf5fcaab1f21
Jason Karel <jasonkarel@Jasons-MacBook-Pro.local>
2025-06-02 15:11:40 +0700

    remove unnecessary comment

--- worker/internal/app/app.go
+++ worker/internal/app/app.go
@@ -40,7 +40,6 @@ func initEcho(ctx context.Context, cfg *ServerConfig, handler *Handler) *echo.Ec
 
 	api.GET("/ping", func(c echo.Context) error { return c.JSON(http.StatusOK, "pong") })
 
-	// Health check endpoint
 	e.GET("/health", HealthCheck(cfg.Config, cfg.Version))
 
 	t := handler.DecompressHandler()
