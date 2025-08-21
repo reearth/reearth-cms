3b3eb19b99fb7e2a7b3e70eae41b887fb5456285
nourbalaha <nourbalaha0@gmail.com>
2025-08-21 13:42:12 +0900

    replace dashboard profile url with dashboard base url

--- web/src/components/molecules/Common/WorkspaceMenu/index.tsx
+++ web/src/components/molecules/Common/WorkspaceMenu/index.tsx
@@ -1,10 +1,10 @@
+import styled from "@emotion/styled";
 import { ItemType } from "antd/lib/menu/interface";
 import { useCallback, useEffect, useMemo, useState } from "react";
 
 import Icon from "@reearth-cms/components/atoms/Icon";
 import Menu, { MenuInfo } from "@reearth-cms/components/atoms/Menu";
 import { useT } from "@reearth-cms/i18n";
-import { parseConfigBoolean } from "@reearth-cms/utils/format";
 
 type Props = {
   inlineCollapsed: boolean;
@@ -15,7 +15,7 @@ type Props = {
 
 type MenuShowType = "personal" | "notPersonal" | "both";
 
-type WorkspaceItemType = ItemType & { show: MenuShowType; order: number };
+type WorkspaceItemType = ItemType & { show: MenuShowType };
 
 const WorkspaceMenu: React.FC<Props> = ({
   inlineCollapsed,
@@ -31,67 +31,56 @@ const WorkspaceMenu: React.FC<Props> = ({
   }, [defaultSelectedKey]);
 
   const topItems: WorkspaceItemType[] = [
-    { label: t("Home"), key: "home", icon: <Icon icon="home" />, show: "both", order: 1 },
+    { label: t("Home"), key: "home", icon: <Icon icon="home" />, show: "both" },
   ];
 
-  const disableWorkspaceUi = parseConfigBoolean(window.REEARTH_CONFIG?.disableWorkspaceUi);
   const items: WorkspaceItemType[] = useMemo(() => {
     const res = [
+      {
+        label: t("Member"),
+        key: "members",
+        icon: <Icon icon="userGroupAdd" />,
+        show: "notPersonal" as MenuShowType,
+      },
       {
         label: t("Integrations"),
         key: "integrations",
         icon: <Icon icon="api" />,
         show: "both" as MenuShowType,
-        order: 3,
       },
       {
         label: t("My Integrations"),
         key: "myIntegrations",
         icon: <Icon icon="myIntegrations" />,
         show: "personal" as MenuShowType,
-        order: 4,
       },
       {
         label: t("Settings"),
         key: "settings",
         icon: <Icon icon="settings" />,
         show: "both" as MenuShowType,
-        order: 5,
       },
-    ];
-    if (!disableWorkspaceUi) {
-      res.push({
-        label: t("Member"),
-        key: "members",
-        icon: <Icon icon="userGroupAdd" />,
-        show: "notPersonal" as MenuShowType,
-        order: 2,
-      });
-      res.push({
-        label: t("Workspace"),
+      {
+        label: t("Workspace Settings"),
         key: "workspaceSettings",
         icon: <Icon size={"1em"} icon="workspaceSettings" />,
         show: "notPersonal" as MenuShowType,
-        order: 6,
-      });
-      res.push({
-        label: t("Account"),
+      },
+      {
+        label: t("Account Settings"),
         key: "account",
         icon: <Icon icon="user" />,
         show: "personal" as MenuShowType,
-        order: 7,
-      });
-    }
+      },
+    ];
 
-    return res
-      .filter(
-        item =>
-          (isPersonalWorkspace && item.show === "personal") ||
-          (!isPersonalWorkspace && item.show === "notPersonal") ||
-          item.show === "both",
-      )
-      .sort((a, b) => a.order - b.order);
-  }, [t, isPersonalWorkspace, disableWorkspaceUi]);
+    return res.filter(
+      item =>
+        (isPersonalWorkspace && item.show === "personal") ||
+        (!isPersonalWorkspace && item.show === "notPersonal") ||
+        item.show === "both",
+    );
+  }, [t, isPersonalWorkspace]);
 
   const onClick = useCallback(
     (info: MenuInfo) => {
@@ -103,14 +92,14 @@ const WorkspaceMenu: React.FC<Props> = ({
 
   return (
     <>
-      <Menu
+      <StyledMenu
         onClick={onClick}
         selectedKeys={selected}
         inlineCollapsed={inlineCollapsed}
         mode="inline"
         items={topItems}
       />
-      <Menu
+      <StyledMenu
         onClick={onClick}
         selectedKeys={selected}
         inlineCollapsed={inlineCollapsed}
@@ -122,3 +111,9 @@ const WorkspaceMenu: React.FC<Props> = ({
 };
 
 export default WorkspaceMenu;
+
+const StyledMenu = styled(Menu)`
+  li {
+    padding-left: 16px !important;
+  }
+`;
