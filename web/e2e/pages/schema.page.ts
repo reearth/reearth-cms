a0946a56dc6a24c4366cc7255d89017e3ada8ff2
nourbalaha <nourbalaha0@gmail.com>
2025-09-03 11:51:57 +0900

    wip schema page

--- web/e2e/pages/schema.page.ts
+++ web/e2e/pages/schema.page.ts
@@ -1,270 +1,118 @@
-import { expect, Locator } from "@reearth-cms/e2e/fixtures/test";
+import { Locator } from "@reearth-cms/e2e/fixtures/test";
 
 import { BasePage } from "./base.page";
 
 export const DEFAULT_MODEL_NAME = "e2e model name";
 
 export class SchemaPage extends BasePage {
-  private get newModelButton(): Locator {
-    return this.getByRole("button", { name: "plus New Model" });
-  }
-
-  private get addModelButton(): Locator {
-    return this.getByRole("button", { name: "plus Add" });
-  }
-
-  private get modelNameInput(): Locator {
-    return this.getByLabel("Model name");
-  }
-
-  private get modelKeyInput(): Locator {
-    return this.getByLabel("Model key");
-  }
-
-  private get okButton(): Locator {
-    return this.getByRole("button", { name: "OK" });
-  }
-
-  private get moreButton(): Locator {
-    return this.getByRole("button", { name: "more" });
-  }
-
-  private get editButton(): Locator {
-    return this.getByText("Edit", { exact: true });
-  }
-
-  private get deleteButton(): Locator {
-    return this.getByText("Delete");
-  }
-
-  private get deleteModelButton(): Locator {
-    return this.getByRole("button", { name: "Delete Model" });
-  }
-
-  private get updateModelDialog(): Locator {
-    return this.getByLabel("Update Model");
-  }
-
-  private get titleElement(): Locator {
+  get pageTitle(): Locator {
     return this.locator(".ant-page-header-heading-title");
   }
 
-  async expectModelsPageTitle(): Promise<void> {
-    await expect(this.titleElement).toHaveText("Models");
+  get mainMenu(): Locator {
+    return this.getByRole("main").getByRole("menu").first();
+  }
+  get lastMenu(): Locator {
+    return this.getByRole("main").getByRole("menu").last();
   }
 
-  async createModelFromOverview(name = DEFAULT_MODEL_NAME, key?: string): Promise<void> {
-    await this.expectModelsPageTitle();
-    await this.newModelButton.first().click();
-    await this.fillModelForm(name, key);
+  get newModelButton(): Locator {
+    return this.getByRole("button", { name: "plus New Model" });
   }
 
-  async createModelFromSidebar(name = DEFAULT_MODEL_NAME, key?: string): Promise<void> {
-    await this.addModelButton.first().click();
-    await this.fillModelForm(name, key);
+  get addModelButton(): Locator {
+    return this.getByRole("button", { name: "plus Add" });
   }
 
-  private async fillModelForm(name: string, key?: string): Promise<void> {
-    await this.modelNameInput.fill(name);
-    if (key) {
-      await this.modelKeyInput.fill(key);
-    }
-    await this.okButton.click();
-    await this.closeNotification();
+  get modelNameInput(): Locator {
+    return this.getByLabel("Model name");
   }
 
-  async updateModel(name = "new e2e model name", key: string): Promise<void> {
-    await this.moreButton.hover();
-    await this.editButton.click();
-    await this.updateModelDialog.locator("#name").fill(name);
-    await this.updateModelDialog.locator("#key").fill(key);
-    await this.okButton.click();
-    await this.closeNotification();
+  get modelKeyInput(): Locator {
+    return this.getByLabel("Model key");
   }
 
-  async deleteModel(): Promise<void> {
-    await this.moreButton.hover();
-    await this.deleteButton.click();
-    await this.deleteModelButton.click();
-    await this.closeNotification();
+  get okButton(): Locator {
+    return this.getByRole("button", { name: "OK" });
   }
 
-  async dragModel(sourceModelName: string, targetModelName: string): Promise<void> {
-    const sourceModel = this.getByRole("menuitem", { name: sourceModelName });
-    const targetModel = this.getByRole("menuitem", { name: targetModelName });
-    await sourceModel.dragTo(targetModel);
-    await this.closeNotification();
+  get cancelButton(): Locator {
+    return this.getByRole("button", { name: "Cancel" });
   }
 
-  async expectModelInPosition(position: number, modelName: string): Promise<void> {
-    await expect(this.getByRole("main").getByRole("menuitem").nth(position)).toContainText(
-      modelName,
-    );
+  get moreButton(): Locator {
+    return this.getByRole("button", { name: "more" });
   }
 
-  async expectModelVisible(modelName: string): Promise<void> {
-    await expect(this.getByTitle(modelName, { exact: true })).toBeVisible();
+  get editMenuItem(): Locator {
+    return this.getByText("Edit", { exact: true });
+  }
+  get deleteMenuItem(): Locator {
+    return this.getByText("Delete");
   }
 
-  async expectModelHidden(modelName: string): Promise<void> {
-    await expect(this.getByTitle(modelName)).toBeHidden();
+  get deleteModelConfirmButton(): Locator {
+    return this.getByRole("button", { name: "Delete Model" });
   }
 
-  async expectModelKey(modelKey: string): Promise<void> {
-    await expect(this.getByText(`#${modelKey}`)).toBeVisible();
+  get updateModelDialog(): Locator {
+    return this.getByLabel("Update Model");
   }
 
-  async expectModelInSidebar(modelName: string): Promise<void> {
-    await expect(this.getByRole("menuitem", { name: modelName }).locator("span")).toBeVisible();
+  get titleArea(): Locator {
+    return this.getByTitle(/.*/);
   }
 
-  // Field operations
-  async addTextField(): Promise<void> {
-    await this.locator("li").filter({ hasText: "Text" }).locator("div").first().click();
+  get keyBadgeArea(): Locator {
+    return this.getByText(/#.+/);
   }
 
-  async addGroupField(): Promise<void> {
-    await this.locator("li").filter({ hasText: "Group" }).locator("div").first().click();
+  get paletteListItems(): Locator {
+    return this.locator("li");
   }
 
-  async createGroupFromFieldDialog(): Promise<void> {
-    await this.getByRole("button", { name: "Create Group" }).click();
+  get fieldsContainer(): Locator {
+    return this.getByLabel("Fields");
   }
 
-  async fillGroupDialogForm(groupName: string, groupKey: string): Promise<void> {
-    await this.getByLabel("New Group").locator("#name").fill(groupName);
-    await this.getByLabel("New Group").locator("#key").fill(groupKey);
+  get fieldRowEllipsis(): Locator {
+    return this.getByRole("img", { name: "ellipsis" }).locator("svg");
+  }
+  get fieldDeleteIcon(): Locator {
+    return this.getByLabel("delete").locator("svg");
   }
 
-  async expectCreateGroupOKDisabled(): Promise<void> {
-    await expect(this.getByRole("button", { name: "OK" })).toBeDisabled();
+  get createGroupFromFieldButton(): Locator {
+    return this.getByRole("button", { name: "Create Group" });
+  }
+  get newGroupDialog(): Locator {
+    return this.getByLabel("New Group");
   }
 
-  async expectGroupInSidebar(groupName: string): Promise<void> {
-    await expect(this.getByRole("menuitem", { name: groupName }).locator("span")).toBeVisible();
+  get createGroupFieldDialogTitle(): Locator {
+    return this.getByText("Create Group Field");
+  }
+  get groupSelectTrigger(): Locator {
+    return this.locator(".ant-select-selector");
   }
 
-  async expectFieldsMetaDataHidden(): Promise<void> {
-    await expect(this.getByText("FieldsMeta Data")).toBeHidden();
+  get addGroupButton(): Locator {
+    return this.getByRole("button", { name: "plus Add Group" });
   }
 
-  async expectReferenceFieldHidden(): Promise<void> {
-    await expect(this.locator("li").getByText("Reference", { exact: true })).toBeHidden();
-  }
-
-  async expectGroupFieldHidden(): Promise<void> {
-    await expect(this.locator("li").getByText("Group", { exact: true })).toBeHidden();
-  }
-
-  async clickCreateGroupField(): Promise<void> {
-    await this.locator("li").getByText("Group", { exact: true }).click();
-  }
-
-  async expectCreateGroupFieldDialog(): Promise<void> {
-    await expect(this.getByText("Create Group Field")).toBeVisible();
-  }
-
-  async selectGroupInDropdown(groupName: string, groupKey: string): Promise<void> {
-    await this.locator(".ant-select-selector").click();
-    await expect(this.getByText(`${groupName} #${groupKey}`)).toBeVisible();
-  }
-
-  async cancelGroupFieldDialog(): Promise<void> {
-    await this.getByRole("button", { name: "Cancel" }).click();
-  }
-
-  async clickFieldEdit(): Promise<void> {
-    await this.getByRole("img", { name: "ellipsis" }).locator("svg").click();
-  }
-
-  async deleteField(): Promise<void> {
-    await this.getByLabel("delete").locator("svg").click();
-    await this.getByRole("button", { name: "OK" }).click();
-    await this.closeNotification();
-  }
-
-  async expectFieldHidden(fieldName: string, fieldKey: string): Promise<void> {
-    await expect(this.getByText(`${fieldName} #${fieldKey}`)).toBeHidden();
-  }
-
-  async expectFieldVisible(fieldName: string, fieldKey: string): Promise<void> {
-    await expect(this.getByText(`${fieldName}#${fieldKey}`)).toBeVisible();
-  }
-
-  async expectFieldInPosition(
-    position: number,
-    fieldName: string,
-    fieldKey: string,
-  ): Promise<void> {
-    await expect(this.getByLabel("Fields").locator(".draggable-item").nth(position)).toContainText(
-      `${fieldName}#${fieldKey}`,
-    );
-  }
-
-  async dragField(fromPosition: number, toPosition: number): Promise<void> {
-    const sourceField = this.getByLabel("Fields").locator(".grabbable").nth(fromPosition);
-    const targetField = this.getByLabel("Fields").locator(".draggable-item").nth(toPosition);
-    await sourceField.dragTo(targetField);
-    await this.closeNotification();
-  }
-
-  async clickModelInSidebar(modelName: string): Promise<void> {
-    await this.getByText(modelName).click();
-  }
-
-  // Group operations (need to extract from group utils)
-  private get groupNameInput(): Locator {
+  get groupNameInput(): Locator {
     return this.getByLabel("Group name");
   }
 
-  private get groupKeyInput(): Locator {
+  get groupKeyInput(): Locator {
     return this.getByLabel("Group key");
   }
 
-  async createGroup(groupName: string, groupKey: string): Promise<void> {
-    await this.getByRole("button", { name: "plus Add Group" }).click();
-    await this.groupNameInput.fill(groupName);
-    await this.groupKeyInput.fill(groupKey);
-    await this.okButton.click();
-    await this.closeNotification();
+  get deleteGroupConfirmButton(): Locator {
+    return this.getByRole("button", { name: "Delete Group" });
   }
 
-  async updateGroup(groupName: string, groupKey: string): Promise<void> {
-    await this.moreButton.hover();
-    await this.editButton.click();
-    await this.groupNameInput.clear();
-    await this.groupNameInput.fill(groupName);
-    await this.groupKeyInput.clear();
-    await this.groupKeyInput.fill(groupKey);
-    await this.okButton.click();
-    await this.closeNotification();
-  }
-
-  async deleteGroup(): Promise<void> {
-    await this.moreButton.hover();
-    await this.deleteButton.click();
-    await this.getByRole("button", { name: "Delete Group" }).click();
-    await this.closeNotification();
-  }
-
-  async dragGroup(fromPosition: number, toPosition: number): Promise<void> {
-    const sourceGroup = this.getByRole("main")
-      .getByRole("menu")
-      .last()
-      .getByRole("menuitem")
-      .nth(fromPosition);
-    const targetGroup = this.getByRole("main")
-      .getByRole("menu")
-      .last()
-      .getByRole("menuitem")
-      .nth(toPosition);
-    await sourceGroup.dragTo(targetGroup);
-    await this.closeNotification();
-  }
-
-  async expectGroupInPosition(position: number, groupName: string): Promise<void> {
-    await expect(
-      this.getByRole("main").getByRole("menu").last().getByRole("menuitem").nth(position),
-    ).toContainText(groupName);
+  get fieldsMetaDataText(): Locator {
+    return this.getByText("FieldsMeta Data");
   }
 }
--- web/e2e/project/schema.spec.ts
+++ web/e2e/project/schema.spec.ts
@@ -1,144 +1,224 @@
-/* eslint-disable playwright/expect-expect */
+import { closeNotification } from "@reearth-cms/e2e/common/notification";
 import { expect, test } from "@reearth-cms/e2e/fixtures/test";
-import { getId } from "@reearth-cms/e2e/utils/mock";
+import { handleFieldForm } from "@reearth-cms/e2e/project/utils/field";
+import { createProject, deleteProject } from "@reearth-cms/e2e/project/utils/project";
 
-import { handleFieldForm } from "./utils/field";
-
-test.beforeEach(async ({ reearth, homePage, projectLayoutPage }) => {
+test.beforeEach(async ({ reearth, page, projectLayoutPage }) => {
   await reearth.goto("/", { waitUntil: "domcontentloaded" });
-  await homePage.createProject(getId());
+  await createProject(page);
   await projectLayoutPage.navigateToSchema();
 });
 
-test.afterEach(async ({ projectLayoutPage, projectSettingsPage }) => {
-  await projectLayoutPage.navigateToSettings();
-  await projectSettingsPage.deleteProject();
+test.afterEach(async ({ page }) => {
+  await deleteProject(page);
 });
+
+async function createModelFromSidebar(schemaPage: any, name: string, key?: string) {
+  await schemaPage.addModelButton.first().click();
+  await schemaPage.modelNameInput.fill(name);
+  if (key) await schemaPage.modelKeyInput.fill(key);
+  await schemaPage.okButton.click();
+  await closeNotification(schemaPage["page"]);
+}
+async function updateModel(schemaPage: any, name: string, key: string) {
+  await schemaPage.moreButton.hover();
+  await schemaPage.editMenuItem.click();
+  const dlg = schemaPage.updateModelDialog;
+  await dlg.locator("#name").fill(name);
+  await dlg.locator("#key").fill(key);
+  await schemaPage.okButton.click();
+  await closeNotification(schemaPage["page"]);
+}
+async function deleteModel(schemaPage: any) {
+  await schemaPage.moreButton.hover();
+  await schemaPage.deleteMenuItem.click();
+  await schemaPage.deleteModelConfirmButton.click();
+  await closeNotification(schemaPage["page"]);
+}
+async function createGroup(schemaPage: any, name: string, key: string) {
+  await schemaPage.addGroupButton.click();
+  await schemaPage.groupNameInput.fill(name);
+  await schemaPage.groupKeyInput.fill(key);
+  await schemaPage.okButton.click();
+  await closeNotification(schemaPage["page"]);
+}
+async function updateGroup(schemaPage: any, name: string, key: string) {
+  await schemaPage.moreButton.hover();
+  await schemaPage.editMenuItem.click();
+  await schemaPage.groupNameInput.clear();
+  await schemaPage.groupNameInput.fill(name);
+  await schemaPage.groupKeyInput.clear();
+  await schemaPage.groupKeyInput.fill(key);
+  await schemaPage.okButton.click();
+  await closeNotification(schemaPage["page"]);
+}
+async function deleteGroup(schemaPage: any) {
+  await schemaPage.moreButton.hover();
+  await schemaPage.deleteMenuItem.click();
+  await schemaPage.deleteGroupConfirmButton.click();
+  await closeNotification(schemaPage["page"]);
+}
 
 test("Model CRUD has succeeded", async ({ schemaPage }) => {
   const modelName = "model name";
   const modelKey = "model-key";
-  await schemaPage.createModelFromSidebar(modelName, modelKey);
-  await schemaPage.expectModelVisible(modelName);
-  await schemaPage.expectModelKey(modelKey);
-  await schemaPage.expectModelInSidebar(modelName);
 
-  const newModelName = "new model name";
-  const newModelKey = "new-model-key";
-  await schemaPage.updateModel(newModelName, newModelKey);
-  await schemaPage.expectModelVisible(newModelName);
-  await schemaPage.expectModelKey(newModelKey);
-  await schemaPage.expectModelInSidebar(newModelName);
+  await createModelFromSidebar(schemaPage, modelName, modelKey);
 
-  await schemaPage.deleteModel();
-  await schemaPage.expectModelHidden(newModelName);
+  await expect(schemaPage.getByTitle(modelName, { exact: true })).toBeVisible();
+  await expect(schemaPage.getByText(`#${modelKey}`)).toBeVisible();
+  await expect(schemaPage.getByRole("menuitem", { name: modelName }).locator("span")).toBeVisible();
+
+  const newName = "new model name";
+  const newKey = "new-model-key";
+  await updateModel(schemaPage, newName, newKey);
+
+  await expect(schemaPage.getByTitle(newName)).toBeVisible();
+  await expect(schemaPage.getByText(`#${newKey}`)).toBeVisible();
+  await expect(schemaPage.getByRole("menuitem", { name: newName }).locator("span")).toBeVisible();
+
+  await deleteModel(schemaPage);
+  await expect(schemaPage.getByTitle(newName)).toBeHidden();
 });
 
 test("Model reordering has succeeded", async ({ schemaPage }) => {
-  const modelName1 = "model1";
-  const modelName2 = "model2";
-  const modelName3 = "model3";
+  const m1 = "model1";
+  const m2 = "model2";
+  const m3 = "model3";
 
-  await schemaPage.createModelFromSidebar(modelName1);
-  await schemaPage.createModelFromSidebar(modelName2);
-  await schemaPage.expectModelInPosition(0, modelName1);
-  await schemaPage.expectModelInPosition(1, modelName2);
+  await createModelFromSidebar(schemaPage, m1);
+  await createModelFromSidebar(schemaPage, m2);
 
-  await schemaPage.dragModel(modelName2, modelName1);
-  await schemaPage.expectModelInPosition(0, modelName2);
-  await schemaPage.expectModelInPosition(1, modelName1);
+  await expect(schemaPage.mainMenu.getByRole("menuitem").nth(0)).toContainText(m1);
+  await expect(schemaPage.mainMenu.getByRole("menuitem").nth(1)).toContainText(m2);
 
-  await schemaPage.createModelFromSidebar(modelName3);
-  await schemaPage.expectModelInPosition(0, modelName2);
-  await schemaPage.expectModelInPosition(1, modelName1);
-  await schemaPage.expectModelInPosition(2, modelName3);
+  await schemaPage
+    .getByRole("menuitem", { name: m2 })
+    .dragTo(schemaPage.getByRole("menuitem", { name: m1 }));
+  await closeNotification(schemaPage["page"]);
+
+  await expect(schemaPage.mainMenu.getByRole("menuitem").nth(0)).toContainText(m2);
+  await expect(schemaPage.mainMenu.getByRole("menuitem").nth(1)).toContainText(m1);
+
+  await createModelFromSidebar(schemaPage, m3);
+  await expect(schemaPage.mainMenu.getByRole("menuitem").nth(0)).toContainText(m2);
+  await expect(schemaPage.mainMenu.getByRole("menuitem").nth(1)).toContainText(m1);
+  await expect(schemaPage.mainMenu.getByRole("menuitem").nth(2)).toContainText(m3);
 });
 
 test("Group CRUD has succeeded", async ({ schemaPage }) => {
-  const groupName = "e2e group name";
-  const groupKey = "e2e-group-key";
-  const updateGroupName = "new e2e group name";
-  const updateGroupKey = "new-e2e-group-key";
+  const g = "e2e group name";
+  const k = "e2e-group-key";
+  const g2 = "new e2e group name";
+  const k2 = "new-e2e-group-key";
 
-  await schemaPage.createGroup(groupName, groupKey);
-  await schemaPage.expectModelVisible(groupName);
-  await schemaPage.expectModelKey(groupKey);
+  await createGroup(schemaPage, g, k);
+  await expect(schemaPage.getByTitle(g, { exact: true })).toBeVisible();
+  await expect(schemaPage.getByText(`#${k}`)).toBeVisible();
 
-  await schemaPage.updateGroup(updateGroupName, updateGroupKey);
-  await schemaPage.expectModelVisible(updateGroupName);
-  await schemaPage.expectModelKey(updateGroupKey);
-  await schemaPage.expectGroupInSidebar(updateGroupName);
+  await updateGroup(schemaPage, g2, k2);
+  await expect(schemaPage.getByTitle(g2)).toBeVisible();
+  await expect(schemaPage.getByText(`#${k2}`)).toBeVisible();
+  await expect(schemaPage.getByRole("menuitem", { name: g2 })).toBeVisible();
 
-  await schemaPage.deleteGroup();
-  await schemaPage.expectModelHidden(updateGroupName);
+  await deleteGroup(schemaPage);
+  await expect(schemaPage.getByTitle(g2)).toBeHidden();
 });
 
 test("Group creating from adding field has succeeded", async ({ page, schemaPage }) => {
-  const groupName = "e2e group name";
-  const groupKey = "e2e-group-key";
+  await createModelFromSidebar(schemaPage, "e2e model name");
 
-  await schemaPage.createModelFromSidebar();
-  await schemaPage.addGroupField();
-  await schemaPage.createGroupFromFieldDialog();
-  await expect(schemaPage.getByLabel("New Group")).toContainText("New Group");
-  await schemaPage.expectCreateGroupOKDisabled();
+  await schemaPage.paletteListItems.filter({ hasText: "Group" }).locator("div").first().click();
+  await schemaPage.createGroupFromFieldButton.click();
+  await expect(schemaPage.newGroupDialog).toContainText("New Group");
+  await expect(schemaPage.okButton).toBeDisabled();
 
-  await schemaPage.fillGroupDialogForm(groupName, groupKey);
-  await schemaPage.getByRole("button", { name: "OK" }).click();
-  await schemaPage.closeNotification();
+  await schemaPage.newGroupDialog.locator("#name").fill("e2e group name");
+  await schemaPage.newGroupDialog.locator("#key").fill("e2e-group-key");
+  await schemaPage.okButton.click();
+  await closeNotification(page);
 
-  await schemaPage.expectGroupInSidebar(groupName);
-  await schemaPage.expectFieldVisible(groupName, groupKey);
-  await schemaPage.expectFieldsMetaDataHidden();
-  await schemaPage.expectReferenceFieldHidden();
-  await schemaPage.expectGroupFieldHidden();
+  await expect(
+    schemaPage.getByRole("menuitem", { name: "e2e group name" }).locator("span"),
+  ).toBeVisible();
+  await expect(schemaPage.getByText("e2e group name#e2e-group-key")).toBeVisible();
+  await expect(schemaPage.fieldsMetaDataText).toBeHidden();
+  await expect(schemaPage.paletteListItems.getByText("Reference", { exact: true })).toBeHidden();
+  await expect(schemaPage.paletteListItems.getByText("Group", { exact: true })).toBeHidden();
 
-  await schemaPage.addTextField();
+  // Add a Text field and open "Create Group Field"
+  await schemaPage.paletteListItems.filter({ hasText: "Text" }).locator("div").first().click();
   await handleFieldForm(page, "text");
-  await schemaPage.clickModelInSidebar("e2e model name");
-  await schemaPage.clickCreateGroupField();
-  await schemaPage.expectCreateGroupFieldDialog();
-  await schemaPage.selectGroupInDropdown(groupName, groupKey);
-  await schemaPage.cancelGroupFieldDialog();
+  await schemaPage.getByText("e2e model name").click();
+  await schemaPage.paletteListItems.getByText("Group", { exact: true }).click();
+  await expect(schemaPage.createGroupFieldDialogTitle).toBeVisible();
+  await schemaPage.groupSelectTrigger.click();
+  await expect(schemaPage.getByText("e2e group name #e2e-group-key")).toBeVisible();
+  await schemaPage.cancelButton.click();
 });
 
 test("Group reordering has succeeded", async ({ schemaPage }) => {
-  await schemaPage.createGroup("group1", "group1");
-  await schemaPage.createGroup("group2", "group2");
-  await schemaPage.expectGroupInPosition(0, "group1");
-  await schemaPage.expectGroupInPosition(1, "group2");
+  await createGroup(schemaPage, "group1", "group1");
+  await createGroup(schemaPage, "group2", "group2");
 
-  await schemaPage.dragGroup(1, 0);
-  await schemaPage.expectGroupInPosition(0, "group2");
-  await schemaPage.expectGroupInPosition(1, "group1");
+  await expect(schemaPage.lastMenu.getByRole("menuitem").nth(0)).toContainText("group1");
+  await expect(schemaPage.lastMenu.getByRole("menuitem").nth(1)).toContainText("group2");
 
-  await schemaPage.createGroup("group3", "group3");
-  await schemaPage.expectGroupInPosition(0, "group2");
-  await schemaPage.expectGroupInPosition(1, "group1");
-  await schemaPage.expectGroupInPosition(2, "group3");
+  await schemaPage
+    .lastMenu.getByRole("menuitem")
+    .nth(1)
+    .dragTo(schemaPage.lastMenu.getByRole("menuitem").nth(0));
+  await closeNotification(schemaPage["page"]);
+
+  await expect(schemaPage.lastMenu.getByRole("menuitem").nth(0)).toContainText("group2");
+  await expect(schemaPage.lastMenu.getByRole("menuitem").nth(1)).toContainText("group1");
+
+  await createGroup(schemaPage, "group3", "group3");
+  await expect(schemaPage.lastMenu.getByRole("menuitem").nth(0)).toContainText("group2");
+  await expect(schemaPage.lastMenu.getByRole("menuitem").nth(1)).toContainText("group1");
+  await expect(schemaPage.lastMenu.getByRole("menuitem").nth(2)).toContainText("group3");
 });
 
 test("Text field CRUD has succeeded", async ({ page, schemaPage }) => {
-  await schemaPage.createModelFromSidebar();
-  await schemaPage.addTextField();
+  await createModelFromSidebar(schemaPage, "e2e model name");
+
+  await schemaPage.paletteListItems.filter({ hasText: /Text/ }).locator("div").first().click();
   await handleFieldForm(page, "text");
-  await schemaPage.clickFieldEdit();
+
+  await schemaPage.fieldRowEllipsis.click();
   await handleFieldForm(page, "new text", "new-text");
-  await schemaPage.deleteField();
-  await schemaPage.expectFieldHidden("new text", "new-text");
+
+  await schemaPage.fieldDeleteIcon.click();
+  await schemaPage.okButton.click();
+  await closeNotification(page);
+  await expect(schemaPage.getByText("new text #new-text")).toBeHidden();
 });
 
 test("Schema reordering has succeeded", async ({ page, schemaPage }) => {
-  await schemaPage.createModelFromSidebar();
-  await schemaPage.addTextField();
+  await createModelFromSidebar(schemaPage, "e2e model name");
+
+  await schemaPage.paletteListItems.filter({ hasText: /Text/ }).locator("div").first().click();
   await handleFieldForm(page, "text1");
-  await schemaPage.addTextField();
+  await schemaPage.paletteListItems.filter({ hasText: /Text/ }).locator("div").first().click();
   await handleFieldForm(page, "text2");
 
-  await schemaPage.expectFieldInPosition(0, "text1", "text1");
-  await schemaPage.expectFieldInPosition(1, "text2", "text2");
+  await expect(schemaPage.fieldsContainer.locator(".draggable-item").nth(0)).toContainText(
+    "text1#text1",
+  );
+  await expect(schemaPage.fieldsContainer.locator(".draggable-item").nth(1)).toContainText(
+    "text2#text2",
+  );
 
-  await schemaPage.dragField(1, 0);
-  await schemaPage.expectFieldInPosition(0, "text2", "text2");
-  await schemaPage.expectFieldInPosition(1, "text1", "text1");
+  await schemaPage
+    .fieldsContainer.locator(".grabbable")
+    .nth(1)
+    .dragTo(schemaPage.fieldsContainer.locator(".draggable-item").nth(0));
+  await closeNotification(page);
+
+  await expect(schemaPage.fieldsContainer.locator(".draggable-item").nth(0)).toContainText(
+    "text2#text2",
+  );
+  await expect(schemaPage.fieldsContainer.locator(".draggable-item").nth(1)).toContainText(
+    "text1#text1",
+  );
 });
