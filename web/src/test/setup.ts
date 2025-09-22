78ba0d5a0ff23c2732c0554ac0d143e7b5050ec0
Woody Chang <kazettique@gmail.com>
2025-09-22 10:24:48 +0800

    chore: update setup & config

--- web/src/test/setup.ts
+++ web/src/test/setup.ts
@@ -26,7 +26,53 @@ Object.defineProperty(window, "matchMedia", {
 
 beforeAll(() => {
   const { getComputedStyle } = window;
-  window.getComputedStyle = elt => getComputedStyle(elt);
+  window.getComputedStyle = elt => {
+    if (!elt) {
+      // Return a proper mock CSSStyleDeclaration when element is null/undefined
+      return new Proxy({} as CSSStyleDeclaration, {
+        get(target, prop) {
+          if (prop === "getPropertyValue") {
+            return () => "";
+          }
+          return "";
+        },
+        set() {
+          return true;
+        },
+      });
+    }
+    try {
+      const styles = getComputedStyle(elt);
+      // If the original styles are null or undefined, return our proxy instead
+      if (!styles) {
+        return new Proxy({} as CSSStyleDeclaration, {
+          get(target, prop) {
+            if (prop === "getPropertyValue") {
+              return () => "";
+            }
+            return "";
+          },
+          set() {
+            return true;
+          },
+        });
+      }
+      return styles;
+    } catch (_error) {
+      // If there's any error, return our proxy mock
+      return new Proxy({} as CSSStyleDeclaration, {
+        get(target, prop) {
+          if (prop === "getPropertyValue") {
+            return () => "";
+          }
+          return "";
+        },
+        set() {
+          return true;
+        },
+      });
+    }
+  };
 });
 
 afterEach(cleanup);
