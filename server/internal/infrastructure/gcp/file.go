4f3f2108a2e36b195f0ac40ae5aeb457c1170499
“jasonkarel” <jasonkareli@gmail.com>
2025-10-01 19:17:58 +0700

    add debug log and endpointWithBucket

--- server/internal/infrastructure/gcp/file.go
+++ server/internal/infrastructure/gcp/file.go
@@ -92,7 +92,8 @@ func (f *fileRepo) ReadAsset(ctx context.Context, u string, fn string, h map[str
 
 func (f *fileRepo) GetAssetFiles(ctx context.Context, u string) ([]gateway.FileEntry, error) {
 	p := getGCSObjectPath(u, "")
-	b, err := f.bucket(ctx)
+	// Use standard client for GetAssetFiles to avoid bucket name corruption
+	b, err := f.bucketWithOptions(ctx, true)
 	if err != nil {
 		return nil, rerror.ErrInternalBy(err)
 	}
@@ -397,6 +398,7 @@ func (f *fileRepo) Upload(ctx context.Context, file *file.File, objectName strin
 		return 0, rerror.ErrInternalBy(err)
 	}
 
+	log.Debugf("DEBUG Upload: About to perform GCS operations with bucketName='%s', objectName='%s'", f.bucketName, objectName)
 	object := bucket.Object(objectName)
 	if err := object.Delete(ctx); err != nil && !errors.Is(err, storage.ErrObjectNotExist) {
 		log.Errorf("gcs: upload err: %+v\n", err)
@@ -617,17 +619,29 @@ func (f *fileRepo) bucketWithOptions(ctx context.Context, forSigning bool) (*sto
 	var client *storage.Client
 	var err error
 
-	log.Infof("DEBUG bucketWithOptions: bucketName='%s', customEndpoint='%s', forSigning=%v",
+	log.Debugf("DEBUG bucketWithOptions: bucketName='%s', customEndpoint='%s', forSigning=%v",
 		f.bucketName, f.customEndpoint, forSigning)
 
 	// For signed URLs, always use standard GCS client to ensure signatures work
 	// with storage.googleapis.com, regardless of custom endpoint configuration
 	if forSigning || f.customEndpoint == "" {
-		log.Infof("DEBUG: Using standard GCS client")
+		log.Debugf("DEBUG: Using standard GCS client")
 		client, err = storage.NewClient(ctx)
 	} else {
-		log.Infof("DEBUG: Using custom endpoint client: %s", f.customEndpoint)
-		client, err = storage.NewClient(ctx, option.WithEndpoint(f.customEndpoint))
+		// Include bucket name in the custom endpoint path to match expected format:
+		// https://assets.cms.dev.reearth.io/reearth-cms-dev-assets/...
+		endpointWithBucket := f.customEndpoint + "/" + f.bucketName
+		log.Debugf("DEBUG: Using custom endpoint client: %s", endpointWithBucket)
+		log.Debugf("DEBUG: About to create client with bucket='%s', endpoint='%s'", f.bucketName, endpointWithBucket)
+
+		client, err = storage.NewClient(ctx, option.WithEndpoint(endpointWithBucket))
+
+		if err != nil {
+			log.Errorf("gcs: failed to initialize custom endpoint client: %v", err)
+			return nil, err
+		}
+
+		log.Debugf("DEBUG: Custom endpoint client created successfully")
 	}
 
 	if err != nil {
@@ -635,8 +649,10 @@ func (f *fileRepo) bucketWithOptions(ctx context.Context, forSigning bool) (*sto
 		return nil, err
 	}
 
+	log.Debugf("DEBUG: Creating bucket handle with bucketName='%s'", f.bucketName)
 	bucket := client.Bucket(f.bucketName)
-	log.Infof("DEBUG: Created bucket handle for: '%s'", f.bucketName)
+	log.Debugf("DEBUG: Bucket handle created successfully for bucket='%s'", f.bucketName)
+
 	return bucket, nil
 }
 
