bad32ea9affa398ef07954a7e0a5810dd79e6509
nourbalaha <nourbalaha0@gmail.com>
2025-10-15 14:38:19 +0900

    wip

--- web/src/stories/Button.stories.tsx
+++ web/src/stories/Button.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import Button from "@reearth-cms/components/atoms/Button";
 import Icon from "@reearth-cms/components/atoms/Icon";
