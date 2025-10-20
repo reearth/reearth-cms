82a389b87d85b66667fa84a5d5739760525b5d35
nourbalaha <nourbalaha0@gmail.com>
2025-10-20 18:45:12 +0900

    fix

--- web/e2e/support/i18n.ts
+++ web/e2e/support/i18n.ts
@@ -3,16 +3,14 @@ import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import yaml from "yaml";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Load translation files
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const enPath = path.resolve(__dirname, "../../src/i18n/translations/en.yml");
 const jaPath = path.resolve(__dirname, "../../src/i18n/translations/ja.yml");
-
 const en = yaml.parse(fs.readFileSync(enPath, "utf-8"));
 const ja = yaml.parse(fs.readFileSync(jaPath, "utf-8"));
 
@@ -27,7 +25,7 @@ const resources = {
 
 export const availableLanguages = Object.keys(resources);
 
-i18n.init({
+i18n.use(LanguageDetector).use(initReactI18next).init({
   resources,
   fallbackLng: "en",
   // allow keys to be phrases having `:`, `.`
