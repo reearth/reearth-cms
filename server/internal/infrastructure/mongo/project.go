46164c7b02f87bd4db345377ff1df25aeafebdac
zombozo12 <wigunaramadhan1998@gmail.com>
2025-10-25 00:38:47 +0700

    update topics filter to use regex patterns for better matching

--- server/internal/infrastructure/mongo/project.go
+++ server/internal/infrastructure/mongo/project.go
@@ -109,11 +109,11 @@ func (r *ProjectRepo) Search(ctx context.Context, f interfaces.ProjectFilter) (p
 	}
 
 	if f.Topics != nil && len(f.Topics) > 0 {
-		regexTopics := make([]bson.M, len(f.Topics))
+		regexPatterns := make([]primitive.Regex, len(f.Topics))
 		for i, topic := range f.Topics {
-			regexTopics[i] = bson.M{"$regex": primitive.Regex{Pattern: fmt.Sprintf("^%s$", regexp.QuoteMeta(topic)), Options: "i"}}
+			regexPatterns[i] = primitive.Regex{Pattern: fmt.Sprintf("^%s$", regexp.QuoteMeta(topic)), Options: "i"}
 		}
-		filter["topics"] = bson.M{"$in": regexTopics}
+		filter["topics"] = bson.M{"$elemMatch": bson.M{"$in": regexPatterns}}
 	}
 
 	return r.paginate(ctx, filter, f.Sort, f.Pagination)
