d1c1b76041b084b4b40e643f882ee6c97d77048a
nourbalaha <nourbalaha0@gmail.com>
2025-12-10 12:52:14 +0900

    wip

--- server/internal/app/main.go
+++ server/internal/app/main.go
@@ -32,8 +32,8 @@ func Start(debug bool, version string) {
 	// Init repositories
 	repos, gateways, acRepos, acGateways := InitReposAndGateways(ctx, conf)
 
-	// Start web server
-	NewServer(ctx, &ApplicationContext{
+	// Create application context
+	appCtx := &ApplicationContext{
 		Config:     conf,
 		Debug:      debug,
 		Version:    version,
@@ -41,7 +41,15 @@ func Start(debug bool, version string) {
 		Gateways:   gateways,
 		AcRepos:    acRepos,
 		AcGateways: acGateways,
-	}).Run(ctx)
+	}
+
+	// Run initial health check
+	if err := RunInitialHealthCheck(ctx, appCtx.Config, appCtx.Gateways.File); err != nil {
+		log.Fatalf("initial health check failed: %v", err)
+	}
+
+	// Start web server
+	NewServer(ctx, appCtx).Run(ctx)
 }
 
 type WebServer struct {
