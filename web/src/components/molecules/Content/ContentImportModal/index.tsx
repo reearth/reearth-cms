7c00e7367faed80a13a00cef626f56601fdc9244
kazettique <kazettique@gmail.com>
2025-12-22 12:33:33 +0800

    Revert "feat(web): import content for demo (#1732)"
    This reverts commit e4177d9896b1d29614efe32856c16a7e5cc9800a.

--- web/src/components/molecules/Content/ContentImportModal/index.tsx
+++ web/src/components/molecules/Content/ContentImportModal/index.tsx
@@ -11,17 +11,13 @@ import Loading from "@reearth-cms/components/atoms/Loading";
 import Modal from "@reearth-cms/components/atoms/Modal";
 import Space from "@reearth-cms/components/atoms/Space";
 import Typography from "@reearth-cms/components/atoms/Typography";
-import Upload, { RcFile, UploadProps } from "@reearth-cms/components/atoms/Upload";
+import Upload, { UploadProps } from "@reearth-cms/components/atoms/Upload";
 import { Model } from "@reearth-cms/components/molecules/Model/types";
 import { ValidateImportResult } from "@reearth-cms/components/organisms/Project/Content/ContentList/hooks";
 import { Trans, useT } from "@reearth-cms/i18n";
 import { Constant } from "@reearth-cms/utils/constant";
 import { FileUtils } from "@reearth-cms/utils/file";
-import {
-  ImportContentUtils,
-  ImportContentJSON2,
-  ValidationErrorMeta2,
-} from "@reearth-cms/utils/importContent";
+import { ErrorMeta, ImportContentJSON, ImportUtils } from "@reearth-cms/utils/import";
 import { ObjectUtils } from "@reearth-cms/utils/object";
 
 const { Dragger } = Upload;
@@ -32,12 +28,14 @@ type Props = {
   modelFields: Model["schema"]["fields"];
   onSetDataChecking: (isChecking: boolean) => void;
   onClose: () => void;
-  onFileContentChange: (payload: {
+  onFileContentChange: ({
+    fileContent,
+    extension,
+  }: {
     fileName: string;
-    fileContent: ImportContentJSON2["results"];
+    fileContent: Record<string, unknown>[];
     extension: "csv" | "json" | "geojson";
     url: string;
-    raw: RcFile;
   }) => void;
   alertList: AlertProps[];
   setAlertList: Dispatch<SetStateAction<AlertProps[]>>;
@@ -100,7 +98,7 @@ const ContentImportModal: React.FC<Props> = ({
   }, [setAlertList, t]);
 
   const schemaValidationAlert = useCallback(
-    (errorMeta: ValidationErrorMeta2) => {
+    (errorMeta: ErrorMeta) => {
       // const partialAlertProps: Pick<AlertProps, "type" | "closable" | "showIcon"> = {
       //   type: "error",
       //   closable: true,
@@ -113,20 +111,17 @@ const ContentImportModal: React.FC<Props> = ({
 
       if (errorMeta.exceedLimit) {
         // case: above limit + some mismatch (exceedLimit = true, mismatchFields.size > 0)
-        if (errorMeta.typeMismatchFieldKeys.size > 0) {
+        if (errorMeta.mismatchFields.size > 0) {
           setValidateImportResult({
             type: "error",
             title: t("Data file is too large and some fields don't match the schema."),
             description: t(
               "The data file can contain a maximum of {{max}} records. Please split the file and re-upload it. And {{count}} fields do not match the schema.",
-              {
-                max: Constant.IMPORT.MAX_CONTENT_RECORDS,
-                count: errorMeta.typeMismatchFieldKeys.size,
-              },
+              { max: Constant.IMPORT.MAX_CONTENT_RECORDS, count: errorMeta.mismatchFields.size },
             ),
             hint: t("Unmatched field hint", {
-              count: errorMeta.typeMismatchFieldKeys.size,
-              fields: Array.from(errorMeta.typeMismatchFieldKeys),
+              count: errorMeta.mismatchFields.size,
+              fields: Array.from(errorMeta.mismatchFields),
             }),
           });
         }
@@ -143,18 +138,21 @@ const ContentImportModal: React.FC<Props> = ({
         }
       } else {
         // case: below limit, some mismatch (exceedLimit = false, mismatchFields.size > 0)
-        if (errorMeta.typeMismatchFieldKeys.size > 0) {
+        if (
+          errorMeta.mismatchFields.size > 0 &&
+          errorMeta.mismatchFields.size < errorMeta.modelFieldCount
+        ) {
           console.log("errorMeta", errorMeta);
           setValidateImportResult({
             type: "warning",
             title: t("Some fields don't match the schema"),
             description: t(
               "{{count}} fields do not match the schema. You can continue the import, but the unmatched fields will be ignored.",
-              { count: errorMeta.typeMismatchFieldKeys.size },
+              { count: errorMeta.mismatchFields.size },
             ),
             hint: t("Unmatched field hint", {
-              count: errorMeta.typeMismatchFieldKeys.size,
-              fields: Array.from(errorMeta.typeMismatchFieldKeys),
+              count: errorMeta.mismatchFields.size,
+              fields: Array.from(errorMeta.mismatchFields),
             }),
             canForwardToImport: true,
           });
@@ -204,87 +202,87 @@ const ContentImportModal: React.FC<Props> = ({
         handleStartLoading();
         const content = await FileUtils.parseTextFile(file);
 
-        const demoTemp = JSON.stringify({ results: JSON.parse(content), totalCount: 1 });
-
         switch (extension) {
-          case "json": {
-            const jsonValidation = await ObjectUtils.safeJSONParse<ImportContentJSON2>(demoTemp);
-            if (!jsonValidation.isValid) {
-              raiseIllegalFileAlert();
-              return;
+          case "json":
+            {
+              const jsonValidation = await ObjectUtils.safeJSONParse<ImportContentJSON>(content);
+              if (!jsonValidation.isValid) {
+                raiseIllegalFileAlert();
+                return;
+              }
+
+              const jsonContentValidation = await ImportUtils.validateContentFromJSON(
+                jsonValidation.data,
+                modelFields,
+              );
+
+              if (!jsonContentValidation.isValid) {
+                schemaValidationAlert(jsonContentValidation.error);
+                return;
+              }
+
+              onFileContentChange({
+                fileContent: jsonContentValidation.data,
+                extension,
+                fileName,
+                url: location.pathname,
+              });
             }
-
-            const jsonContentValidation = await ImportContentUtils.validateContentFromJSON(
-              jsonValidation.data.results,
-              modelFields,
-            );
-
-            if (!jsonContentValidation.isValid) {
-              schemaValidationAlert(jsonContentValidation.error);
-              return;
-            }
-
-            onFileContentChange({
-              fileContent: jsonContentValidation.data,
-              extension,
-              fileName,
-              url: location.pathname,
-              raw: file,
-            });
             break;
-          }
 
-          // case "geojson": {
-          //   const geoJSONValidation = await ObjectUtils.validateGeoJson(content);
-          //   if (!geoJSONValidation.isValid) {
-          //     raiseIllegalFileAlert();
-          //     return;
-          //   }
+          case "geojson":
+            {
+              const geoJSONValidation = await ObjectUtils.validateGeoJson(content);
+              if (!geoJSONValidation.isValid) {
+                raiseIllegalFileAlert();
+                return;
+              }
 
-          //   const geoJSONContentValidation = await ImportContentUtils.validateContentFromGeoJson(
-          //     geoJSONValidation.data,
-          //     modelFields,
-          //   );
+              const geoJSONContentValidation = await ImportUtils.validateContentFromGeoJson(
+                geoJSONValidation.data,
+                modelFields,
+              );
 
-          //   if (!geoJSONContentValidation.isValid) {
-          //     schemaValidationAlert(geoJSONContentValidation.error);
-          //     return;
-          //   }
+              if (!geoJSONContentValidation.isValid) {
+                schemaValidationAlert(geoJSONContentValidation.error);
+                return;
+              }
 
-          //   onFileContentChange({
-          //     fileContent: geoJSONContentValidation.data,
-          //     extension,
-          //     fileName,
-          //     url: location.pathname,
-          //   });
-          //   break;
-          // }
+              onFileContentChange({
+                fileContent: geoJSONContentValidation.data,
+                extension,
+                fileName,
+                url: location.pathname,
+              });
+            }
+            break;
 
-          // case "csv": {
-          //   const csvValidation = await ImportContentUtils.convertCSVToJSON(content);
-          //   if (!csvValidation.isValid) {
-          //     raiseIllegalFileAlert();
-          //     return;
-          //   }
+          case "csv":
+            {
+              const csvValidation = await ImportUtils.convertCSVToJSON(content);
+              if (!csvValidation.isValid) {
+                raiseIllegalFileAlert();
+                return;
+              }
 
-          //   const csvContentValidation = await ImportContentUtils.validateContentFromCSV(
-          //     csvValidation.data,
-          //     modelFields,
-          //   );
+              const csvContentValidation = await ImportUtils.validateContentFromCSV(
+                csvValidation.data,
+                modelFields,
+              );
 
-          //   if (!csvContentValidation.isValid) {
-          //     schemaValidationAlert(csvContentValidation.error);
-          //     return;
-          //   }
+              if (!csvContentValidation.isValid) {
+                schemaValidationAlert(csvContentValidation.error);
+                return;
+              }
 
-          //   onFileContentChange({
-          //     fileContent: csvContentValidation.data,
-          //     extension,
-          //     fileName,
-          //     url: location.pathname,
-          //   });
-          //   break;
-          // }
+              onFileContentChange({
+                fileContent: csvContentValidation.data,
+                extension,
+                fileName,
+                url: location.pathname,
+              });
+            }
+            break;
 
           default:
         }
@@ -382,10 +380,6 @@ const ContentImportModal: React.FC<Props> = ({
                   }}
                 />
               </p>
-              {/* TODO: demo use */}
-              <TemplateLink href={Constant.PUBLIC_FILE.IMPORT_CONTENT_JSON_FOR_DEMO}>
-                JSON file for demo
-              </TemplateLink>
               {alertList.map((alert, index) => (
                 <Alert
                   {...alert}
--- web/src/components/organisms/Project/Content/ContentList/hooks.ts
+++ web/src/components/organisms/Project/Content/ContentList/hooks.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { RcFile } from "antd/es/upload";
 import { useCallback, useEffect, useMemo, useState, useRef, Key } from "react";
 import { useNavigate, useParams, useLocation } from "react-router-dom";
 
@@ -19,6 +17,7 @@ import {
 import { selectedTagIdsGet } from "@reearth-cms/components/molecules/Content/utils";
 import { Model } from "@reearth-cms/components/molecules/Model/types";
 import { Request, RequestItem } from "@reearth-cms/components/molecules/Request/types";
+import { UploadStatus } from "@reearth-cms/components/molecules/Uploader/types";
 import {
   ConditionInput,
   ItemSort,
@@ -48,11 +47,10 @@ import {
   View as GQLView,
   useGetViewsQuery,
   ItemFieldInput,
-  useImportItemsMutation,
 } from "@reearth-cms/gql/graphql-client-api";
 import { useT } from "@reearth-cms/i18n";
 import { useUserId, useCollapsedModelMenu, useUserRights, useUploader } from "@reearth-cms/state";
-import { ImportContentJSON2 } from "@reearth-cms/utils/importContent";
+import { newID } from "@reearth-cms/utils/id";
 
 import { fileName } from "./utils";
 
@@ -70,6 +68,17 @@ export type ValidateImportResult = {
   canForwardToImport?: boolean;
   hint?: string;
 };
+
+// export type ImportState = {
+//   step: 'SELECT_FILE' | 'CHECKING' | 'RESULT';
+//   fileError: AlertProps[];
+//   checkResult: ValidateImportResult;
+// }
+
+// export type ImportAction =
+//   | { type: 'TO_CHECKING' }
+//   | { type: 'TO_SELECT_FILE' }
+//   | { type: 'TO_RESULT' }
 
 export default () => {
   const {
@@ -104,7 +113,7 @@ export default () => {
     null,
   );
   // TODO: move states above into machine
-  const [_uploader, _setUploader] = useUploader();
+  const [_uploader, setUploader] = useUploader();
 
   const navigate = useNavigate();
   const { modelId } = useParams();
@@ -213,8 +222,7 @@ export default () => {
 
   const [updateItemMutation] = useUpdateItemMutation();
   const [getItem] = useGetItemLazyQuery({ fetchPolicy: "no-cache" });
-  const [createNewItem] = useCreateItemMutation({ refetchQueries: ["SearchItem"] });
-  const [importItemMutation] = useImportItemsMutation({ refetchQueries: ["SearchItem"] });
+  const [createNewItem] = useCreateItemMutation();
 
   const itemIdToMetadata = useRef(new Map<string, Metadata>());
   const metadataVersionSet = useCallback(
@@ -387,6 +395,7 @@ export default () => {
 
   const fieldsGet = useCallback(
     (item: Item) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const result: Record<string, any> = {};
       item?.fields?.map(field => {
         result[field.schemaFieldId] = fieldValueGet(field, item);
@@ -397,6 +406,7 @@ export default () => {
   );
 
   const metadataGet = useCallback((fields?: ItemField[]) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const result: Record<string, any> = {};
     fields?.forEach(field => {
       if (Array.isArray(field.value) && field.value.length > 0) {
@@ -470,7 +480,7 @@ export default () => {
       required: field.required,
       sorter: true,
       sortOrder: sortOrderGet(field.id),
-
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       render: (el: any) => renderField(el, field),
     }));
 
@@ -489,7 +499,7 @@ export default () => {
         required: field.required,
         sorter: true,
         sortOrder: sortOrderGet(field.id),
-
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         render: (el: any, record: ContentTableField) => {
           const update = hasRightGet(record.createdBy.id)
             ? (value?: string | string[] | boolean, index?: number) => {
@@ -647,120 +657,47 @@ export default () => {
     setValidateImportResult(null);
   }, []);
 
-  // const handleQueueToUploader = useCallback(
-  //   (payload: { fileName: string; fileContent: Record<string, unknown>[]; url: string }) => {
-  //     setUploader(prev => ({
-  //       ...prev,
-  //       showBadge: true,
-  //       isOpen: true,
-  //       queue: [
-  //         {
-  //           id: newID(),
-  //           status: UploadStatus.InProgress,
-  //           fileName: payload.fileName,
-  //           fileContent: payload.fileContent,
-  //           progress: 0,
-  //           url: payload.url,
-  //           error: null,
-  //         },
-  //         ...prev.queue,
-  //       ],
-  //     }));
-  //   },
-  //   [setUploader],
-  // );
+  const handleQueueToUploader = useCallback(
+    (payload: { fileName: string; fileContent: Record<string, unknown>[]; url: string }) => {
+      setUploader(prev => ({
+        ...prev,
+        showBadge: true,
+        isOpen: true,
+        queue: [
+          {
+            id: newID(),
+            status: UploadStatus.InProgress,
+            fileName: payload.fileName,
+            fileContent: payload.fileContent,
+            progress: 0,
+            url: payload.url,
+            error: null,
+          },
+          ...prev.queue,
+        ],
+      }));
+    },
+    [setUploader],
+  );
 
   const handleImportContentFileChange = useCallback(
-    async ({
-      fileName: _fileName,
+    ({
+      fileName,
       fileContent,
       extension: _extension,
-      url: _url,
-      raw,
+      url,
     }: {
       fileName: string;
-      fileContent: ImportContentJSON2["results"];
+      fileContent: Record<string, unknown>[];
       extension: "csv" | "json" | "geojson";
       url: string;
-      raw: RcFile;
     }) => {
-      // handleQueueToUploader({ fileName, fileContent, url });
-      if (_extension !== "json") {
-        alert("only support json for now");
-        return;
-      }
-
-      if (!currentModel) return;
-
-      const targetFields = currentModel.schema.fields;
-
-      const _reqList = Object.values(fileContent).reduce(
-        (acc, curr) => {
-          const one = Object.entries(curr).reduce(
-            (_acc, _curr) => {
-              const [key, value] = _curr;
-              const findField = targetFields.find(_item => key === _item.key);
-              // const modelField = modelFields.get(item.key);
-              if (findField) {
-                return [
-                  ..._acc,
-                  {
-                    value,
-                    schemaFieldId: findField.id,
-                    type: findField.type,
-                  },
-                ];
-              }
-
-              return _acc;
-            },
-            [] as Record<string, any>[],
-          );
-
-          return [...acc, one];
-        },
-        [] as Record<string, any>[],
-      );
-
-      // console.log("raw", raw);
-
-      const res = await importItemMutation({
-        variables: {
-          importItemsInput: {
-            file: raw,
-            modelId: currentModel.id,
-            // geoField: "location",
-          },
-        },
-      });
-
-      if (res.errors) {
-        Notification.error({ message: "Import failed" });
-      } else {
-        Notification.success({ message: "Import success" });
-      }
-
-      // console.log("res", res);
-
-      // go to backend
-      // for await (const reqItem of reqList) {
-      //   const result = await createNewItem({
-      //     variables: {
-      //       modelId: currentModel.id,
-      //       schemaId: currentModel.schema.id,
-      //       fields: reqItem as ItemFieldInput[],
-      //     },
-      //   });
-      //   if (result.errors) {
-      //     Notification.error({ message: "Import failed" });
-      //   } else {
-      //     Notification.success({ message: "Import success" });
-      //   }
-      // }
-
+      handleQueueToUploader({ fileName, fileContent, url });
       handleImportContentModalClose();
+
+      // console.log("success, go to backend");
     },
-    [currentModel, handleImportContentModalClose, importItemMutation],
+    [handleImportContentModalClose, handleQueueToUploader],
   );
 
   return {
