4f4442b79c980e19aaab023bb8fcc6e4ff6817d5
nourbalaha <nourbalaha0@gmail.com>
2026-01-15 12:03:13 +0300

    remove

--- server/internal/infrastructure/mongo/asset.go
+++ server/internal/infrastructure/mongo/asset.go
@@ -95,7 +95,6 @@ func (r *Asset) Search(ctx context.Context, pID id.ProjectID, filter repo.AssetF
 	}
 
 	if filter.Keyword != nil && *filter.Keyword != "" {
-		// Normalize the search keyword using NFKC to match how filenames are stored
 		normalizedKeyword := asset.NormalizeFileName(*filter.Keyword)
 		filters["filename"] = bson.M{
 			"$regex": primitive.Regex{Pattern: fmt.Sprintf(".*%s.*", regexp.QuoteMeta(normalizedKeyword)), Options: "i"},
