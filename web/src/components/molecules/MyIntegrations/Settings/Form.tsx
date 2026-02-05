135c8d73cf3e02d1d1ac36b547d21289312487e6
Woody Chang <kazettique@gmail.com>
2026-02-05 18:13:06 +0900

    feat: update my integration code exmple

--- web/src/components/molecules/MyIntegrations/Settings/Form.tsx
+++ web/src/components/molecules/MyIntegrations/Settings/Form.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 import Button from "@reearth-cms/components/atoms/Button";
 import Col from "@reearth-cms/components/atoms/Col";
@@ -12,6 +12,7 @@ import { useModal } from "@reearth-cms/components/atoms/Modal";
 import Password from "@reearth-cms/components/atoms/Password";
 import Row from "@reearth-cms/components/atoms/Row";
 import TextArea from "@reearth-cms/components/atoms/TextArea";
+import Typography from "@reearth-cms/components/atoms/Typography";
 import {
   Integration,
   IntegrationInfo,
@@ -30,6 +31,12 @@ type FormType = {
   name: string;
   description: string;
   logoUrl: string;
+};
+
+type CodeExampleItem = {
+  type: "normal" | "highlight";
+  text: string;
+  lineBreak: boolean;
 };
 
 const MyIntegrationForm: React.FC<Props> = ({
@@ -85,6 +92,77 @@ const MyIntegrationForm: React.FC<Props> = ({
       },
     });
   }, [confirm, t, onRegenerateToken]);
+
+  const codeExampleTest = useMemo<CodeExampleItem[]>(
+    () => [
+      {
+        type: "normal",
+        text: "curl --location --request POST ",
+        lineBreak: true,
+      },
+      {
+        type: "normal",
+        text: "'",
+        lineBreak: false,
+      },
+      {
+        type: "normal",
+        text: window.REEARTH_CONFIG?.api || "",
+        lineBreak: false,
+      },
+      {
+        type: "normal",
+        text: "/",
+        lineBreak: false,
+      },
+      {
+        type: "highlight",
+        text: t("<workspace_id_or_alias>"),
+        lineBreak: false,
+      },
+      {
+        type: "normal",
+        text: "/projects/",
+        lineBreak: false,
+      },
+      {
+        type: "highlight",
+        text: t("<project_id_or_alias>"),
+        lineBreak: false,
+      },
+      {
+        type: "normal",
+        text: "/models/",
+        lineBreak: false,
+      },
+      {
+        type: "highlight",
+        text: t("<model_id_or_key>"),
+        lineBreak: false,
+      },
+      {
+        type: "normal",
+        text: "/items' ",
+        lineBreak: true,
+      },
+      {
+        type: "normal",
+        text: "--header 'Authorization: Bearer ",
+        lineBreak: false,
+      },
+      {
+        type: "highlight",
+        text: t("<your_integration_token>"),
+        lineBreak: false,
+      },
+      {
+        type: "normal",
+        text: "'",
+        lineBreak: false,
+      },
+    ],
+    [t],
+  );
 
   return (
     <Form
@@ -145,12 +223,19 @@ const MyIntegrationForm: React.FC<Props> = ({
         <Col span={11}>
           <CodeExampleTitle>{t("Code Example")}</CodeExampleTitle>
           <CodeExample>
-            curl --location --request POST <br />
-            &apos;{window.REEARTH_CONFIG?.api}/models/
-            <CodeImportant>“{t("your model id here")}”</CodeImportant>/items&apos;&nbsp;\
-            <br />
-            --header &apos;Authorization: Bearer&nbsp;
-            <CodeImportant>“{t("your Integration Token here")}”</CodeImportant>&apos;
+            <StyledCopyButton
+              copyable={{ text: codeExampleTest.map(item => item.text).join("") }}
+            />
+            {codeExampleTest.map((item, index) => (
+              <span key={index}>
+                {item.type === "highlight" ? (
+                  <Typography.Text code>{item.text}</Typography.Text>
+                ) : (
+                  <span>{item.text}</span>
+                )}
+                {item.lineBreak && <br />}
+              </span>
+            ))}
           </CodeExample>
         </Col>
       </Row>
@@ -165,17 +250,16 @@ const CodeExampleTitle = styled.h2`
   color: rgba(0, 0, 0, 0.85);
 `;
 
-const CodeExample = styled.div`
+const CodeExample = styled.pre`
   border: 1px solid #d9d9d9;
   padding: 5px 12px;
   font-weight: 400;
   font-size: 14px;
   line-height: 22px;
   color: rgba(0, 0, 0, 0.85);
-`;
-
-const CodeImportant = styled.span`
-  color: #ff4d4f;
+  position: relative;
+  white-space: pre-wrap;
+  word-wrap: break-word;
 `;
 
 const StyledDivider = styled(Divider)`
@@ -203,6 +287,13 @@ const StyledTokenInput = styled(Password)`
   .ant-input-suffix {
     order: 2;
   }
+`;
+
+const StyledCopyButton = styled(CopyButton)`
+  position: absolute;
+  top: 5px;
+  right: 12px;
+  background: #ffffff;
 `;
 
 export default MyIntegrationForm;
