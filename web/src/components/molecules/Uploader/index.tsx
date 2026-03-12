33fd3ea60d2a009a97cd47a48cf1233b7deab5f6
Woody Chang <kazettique@gmail.com>
2026-03-12 15:55:18 +0900

    fix: update style

--- web/src/components/molecules/Uploader/index.tsx
+++ web/src/components/molecules/Uploader/index.tsx
@@ -249,7 +249,7 @@ const Card = styled(motion.div)`
     0 1px 2px -2px rgba(0, 0, 0, 0.16),
     0 3px 6px 0 rgba(0, 0, 0, 0.12),
     0 5px 12px 4px rgba(0, 0, 0, 0.09);
-  overflow-y: scroll;
+  overflow-y: auto;
 
   &[data-corner="${Corner.BottomRight}"] {
     bottom: 0;
