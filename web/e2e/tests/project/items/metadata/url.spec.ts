adb2931d2ffa448a6c4d22caf51c440708972b96
Woody Chang <kazettique@gmail.com>
2026-03-13 16:10:06 +0900

    chore: test

--- web/e2e/tests/project/items/metadata/url.spec.ts
+++ web/e2e/tests/project/items/metadata/url.spec.ts
@@ -77,7 +77,11 @@ test("Url metadata creating and updating has succeeded", async ({
   await expect(contentPage.linkByName("http://test3.com")).toBeVisible();
 });
 
-test("Url metadata editing has succeeded", async ({ page, fieldEditorPage, contentPage }) => {
+test("@focus Url metadata editing has succeeded", async ({
+  page,
+  fieldEditorPage,
+  contentPage,
+}) => {
   await fieldEditorPage.metaDataTab.click();
   await fieldEditorPage.fieldTypeListItem("URL").click();
   await fieldEditorPage.displayNameInput.fill("url1");
