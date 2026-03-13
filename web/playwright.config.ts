733345c7bb0866a4a449a6889809ce7259641027
Woody Chang <kazettique@gmail.com>
2026-03-13 21:47:59 +0900

    chore: test

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -14,7 +14,7 @@ export const baseURL = process.env.REEARTH_CMS_E2E_BASEURL || "http://localhost:
 
 const config: PlaywrightTestConfig = {
   globalSetup: path.resolve(__dirname, "./e2e/global-setup.ts"),
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   retries: 2,
   maxFailures: process.env.CI ? 30 : 10,
   forbidOnly: !!process.env.CI,
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
   testMatch: "**/*.spec.ts",
   testIgnore: ["**/node_modules/**", "**/dist/**", "**/build/**"],
   reporter: process.env.CI ? "github" : [["list"], ["html", { open: "never" }]],
-  fullyParallel: false,
+  fullyParallel: true,
   projects: [
     {
       name: "setup",
