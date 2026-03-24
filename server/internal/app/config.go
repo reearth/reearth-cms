effdb045b582f7905a5c8bd39a7549af6d3565a1
“jasonkarel” <jasonkareli@gmail.com>
2026-03-24 11:40:58 +0700

    add swtich for cerbos

--- server/internal/app/config.go
+++ server/internal/app/config.go
@@ -89,9 +89,10 @@ type HealthCheckConfig struct {
 }
 
 type AccountAPIConfig struct {
-	Enabled bool   `pp:",omitempty"`
-	Host    string `pp:",omitempty"`
-	Timeout int    `pp:",omitempty"`
+	Enabled         bool   `pp:",omitempty"`
+	Host            string `pp:",omitempty"`
+	Timeout         int    `pp:",omitempty"`
+	CheckPermission bool   `pp:",omitempty"`
 }
 
 type PolicyCheckerConfig struct {
