af49874d35fdd527b55a5fc6a6909e512e98d66d
Woody Chang <kazettique@gmail.com>
2026-03-30 17:08:55 +0900

    fix: type issue

--- web/e2e/pages/content.page.ts
+++ web/e2e/pages/content.page.ts
@@ -123,7 +123,7 @@ export class ContentPage extends BasePage {
     return this.getByRole("button", { name: "Cancel" });
   }
   get moreButton(): Locator {
-    return this.getByRole("tab", { selected: true }).getByLabel("more");
+    return this.page.locator('role=tab[selected]').getByLabel("more");
   }
   get renameViewButton(): Locator {
     return this.getByText("Rename");
