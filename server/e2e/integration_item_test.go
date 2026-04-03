16ed5442fe390eff988712e4bceac3b3c134d8bc
“jasonkarel” <jasonkareli@gmail.com>
2026-04-03 14:23:08 +0700

    fix e2e gql

--- server/e2e/integration_item_test.go
+++ server/e2e/integration_item_test.go
@@ -459,8 +459,8 @@ func baseSeeder(ctx context.Context, r *repo.Container, g *gateway.Container) er
 	}
 
 	itm7 := item.New().ID(itmId7).
-		Schema(s1.ID()).
-		Model(m1.ID()).
+		Schema(s0.ID()).
+		Model(m0.ID()).
 		Project(p.ID()).
 		MustBuild()
 	if err := r.Item.Save(ctx, itm7); err != nil {
