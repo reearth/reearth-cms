37333ed5384c4f4c41702d1bfda28da66c6f9528
nourbalaha <nourbalaha0@gmail.com>
2026-08-07 18:58:36 +0900

    wip

--- server/internal/usecase/interactor/asset.go
+++ server/internal/usecase/interactor/asset.go
@@ -966,15 +966,6 @@ func (i *Asset) UpdateFiles(ctx context.Context, aid id.AssetID, s *asset.Archiv
 				return nil, nil, fmt.Errorf("failed to find a project: %w", err)
 			}
 
-			a.UpdateArchiveExtractionStatus(s)
-			if previewType != nil {
-				a.UpdatePreviewType(previewType)
-			}
-
-			if err := i.repos.Asset.Save(ctx, a); err != nil {
-				return nil, nil, fmt.Errorf("failed to save an asset: %w", err)
-			}
-
 			return a, prj, nil
 		},
 	)
@@ -991,6 +982,32 @@ func (i *Asset) UpdateFiles(ctx context.Context, aid id.AssetID, s *asset.Archiv
 		return nil, fmt.Errorf("failed to save asset files: %w", err)
 	}
 	log.Debugfc(ctx, "asset.UpdateFiles: saving asset files done: assetID=%s", aid)
+
+	a, err = Run1(
+		ctx, op, i.repos,
+		Usecase().Transaction(),
+		func(ctx context.Context) (*asset.Asset, error) {
+			a, err := i.repos.Asset.FindByID(ctx, aid)
+			if err != nil {
+				return nil, fmt.Errorf("failed to find an asset: %w", err)
+			}
+
+			a.UpdateArchiveExtractionStatus(s)
+			if previewType != nil {
+				a.UpdatePreviewType(previewType)
+			}
+
+			if err := i.repos.Asset.Save(ctx, a); err != nil {
+				return nil, fmt.Errorf("failed to save an asset: %w", err)
+			}
+
+			return a, nil
+		},
+	)
+	if err != nil {
+		i.markUpdateFilesFailed(ctx, aid)
+		return nil, err
+	}
 
 	if err := i.event(ctx, Event{
 		Project:   prj,
