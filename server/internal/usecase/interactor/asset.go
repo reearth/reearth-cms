0d811c6f6d1af9114f378b80558a6892427ba5fb
nourbalaha <nourbalaha0@gmail.com>
2026-08-17 17:32:00 +0900

    wip

--- server/internal/usecase/interactor/asset.go
+++ server/internal/usecase/interactor/asset.go
@@ -966,6 +966,11 @@ func (i *Asset) UpdateFiles(ctx context.Context, aid id.AssetID, s *asset.Archiv
 		ctx, op, i.repos,
 		Usecase().Transaction(),
 		func(ctx context.Context) (*asset.Asset, error) {
+			log.Debugfc(ctx, "asset.UpdateFiles: saving asset files begin: assetID=%s fileCount=%d", aid, len(assetFiles))
+			if err := i.repos.AssetFile.SaveFlat(ctx, a.ID(), srcfile, assetFiles); err != nil {
+				return nil, fmt.Errorf("failed to save asset files: %w", err)
+			}
+			log.Debugfc(ctx, "asset.UpdateFiles: saving asset files done: assetID=%s", aid)
 
 			a.UpdateArchiveExtractionStatus(s)
 			if previewType != nil {
@@ -976,6 +981,17 @@ func (i *Asset) UpdateFiles(ctx context.Context, aid id.AssetID, s *asset.Archiv
 				return nil, fmt.Errorf("failed to save an asset: %w", err)
 			}
 
+			if err := i.event(ctx, Event{
+				Project:   prj,
+				Workspace: prj.Workspace(),
+				Type:      event.AssetDecompress,
+				Object:    a,
+				Operator:  op.Operator(),
+			}); err != nil {
+				return nil, fmt.Errorf("failed to create an event: %w", err)
+			}
+			log.Debugfc(ctx, "asset.UpdateFiles: done: assetID=%s", aid)
+
 			return a, nil
 		},
 	)
@@ -983,25 +999,6 @@ func (i *Asset) UpdateFiles(ctx context.Context, aid id.AssetID, s *asset.Archiv
 		i.markUpdateFilesFailed(ctx, aid)
 		return nil, err
 	}
-
-	log.Debugfc(ctx, "asset.UpdateFiles: saving asset files begin: assetID=%s fileCount=%d", aid, len(assetFiles))
-	if err := i.repos.AssetFile.SaveFlat(ctx, a.ID(), srcfile, assetFiles); err != nil {
-		i.markUpdateFilesFailed(ctx, aid)
-		return nil, fmt.Errorf("failed to save asset files: %w", err)
-	}
-	log.Debugfc(ctx, "asset.UpdateFiles: saving asset files done: assetID=%s", aid)
-
-	if err := i.event(ctx, Event{
-		Project:   prj,
-		Workspace: prj.Workspace(),
-		Type:      event.AssetDecompress,
-		Object:    a,
-		Operator:  op.Operator(),
-	}); err != nil {
-		i.markUpdateFilesFailed(ctx, aid)
-		return nil, fmt.Errorf("failed to create an event: %w", err)
-	}
-	log.Debugfc(ctx, "asset.UpdateFiles: done: assetID=%s", aid)
 
 	return a, nil
 }
