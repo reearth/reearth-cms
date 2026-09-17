1339a80ad7c9c3ee6fcdab274690a68dc441dd64
nourbalaha <nourbalaha0@gmail.com>
2026-09-17 13:52:55 +0900

    fix

--- server/internal/adapter/integration/item_import.go
+++ server/internal/adapter/integration/item_import.go
@@ -63,9 +63,6 @@ func (s *Server) ModelImport(ctx context.Context, request ModelImportRequestObje
 		if err := runtime.BindMultipart(&body, *request.MultipartBody); err != nil {
 			return nil, err
 		}
-		if body.File == nil {
-			return nil, ErrFileIsMissing
-		}
 		fc, err := body.File.Reader()
 		if err != nil {
 			return nil, err
