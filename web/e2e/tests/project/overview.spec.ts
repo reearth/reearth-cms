5b29ce3240c79eafdc9b7ab0d2071b8e739a43e5
Woody Chang <kazettique@gmail.com>
2026-09-24 13:28:36 +0900

    test

--- web/e2e/tests/project/overview.spec.ts
+++ web/e2e/tests/project/overview.spec.ts
@@ -101,7 +101,7 @@ test.describe("Model Export tests on Overview page", () => {
     });
   });
 
-  test("Model Export as CSV on Overview page has succeeded", async ({
+  test.only("Model Export as CSV on Overview page has succeeded", async ({
     schemaPage,
     projectPage,
     fieldEditorPage,
